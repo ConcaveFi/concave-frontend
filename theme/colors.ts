531623ea8acb9a31e525caee46cb7f04de847a57
jgwentworth92 <95505687+jgwentworth92@users.noreply.github.com>
2022-01-07 10:34:21 -0600

    Update colors.ts

--- theme/colors.ts
+++ theme/colors.ts
@@ -9,6 +9,7 @@ export const colors = {
   grey: {
     500: '#5F7A99',
     700: '#708aa7',
+    800: '#364C67'
   },
   blue: {
     500: '#419ceb',
@@ -16,12 +17,15 @@ export const colors = {
   green: {
     500: '#1B3442',
   },
-
+ 
+ radial_reflection:'radial-gradient(97.48% 82.4% at 49.69% 76.45%, #3082E1 0%, #3D3786 31.18%, #000000 100%)',
   strokeReflection:
     'linear-gradient(41.89deg, #53399B 0.69%, #7DE0FF 38.19%, #504179 72.85%, #84E2FF 100%)',
   gradients: {
     primary: 'linear(to-l, #8671C0 0%, #4DBEE1 100%)',
     green: 'linear(239.18deg, #1B3442 27.18%, #0A161F 96.11%)',
+    radial_reflection:'radial-gradient(97.48% 82.4% at 49.69% 76.45%, #3082E1 0%, #3D3786 31.18%, #000000 100%)'
+    
   },
 }
 
