079cc9bcd2c474fe7c8ca9463be23229bfdc7a68
shanghigh <cryptoshang@gmail.com>
2022-01-12 07:28:32 -0900

    cleanup buttons
    removed buttons zap, swap, bond and references from other pages.

--- pages/swap.tsx
+++ pages/swap.tsx
@@ -2,7 +2,6 @@ import { Container, Flex } from '@chakra-ui/react'
 import { Card } from 'components/Card'
 import React from 'react'
 import { Page } from '../components/Page'
-import { NavBar } from 'components/Gcnv'
 import GcnvTitle from 'components/GcnvTitle'
 import SwapInfoCard from 'components/GetCNV/SwapInfoCard'
 import BuyCard from 'components/GetCNV/BuyCard'
@@ -17,9 +16,6 @@ function Swap() {
             description="Get your gCNV that will grow internal CNV number your gCNV that will grow number or
                         smth idk lol Get your gCNV that will grow internal CNV number or smth idk lol"
           />
-
-          <NavBar active="swap" />
-
           <Flex gap={6} flexWrap="wrap" justify="center">
             <BuyCard buttonLabel="Swap" />
             <SwapInfoCard />
