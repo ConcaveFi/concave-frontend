eae3636cd2993519f2150edfc905a2aa1b2760be
David Schrammel <schrammel@live.com>
2022-02-20 15:33:06 -0300

    add icons

---
+++ components/icons/settings.tsx
@@ -0,0 +1,15 @@
+import { Icon } from '@chakra-ui/icons'
+import { IconProps } from '@chakra-ui/react'
+
+export const SettingsIcon = (props: IconProps) => (
+  <Icon viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      d="M21.58 6.23h-5.75M6.25 6.23H2.42M10.08 9.58a3.35 3.35 0 1 0 0-6.7 3.35 3.35 0 0 0 0 6.7ZM21.58 16.77h-3.83M8.17 16.77H2.42M13.92 20.13a3.35 3.35 0 1 0 0-6.71 3.35 3.35 0 0 0 0 6.7Z"
+      stroke="#5F7A99"
+      strokeWidth="2"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </Icon>
+)
