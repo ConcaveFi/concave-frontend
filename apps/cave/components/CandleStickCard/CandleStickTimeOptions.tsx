c0d43456d643b0a6cc32c962dbc25c3e9eb9d2a1
David Schrammel <schrammel@live.com>
2022-02-28 23:29:46 -0300

    feat(libs/ui): BoxRadioGroup

--- apps/cave/components/CandleStickCard/CandleStickTimeOptions.tsx
+++ apps/cave/components/CandleStickCard/CandleStickTimeOptions.tsx
@@ -1,8 +1,8 @@
-import { Box, HStack, StackProps, useRadio, useRadioGroup } from '@chakra-ui/react'
-import { gradientStroke } from '@concave/ui'
-import React, { useMemo } from 'react'
+import { HStack, StackProps } from '@chakra-ui/react'
+import { BoxRadioGroup } from '@concave/ui'
 
 type OwnProps = StackProps & { onChangeInteral: (interval: number) => void }
+
 export const CandleStickTimeOptions = ({ onChangeInteral, ...stackProps }: OwnProps) => {
   const intervals = {
     '5m': 5 * 60,
@@ -11,49 +11,9 @@ export const CandleStickTimeOptions = ({ onChangeInteral, ...stackProps }: OwnPr
     '4H': 4 * 60 * 60,
     '1D': 24 * 60 * 60,
   }
-
-  const { getRadioProps } = useRadioGroup({
-    defaultValue: '5m',
-    onChange: (a) => {
-      onChangeInteral(intervals[a])
-    },
-  })
   return (
     <HStack py={1} px={2} borderRadius={'3xl'} shadow={'low'} {...stackProps}>
-      {Object.keys(intervals).map((value) => {
-        const radio = getRadioProps({ value })
-        return (
-          <RadioCard key={value} {...radio}>
-            {value}
-          </RadioCard>
-        )
-      })}
+      <BoxRadioGroup options={intervals} onChange={onChangeInteral} />
     </HStack>
-  )
-}
-
-const RadioCard = (props) => {
-  const { getInputProps, getCheckboxProps } = useRadio(props)
-  const input = getInputProps()
-  const checkbox = getCheckboxProps()
-  const checked = useMemo(() => {
-    return { ...gradientStroke({}), borderRadius: '2xl' }
-  }, [])
-  return (
-    <Box as="label">
-      <input {...input} />
-      <Box
-        {...checkbox}
-        cursor="pointer"
-        minWidth={12}
-        textAlign="center"
-        boxShadow="md"
-        _checked={checked}
-        px={2}
-        py={1}
-      >
-        {props.children}
-      </Box>
-    </Box>
   )
 }
