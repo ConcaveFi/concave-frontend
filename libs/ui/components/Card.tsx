8575fd4ffdb8897a29d0db53a8aeb6059bc4bb6c
gregs <ukfaeku@gmail.com>
2022-03-07 18:23:12 -0300

    ts is a bitch

--- libs/ui/components/Card.tsx
+++ libs/ui/components/Card.tsx
@@ -1,10 +1,14 @@
 import { StackProps, Stack } from '@chakra-ui/react'
-import { StyleFunctionProps } from '@chakra-ui/theme-tools'
+import { StyleFunctionProps, SystemStyleInterpolation } from '@chakra-ui/theme-tools'
 import { gradientStroke } from 'theme/utils/gradientStroke'
 
-export type CardProps = StackProps
+export interface CardProps extends StackProps {
+  borderWidth?: number
+}
 
-export const CardStyles = ({ borderWidth = 1 }: Partial<StyleFunctionProps> = {}) => ({
+export const CardStyles = ({
+  borderWidth = 1,
+}: Partial<StyleFunctionProps> = {}): SystemStyleInterpolation => ({
   ...gradientStroke({ borderWidth }),
   borderRadius: '2xl',
   maxW: '100%',
@@ -14,7 +18,7 @@ export const CardStyles = ({ borderWidth = 1 }: Partial<StyleFunctionProps> = {}
 export function Card({ children, spacing = 0, borderWidth, ...rest }: CardProps) {
   return (
     <Stack
-      __css={CardStyles({ borderWidth })} // __css can be overriten with the sx prop
+      __css={{ ...CardStyles({ borderWidth }) }} // __css can be overriten with the sx prop
       spacing={spacing}
       {...rest}
     >
