46aa02537df04f54cbc1135357e2e9b64f37063e
David Schrammel <schrammel@live.com>
2022-03-08 13:46:50 -0300

    feat(apps/cave/lib): add coingecko api

---
+++ apps/cave/lib/coingecko.adapter.ts
@@ -0,0 +1,45 @@
+import { CandleStickIntervalTypes } from 'components/CandleStickCard/useCandleStickChart'
+import { CoinType, coingeckoApi } from './coingecko.api'
+
+class CoingeckoApiProxy {
+  private coins: Promise<CoinType[]>
+  constructor() {}
+
+  async getCoins() {
+    if (!this.coins) {
+      this.coins = coingeckoApi.fetchCoins()
+    }
+    return this.coins
+  }
+
+  async discoveryId(symbol: string) {
+    const coins = await this.getCoins()
+    const { id } = coins.find((item) => {
+      return item.symbol.toLowerCase() === symbol?.toLowerCase()
+    })
+    return id
+  }
+
+  async getTokenPrice(symbol: string) {
+    const id = await this.discoveryId(symbol)
+    return Promise.resolve(
+      coingeckoApi.tokenPrice({
+        currency: 'usd',
+        ids: id,
+      }),
+    )
+  }
+
+  async fetchCandleStickData({
+    token,
+    interval,
+  }: {
+    token: string
+    interval: CandleStickIntervalTypes
+  }) {
+    const id = await this.discoveryId(token)
+    return coingeckoApi.fetchCandleStickData({ id, interval })
+  }
+}
+
+export const coingecko = new CoingeckoApiProxy()
