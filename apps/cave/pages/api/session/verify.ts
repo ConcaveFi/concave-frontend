a23513f6fa8e75a4095a57fc2c686ef891e23e99
gregs <ukfaeku@gmail.com>
2022-03-11 17:19:10 -0300

    better verify route

--- apps/cave/pages/api/session/verify.ts
+++ apps/cave/pages/api/session/verify.ts
@@ -1,7 +1,7 @@
 import { withIronSessionApiRoute } from 'iron-session/next'
 import { sessionOptions } from 'lib/session'
 import { NextApiRequest, NextApiResponse } from 'next'
-import { SiweMessage } from 'siwe'
+import { ErrorTypes, SiweMessage } from 'siwe'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { method } = req
@@ -9,22 +9,28 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     res.setHeader('Allow', ['POST']).status(405).end(`Method ${method} Not Allowed`)
     return
   }
+
   try {
     const { message, signature } = req.body
     const siweMessage = new SiweMessage(message)
     const validatedSiwe = await siweMessage.validate(signature)
 
-    if (validatedSiwe.nonce !== req.session.nonce) {
-      res.status(422).json({ message: 'Invalid nonce.' })
-      return
-    }
+    if (validatedSiwe.nonce !== req.session.nonce)
+      return res.status(422).json({ message: 'Invalid nonce' })
 
     req.session.siwe = validatedSiwe
     await req.session.save()
 
     res.json({ ok: true })
-  } catch (_error) {
-    res.json({ ok: false })
+  } catch (e) {
+    req.session.siwe = null
+    req.session.nonce = null
+    req.session.save()
+
+    if (e === ErrorTypes.EXPIRED_MESSAGE) return res.status(440).json({ message: e.message })
+    if (e === ErrorTypes.INVALID_SIGNATURE) return res.status(422).json({ message: e.message })
+
+    res.status(500).json({ message: e.message, ok: false })
   }
 }
 
