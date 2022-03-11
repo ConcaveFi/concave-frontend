f0ec486459c4ddc046cf4499f561761fa7f3e274
gregs <ukfaeku@gmail.com>
2022-03-11 17:18:34 -0300

    remove apollo

--- apps/cave/pages/swap.tsx
+++ apps/cave/pages/swap.tsx
@@ -1,14 +1,11 @@
 import { Container, Flex, HStack } from '@concave/ui'
-import { useQuery } from '@apollo/client'
 import React from 'react'
 import GcnvTitle from 'components/GcnvTitle'
-import { QUERY_TEST } from 'graphql/test'
 import { CandleStickCardMemo } from 'components/CandleStickCard'
 import { SwapCard } from 'components/Swap/SwapCard'
 import { useSwap } from 'components/Swap/useSwap'
 
 function Swap() {
-  const { loading, error, data } = useQuery(QUERY_TEST)
   const swap = useSwap({
     inputTokens: ['XMR', 'ETH', 'DAI', 'FRAX'],
     outputTokens: ['XMR', 'ETH', 'DAI', 'FRAX'],
