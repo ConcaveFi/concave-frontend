301273fe3ac59747247d4c066f5fee9fa20a297d
casuwu <erickdelgado@protonmail.com>
2022-03-13 06:16:51 -0400

    smol progress

--- apps/cave/pages/liquidstaking.tsx
+++ apps/cave/pages/liquidstaking.tsx
@@ -16,76 +16,42 @@ const InfoItem = ({ value, label, ...props }) => (
 
 const LiquidStakingInfo = ({ asset, roi, vestingTerm, icon }) => {
   return (
-    <Card bg="none" px={3} py={4} direction="row" shadow="Medium Glow Up">
-      <Avatar src={icon} name="CnvQuestionIcon" mr={3} />
-      <InfoItem value={asset.toUpperCase()} label="Asset" pl={3} />
-      <Box w="1px" mx={-1} my={-4} bg="strokeReflection" />
-      <InfoItem value={roi} label="ROI" />
-      <Box w="1px" mx={-1} my={-4} bg="strokeReflection" />
-      <InfoItem value={vestingTerm} label="Vesting Term" pr={0} />
+    <Card
+      bgGradient={''}
+      px={0}
+      borderRadius={40}
+      width={160}
+      height={326}
+      py={4}
+      direction="column"
+      shadow="Medium Glow Up"
+    >
+      <InfoItem value={''} label="Stake Period" pl={7} />
+      <InfoItem value={asset.toUpperCase()} label="" pl={7} />
     </Card>
   )
 }
 
 function LiquidStaking() {
   return (
-    <Container maxW="container.lg">
+    <Container maxW="container.lg" borderRadius={0} border="">
+      <GcnvTitle
+        title="Liquid Staking"
+        description="Lock CNV in a staking term and recieve a tradeable NFT representing the position. Stakers receive a share of profits from all Concave products and services: bonding revenue, investment returns and protocol fees."
+      />
       <Flex direction="row" gap={12}>
-        <GcnvTitle
-          title="Liquid Staking"
-          description="Lock CNV in a staking term and recieve a tradeable NFT representing the position. Stakers receive a share of profits from all Concave products and services: bonding revenue, investment returns and protocol fees."
-        />
         <Card
           w="220px"
           h="490px"
-          borderWidth={1}
+          borderWidth={0}
           borderRadius={16}
           px={6}
-          py={20}
+          py={10}
           shadow="up"
           bgGradient="linear(to-tr, secondary.150, secondary.100)"
-          gap={10}
+          gap={12}
         >
           <LiquidStakingInfo asset="12 Month" icon="" roi="400%" vestingTerm="360 days" />
-        </Card>
-        <Card
-          w="220px"
-          h="490px"
-          borderWidth={1}
-          borderRadius={16}
-          px={6}
-          py={20}
-          shadow="up"
-          bgGradient="linear(to-tr, secondary.150, secondary.100)"
-          gap={10}
-        >
-          <LiquidStakingInfo asset="6 Month" icon="" roi="300%" vestingTerm="180 days" />
-        </Card>
-        <Card
-          w="220px"
-          h="490px"
-          borderWidth={1}
-          borderRadius={16}
-          px={6}
-          py={20}
-          shadow="up"
-          bgGradient="linear(to-tr, secondary.150, secondary.100)"
-          gap={10}
-        >
-          <LiquidStakingInfo asset="3 Month" icon="" roi="200%" vestingTerm="90 days" />
-        </Card>
-        <Card
-          w="220px"
-          h="490px"
-          borderWidth={1}
-          borderRadius={16}
-          px={6}
-          py={20}
-          shadow="up"
-          bgGradient="linear(to-tr, secondary.150, secondary.100)"
-          gap={10}
-        >
-          <LiquidStakingInfo asset="1 Month" icon="" roi="20%" vestingTerm="30 days" />
         </Card>
       </Flex>
     </Container>
