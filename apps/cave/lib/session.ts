ad66646418fa04620d9a59ffa8e76aaa9ffd469d
gregs <ukfaeku@gmail.com>
2022-03-14 11:12:41 -0300

    remove Iron session

--- apps/cave/lib/session.ts
+++ apps/cave/lib/session.ts
@@ -1,17 +1,17 @@
-import type { IronSessionOptions } from 'iron-session'
 import { SiweMessage } from 'siwe'
 import aes256 from 'aes256'
 import { serialize } from 'cookie'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { CookieSerializeOptions } from 'next/dist/server/web/types'
 
-export const sessionOptions: IronSessionOptions = {
-  password: process.env.SECRET_COOKIE_PASSWORD || ('H81HEAUISBNDKJAND1UNQWE8901HJDQNj' as string),
+export const sessionOptions = {
+  password: process.env.SECRET_COOKIE_PASSWORD,
   cookieName: 'session',
   cookieOptions: {
     secure: process.env.NODE_ENV === 'production',
     sameSite: 'strict',
     httpOnly: true,
-  },
+  } as CookieSerializeOptions,
 }
 
 type Session = {
@@ -19,9 +19,6 @@ type Session = {
   nonce: string
   hasura: { id: string; role: string }
 }
-
-export const decryptSession = (session): Session =>
-  JSON.parse(aes256.decrypt(sessionOptions.password, session))
 
 export const setSessionCookie = (req: NextApiRequest, res: NextApiResponse, cookie) => {
   const currentSession = getSessionCookie(req)
@@ -35,6 +32,9 @@ export const setSessionCookie = (req: NextApiRequest, res: NextApiResponse, cook
     serialize(sessionOptions.cookieName, encryptedCookie, sessionOptions.cookieOptions),
   )
 }
+
+export const decryptSession = (session): Session =>
+  JSON.parse(aes256.decrypt(sessionOptions.password, session))
 
 export const getSessionCookie = (req: NextApiRequest) => {
   if (req.cookies.session) return decryptSession(req.cookies.session)
