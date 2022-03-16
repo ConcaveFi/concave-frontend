828a5dd6f9870da6c498b13357eb865f7a98b883
gregs <ukfaeku@gmail.com>
2022-03-16 15:14:22 -0300

    Revert "Fix menu"

--- apps/cave/components/SideBar/SideBar.tsx
+++ apps/cave/components/SideBar/SideBar.tsx
@@ -106,22 +106,17 @@ interface MobileProps extends FlexProps {
 const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
   return (
     <Flex
-    // ml={{ base: 0, md: 60 }}
-    // px={{ base: 4, md: 24 }}
-    // height="20"
-    // alignItems="center"
-    // bg={useColorModeValue('white', 'gray.900')}
-    // borderBottomWidth="1px"
-    // borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
-    // justifyContent="flex-start"
-    // {...rest}
+      ml={{ base: 0, md: 60 }}
+      px={{ base: 4, md: 24 }}
+      height="20"
+      alignItems="center"
+      bg={useColorModeValue('white', 'gray.900')}
+      borderBottomWidth="1px"
+      borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
+      justifyContent="flex-start"
+      {...rest}
     >
-      <IconButton
-        variant="outline-none"
-        onClick={onOpen}
-        aria-label="open menu"
-        icon={<FiMenu />}
-      />
+      <IconButton variant="outline" onClick={onOpen} aria-label="open menu" icon={<FiMenu />} />
     </Flex>
   )
 }
