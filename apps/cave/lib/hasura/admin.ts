af75e02cc45f5c87b72f1fd5b9cb4aeaa18acc8a
rms rob <rr.rmsrob@gmail.com>
2022-03-16 20:57:09 +0100

    fix auth link dev/prod

--- apps/cave/lib/hasura/admin.ts
+++ apps/cave/lib/hasura/admin.ts
@@ -1,9 +1,11 @@
 import { gql, GraphQLClient } from 'graphql-request'
 
-const HasuraUrl =
-  process.env.NODE_ENV !== 'production'
-    ? 'http://localhost:8080/v1/graphql'
-    : (process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT as string)
+// const HasuraUrl =
+//   process.env.NODE_ENV !== 'production'
+//     ? 'http://localhost:8080/v1/graphql'
+//     : (process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT as string)
+
+const HasuraUrl = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT as string
 
 const admin = new GraphQLClient(HasuraUrl, {
   headers: { 'X-Hasura-Admin-Secret': process.env['NEXT_PUBLIC_GRAPHQL_ADMIN_SECRET'] },
