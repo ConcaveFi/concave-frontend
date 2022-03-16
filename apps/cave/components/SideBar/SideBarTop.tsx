575159630ad4a29beb5083160857c690659783ea
NUnicorn <unicornewborn@gmail.com>
2022-03-16 11:55:58 -0700

    Remove div

--- apps/cave/components/SideBar/SideBarTop.tsx
+++ apps/cave/components/SideBar/SideBarTop.tsx
@@ -7,45 +7,43 @@ import { MdOutlineDashboard } from 'react-icons/md'
 
 function SideBarTop() {
   return (
-    <div>
-      <Box
-        top="16px"
-        border-radius="16px"
-        shadow="down"
-        bgGradient="linear(to-tr, secondary.150, secondary.100)"
-        px={2}
-        py={10}
-        box-shadow="lg"
-        rounded="lg"
+    <Box
+      top="16px"
+      border-radius="16px"
+      shadow="down"
+      bgGradient="linear(to-tr, secondary.150, secondary.100)"
+      px={2}
+      py={10}
+      box-shadow="lg"
+      rounded="lg"
+    >
+      <Flex
+        alignItems="center"
+        justify="center"
+        my={4}
+        filter="drop-shadow(0px 0px 27px #81b3ff4f)"
       >
-        <Flex
-          alignItems="center"
-          justify="center"
-          my={4}
-          filter="drop-shadow(0px 0px 27px #81b3ff4f)"
-        >
-          <Image src="/assets/concave/logomark.svg" alt="concave logo" maxWidth="52px" />
-          <Image src="/assets/concave/logotype.svg" alt="concave logo" width="100px" ml={3} />
-        </Flex>
+        <Image src="/assets/concave/logomark.svg" alt="concave logo" maxWidth="52px" />
+        <Image src="/assets/concave/logotype.svg" alt="concave logo" width="100px" ml={3} />
+      </Flex>
 
-        <VStack gap="1" align="flex-end" mt={7}>
-          <ButtonLink
-            href="/treasury"
-            variant="primary.outline"
-            bgGradient="linear(to-tr, secondary.150, secondary.100)"
-            w="200px"
-            size="large"
-            borderRadius="2xl"
-          >
-            <HStack gap="2">
-              <MdOutlineDashboard fontSize="20px" />
-              <Text fontSize="lg">Dashboard</Text>
-            </HStack>
-          </ButtonLink>
-          <ConnectWallet />
-        </VStack>
-      </Box>
-    </div>
+      <VStack gap="1" align="flex-end" mt={7}>
+        <ButtonLink
+          href="/treasury"
+          variant="primary.outline"
+          bgGradient="linear(to-tr, secondary.150, secondary.100)"
+          w="200px"
+          size="large"
+          borderRadius="2xl"
+        >
+          <HStack gap="2">
+            <MdOutlineDashboard fontSize="20px" />
+            <Text fontSize="lg">Dashboard</Text>
+          </HStack>
+        </ButtonLink>
+        <ConnectWallet />
+      </VStack>
+    </Box>
   )
 }
 
