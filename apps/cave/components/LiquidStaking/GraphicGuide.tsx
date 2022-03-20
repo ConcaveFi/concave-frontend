8a98030d58d376dd4eb8fbbb7007b12b9f586033
NUnicorn <unicornewborn@gmail.com>
2022-03-20 11:06:41 -0700

    LiquidStaking assets

--- apps/cave/components/LiquidStaking/GraphicGuide.tsx
+++ apps/cave/components/LiquidStaking/GraphicGuide.tsx
@@ -1,7 +1,20 @@
+import { HStack, Stack, Text } from '@concave/ui'
+import { ChevronRightIcon } from '@concave/icons'
 import React from 'react'
 
 function GraphicGuide() {
-  return <div>GraphicGuide</div>
+  return (
+    <Stack>
+      <HStack>
+        <Text>CNV</Text>
+        <ChevronRightIcon />
+        <Text>Mint NFT</Text>
+        <ChevronRightIcon />
+        <Text>NFT Marketplace</Text>
+      </HStack>
+      <HStack></HStack>
+    </Stack>
+  )
 }
 
 export default GraphicGuide
