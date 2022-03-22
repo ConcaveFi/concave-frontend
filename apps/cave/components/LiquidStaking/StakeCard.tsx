510d105ef504366f2c7c2386ca5f10c80c8bd4bd
NUnicorn <unicornewborn@gmail.com>
2022-03-21 23:27:12 -0700

    Clean up

--- apps/cave/components/LiquidStaking/StakeCard.tsx
+++ apps/cave/components/LiquidStaking/StakeCard.tsx
@@ -1,14 +1,4 @@
-import {
-  Box,
-  Button,
-  Card,
-  Image,
-  Modal,
-  ModalHeader,
-  Stack,
-  Text,
-  useDisclosure,
-} from '@concave/ui'
+import { Box, Button, Card, Image, Modal, Stack, Text, useDisclosure } from '@concave/ui'
 import { ButtonLink } from 'components/ButtonLink'
 import { useState } from 'react'
 import Emissions from './StakeModal/Emissions'
@@ -74,7 +64,6 @@ function StakeCard(props) {
 
         <Button
           mt={5}
-          // href={`/${props.stakingLink}`}
           onClick={onOpen}
           fontWeight="medium"
           variant="primary.outline"
@@ -106,51 +95,6 @@ function StakeCard(props) {
             icon={props.icon}
             vapr={props.vapr}
           />
-          {/* <Box p={2}>
-            <Box
-              mx="auto"
-              pt={5}
-              pb={3}
-              w="30%"
-              h="full"
-              shadow="down"
-              borderRadius="full"
-              textAlign="center"
-              ml={-1}
-            >
-              <Text color="text.low" fontSize="sm">
-                Stake period
-              </Text>
-              <Text fontSize="lg" fontWeight="bold">
-                {props.period}
-              </Text>
-              <Image
-                mx="auto"
-                src={`/assets/liquidstaking/${props.icon}-logo.svg`}
-                alt="stake period logo"
-              />
-              <Text color="text.low" fontSize="sm">
-                {vaprText}
-              </Text>
-              <Text fontSize="lg" fontWeight="bold">
-                {props.vapr} %
-              </Text>
-              <Image
-                mx="auto"
-                src={`/assets/liquidstaking/modal-arrow-logo.svg`}
-                alt="arrow down logo"
-              />
-              <Box
-                shadow="down"
-                borderTopRadius="xl"
-                borderBottomRadius="full"
-                w="90%"
-                h="150px"
-                mx="auto"
-                filter="drop-shadow(0px 0px 27px #81b3ff4f)"
-              ></Box>
-            </Box>
-          </Box> */}
         </Modal>
       </Card>
     </div>
