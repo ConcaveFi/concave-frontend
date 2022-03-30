9a06f8f8405c4cd58c0dba4714667a170b0526ab
David Schrammel <schrammel@live.com>
2022-03-30 15:47:05 -0300

    feat(mock/wrapper): add mock price

--- apps/cave/lib/token.service.ts
+++ apps/cave/lib/token.service.ts
@@ -1,9 +1,11 @@
 import { CandleStickIntervalTypes } from 'components/CandleStickCard/useCandleStickChart'
+import { chain } from 'wagmi'
 import { coingeckoApi } from './coingecko.api'
 import { AvailableTokens, availableTokens } from './tokens'
 
 class TokenService {
   private tokens = Object.values(availableTokens)
+  constructor(private networkName: string = chain.mainnet.name) {}
   async listTokens(search: { label?: string }) {
     if (!search) {
       return this.tokens
@@ -23,7 +25,21 @@ class TokenService {
   }
 
   async getTokenPrice(symbol: AvailableTokens) {
-    const { coingecko } = availableTokens[symbol]
+    if (this.networkName === chain.ropsten.name) {
+      const values = {
+        DAI: 1,
+        MATIC: 1.68,
+        FRAX: 1,
+        Tether: 1.2,
+      }
+      return {
+        token: symbol,
+        currency: 'usd',
+        value: values[symbol],
+      }
+    }
+
+    const coingecko = symbol.toLowerCase()
     return Promise.resolve(
       coingeckoApi.tokenPrice({
         currency: 'usd',
@@ -52,4 +68,4 @@ const daysOptions = {
   '4H': '30',
   '1D': '365',
 } as const
-export const tokenService = new TokenService()
+export const tokenService = new TokenService(chain.ropsten.name)
