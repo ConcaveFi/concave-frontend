6a3df7853152bfc9874048dde7109541504c282c
NUnicorn <unicornewborn@gmail.com>
2022-03-31 00:22:38 -0700

    Create placeholder component

---
+++ apps/cave/components/Placeholder.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Placeholder() {
+  return <div>Placeholder</div>
+}
+
+export default Placeholder
--- apps/cave/pages/marketplace.tsx
+++ apps/cave/pages/marketplace.tsx
@@ -4,38 +4,13 @@ import { BigNumber, utils } from 'ethers'
 import { useCallback, useEffect, useState } from 'react'
 import { Box, Button, Flex, Grid, Heading } from '@concave/ui'
 import GcnvTitle from 'components/GcnvTitle'
+import Placeholder from 'components/Placeholder'
 
 function marketplace() {
   return (
     <Container maxW="container.lg">
-      <GcnvTitle
-        title="Marketplace"
-        description="Buy or Sell liquid staked positions or something something."
-      />
-
-      <Card
-        w="620px"
-        h="903px"
-        borderWidth={0}
-        borderRadius={16}
-        px={6}
-        py={10}
-        shadow="up"
-        bgGradient="linear(to-tr, secondary.150, secondary.100)"
-        gap={1}
-      >
-        <Card
-          w="570px"
-          h="80px"
-          borderWidth={0}
-          borderRadius={16}
-          px={6}
-          py={10}
-          shadow="up"
-          bgGradient="linear(to-tr, secondary.150, secondary.100)"
-          gap={1}
-        ></Card>
-      </Card>
+      <GcnvTitle title="Marketplace" description="" />
+      <Placeholder />
     </Container>
   )
 }
