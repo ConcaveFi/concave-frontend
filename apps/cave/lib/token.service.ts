8b7b278483fc377d0d2df19075acb8e68e7fe692
David Schrammel <schrammel@live.com>
2022-03-31 14:23:43 -0300

    fix_candlestick

--- apps/cave/lib/token.service.ts
+++ apps/cave/lib/token.service.ts
@@ -38,7 +38,7 @@ class TokenService {
     token: AvailableTokens
     interval: CandleStickIntervalTypes
   }) {
-    const coingecko = token
+    const coingecko = token.toLowerCase()
     const days = daysOptions[interval]
     return coingeckoApi.fetchCandleStickData({ id: coingecko, days })
   }
