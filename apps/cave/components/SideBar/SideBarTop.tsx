11a43e18bdbe3a70828783fc234908ed7c0527a3
NUnicorn <unicornewborn@gmail.com>
2022-03-31 03:29:44 -0700

    useBalance hook

--- apps/cave/components/SideBar/SideBarTop.tsx
+++ apps/cave/components/SideBar/SideBarTop.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Box, Flex, Text, Stack, HStack } from '@concave/ui'
 import { Image } from '@concave/ui'
 import { ConnectWallet } from 'components/ConnectWallet'
 import { ButtonLink } from 'components/ButtonLink'
 import { MdOutlineDashboard } from 'react-icons/md'
+import { useBalance } from 'wagmi'
 
 function SideBarTop() {
+  const [{ data, error, loading }, getBalance] = useBalance({
+    addressOrName: '0x9D588E7A18b1c7354066617e89326607988364A7',
+    // token: '0x7B731FFcf1b9C6E0868dA3F1312673A12Da28dc5', // INSERT CNV ADDRESS
+  })
+
+  useEffect(() => {
+    console.log(data)
+  }, [data])
+
   return (
-    <Box shadow="down" px={2} py={10} rounded="2xl">
+    <Box shadow="down" px={2} pt={10} pb={3} rounded="2xl">
       <Flex
         alignItems="center"
         justify="center"
@@ -28,7 +38,17 @@ function SideBarTop() {
         >
           Dashboard
         </ButtonLink>
-        <ConnectWallet />
+        <Box shadow="down" w="full" p={2} rounded="2xl">
+          <ConnectWallet />
+          <Flex justifyContent="space-between" p={2} mt={2}>
+            <Text color="text.low" fontWeight="bold" fontSize="md" lineHeight="100%">
+              Your CNV Balance
+            </Text>
+            <Text color="text.low" fontWeight="bold" fontSize="md" lineHeight="100%">
+              0 CNV
+            </Text>
+          </Flex>
+        </Box>
       </Stack>
     </Box>
   )
