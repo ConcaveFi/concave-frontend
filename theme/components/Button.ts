6b899e3c0499ed8033005308b98bea693db3cc8e
gregs <ukfaeku@gmail.com>
2022-04-04 08:39:47 -0300

    remove unnecessary files added on merge with main

--- theme/components/Button.ts
+++
@@ -1,9 +0,0 @@
-import { ComponentStyleConfig } from '@chakra-ui/react'
-import { mode } from '@chakra-ui/theme-tools'
-
-export const Button: ComponentStyleConfig = {
-  // baseStyle: {
-  //   fontWeight: 'bold',
-  //   borderRadius: '16px',
-  // },
-}
