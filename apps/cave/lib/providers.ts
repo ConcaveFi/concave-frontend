deafd331da566809347e23d65a28d097926cffe6
gregs <ukfaeku@gmail.com>
2022-04-04 12:00:44 -0300

    update provider

--- apps/cave/lib/providers.ts
+++ apps/cave/lib/providers.ts
@@ -6,22 +6,15 @@ const etherscan = process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY as string
 export const infuraId = process.env.NEXT_PUBLIC_INFURA_ID as string
 const concaveKey = process.env.NEXT_PUBLIC_CONCAVE_RPC_KEY
 
-export const concaveRPC = 'https://eth.concave.lol/'
+export const concaveRPC = `https://api.concave.lol/node/${concaveKey}`
 
-// export const concaveProvider = (chainid: number) => {
-//   // const alchemyapi_1 = new providers.JsonRpcProvider(
-//   //   `https://eth-mainnet.alchemyapi.io/v2/${alchemy}`, //${alchemy}`,
-//   // )
-//   const infura_1 = new providers.JsonRpcProvider(`https://ropsten.infura.io/v3/${infuraId}`)
-//   return infura_1
-// }
-
-export const concaveProvider2 = (chainId: number) =>
+export const concaveProvider = (chainId: number) =>
   new multicallProvider.MulticallProvider(
     new providers.FallbackProvider([
+      { provider: new providers.JsonRpcProvider(concaveRPC, chainId), priority: 0 },
       providers.getDefaultProvider(chainId, { alchemy, etherscan, infuraId }),
     ]),
   )
-export const concaveProvider = concaveProvider2
+
 export const concaveWSProvider = (chainId: number) =>
   new providers.InfuraWebSocketProvider(chainId, infuraId)
