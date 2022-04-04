fa400334bde477f1b33211e1a5733faf0649644e
casuwu <erickdelgado@protonmail.com>
2022-04-04 16:41:39 -0400

    disable max, bugs causing when max click used

--- apps/cave/components/Swap/TokenInput.tsx
+++ apps/cave/components/Swap/TokenInput.tsx
@@ -17,7 +17,8 @@ const Balance = ({ value, onClick }) => (
     fontSize="xs"
     ml="auto"
     onClick={onClick}
-    rightIcon={!!onClick && <Text textColor="#2E97E2">Max</Text>}
+    //  Contracts are breaking if we click max first commenting out for the moment
+    // rightIcon={!!onClick && <Text textColor="#2E97E2">Max</Text>}
     leftIcon={<Text>Balance:</Text>}
     iconSpacing={1}
   >
