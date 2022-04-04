5362c508645c73833aa20951fab84546f211a21c
gregs <ukfaeku@gmail.com>
2022-04-04 08:39:47 -0300

    remove unnecessary files added on merge with main

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-const nextJest = require('next/jest')
-
-const createJestConfig = nextJest({ dir: './' })
-
-const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-}
-
-module.exports = createJestConfig(customJestConfig)
