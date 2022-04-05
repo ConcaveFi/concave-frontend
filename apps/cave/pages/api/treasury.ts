f97b158434e9814ba0838a3625d8d8fb233664f5
gregs <ukfaeku@gmail.com>
2022-04-05 10:50:51 -0300

    swr trasury endpoint

--- apps/cave/pages/api/treasury.ts
+++ apps/cave/pages/api/treasury.ts
@@ -5,6 +5,7 @@ const TREASURY_ADDRESS = '0x226e7AF139a0F34c6771DeB252F9988876ac1Ced'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const treasury = await fetchPortfolio(TREASURY_ADDRESS)
+  res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate')
   res.status(200).json(JSON.stringify(treasury, null, 2))
 }
 
