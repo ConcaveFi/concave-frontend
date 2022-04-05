5bf68ac7763534e4b6abfbb71723580043338b98
gregs <ukfaeku@gmail.com>
2022-04-05 10:08:39 -0300

    /api/treasury

---
+++ apps/cave/pages/api/treasury.ts
@@ -0,0 +1,11 @@
+import { fetchPortfolio } from 'lib/debank'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const TREASURY_ADDRESS = '0x226e7AF139a0F34c6771DeB252F9988876ac1Ced'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const treasury = await fetchPortfolio(TREASURY_ADDRESS)
+  res.status(200).json(JSON.stringify(treasury, null, 2))
+}
+
+export default handler
