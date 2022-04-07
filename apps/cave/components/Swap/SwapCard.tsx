4c9c196985e4f83b7c09ff4347fa09015b825a70
casuwu <erickdelgado@protonmail.com>
2022-04-07 15:07:56 -0400

    ..

--- apps/cave/components/Swap/SwapCard.tsx
+++ apps/cave/components/Swap/SwapCard.tsx
@@ -122,107 +122,121 @@ export function SwapCard() {
   }, [swapReceipt.loading])
   return (
     <>
-    <Flex direction="row" justify="center" align="center" h="100%" gap={10}>
-          <CandleStickCard
-        from={swapingIn.currency}
-        to={swapingOut.currency}
-        variant="secondary"
-        gap={2}
-        p={6}
-        h={['100%', 470, 400]}
-        w={['100%', '100%', 500, 567, 567]}
-        align="stretch"
-      />
-      <Card p={6} gap={2} variant="primary" h="fit-content" shadow="Block Up" w="100%" maxW="420px">
-        <TokenInput
-          value={swapingIn.amount}
-          currency={swapingIn.currency}
-          stable={swapingIn.stable}
-          balance={swapingIn.balance}
-          onChangeValue={(v) => {
-            setInOrOut(true)
-            const numberValue = v.replace('-', '')
-            numberValue && setAmountIn(v)
-          }}
-          onChangeCurrency={setCurrencyIn}
-          // bug: fails to execute tx when clicked before hitting swap
-          // onClickMaxBalance={() => {
-          //   if (swapingIn.currency.equals(nativeCurrency)) setAmountIn(+swapingIn.balance * 0.8)
-          //   else setAmountIn(swapingIn.balance)
-          // }}
+      <Flex
+        direction={{ sm: 'column-reverse', md: 'row' }}
+        justify="center"
+        align="center"
+        h="100%"
+        gap={10}
+      >
+        <CandleStickCard
+          from={swapingIn.currency}
+          to={swapingOut.currency}
+          variant="secondary"
+          gap={2}
+          p={6}
+          h={['100%', 470, 400]}
+          w={['100%', '100%', 500, 567, 567]}
+          align="stretch"
         />
-        <SwitchCurrencies onClick={switchCurrencies} />
-        <TokenInput
-          disabled={true}
-          value={swapingOut.amount}
-          currency={swapingOut.currency}
-          stable={swapingOut.stable}
-          balance={swapingOut.balance}
-          onChangeValue={(v) => {
-            setInOrOut(false)
-            !isNaN(+v) && setAmountOut(v)
-          }}
-          onChangeCurrency={setCurrencyOut}
-        />
+        <Card
+          p={6}
+          gap={2}
+          variant="primary"
+          h="fit-content"
+          shadow="Block Up"
+          w="100%"
+          maxW="420px"
+        >
+          <TokenInput
+            value={swapingIn.amount}
+            currency={swapingIn.currency}
+            stable={swapingIn.stable}
+            balance={swapingIn.balance}
+            onChangeValue={(v) => {
+              setInOrOut(true)
+              const numberValue = v.replace('-', '')
+              numberValue && setAmountIn(v)
+            }}
+            onChangeCurrency={setCurrencyIn}
+            // bug: fails to execute tx when clicked before hitting swap
+            // onClickMaxBalance={() => {
+            //   if (swapingIn.currency.equals(nativeCurrency)) setAmountIn(+swapingIn.balance * 0.8)
+            //   else setAmountIn(swapingIn.balance)
+            // }}
+          />
+          <SwitchCurrencies onClick={switchCurrencies} />
+          <TokenInput
+            disabled={true}
+            value={swapingOut.amount}
+            currency={swapingOut.currency}
+            stable={swapingOut.stable}
+            balance={swapingOut.balance}
+            onChangeValue={(v) => {
+              setInOrOut(false)
+              !isNaN(+v) && setAmountOut(v)
+            }}
+            onChangeCurrency={setCurrencyOut}
+          />
 
-        <HStack align="center" justify="end" py={5}>
-          {isFetchingPairs ? (
-            <LoadingBestTradeIndicator />
-          ) : isErrored ? (
-            <PairsError />
-          ) : (
-            swapingOut.relativePrice && (
-              <Flex flexWrap="wrap" fontSize="xs" fontWeight="medium" mr="auto">
-                <Text>
-                  1 {swapingOut.currency.symbol} = {swapingOut.relativePrice}
-                  {swapingIn.currency.symbol}
-                </Text>
-                {swapingOut.stable && (
-                  <Text ml={1} textColor="text.low">
-                    (${swapingOut.stable})
+          <HStack align="center" justify="end" py={5}>
+            {isFetchingPairs ? (
+              <LoadingBestTradeIndicator />
+            ) : isErrored ? (
+              <PairsError />
+            ) : (
+              swapingOut.relativePrice && (
+                <Flex flexWrap="wrap" fontSize="xs" fontWeight="medium" mr="auto">
+                  <Text>
+                    1 {swapingOut.currency.symbol} = {swapingOut.relativePrice}
+                    {swapingIn.currency.symbol}
                   </Text>
-                )}
-              </Flex>
-            )
+                  {swapingOut.stable && (
+                    <Text ml={1} textColor="text.low">
+                      (${swapingOut.stable})
+                    </Text>
+                  )}
+                </Flex>
+              )
+            )}
+            <GasPrice />
+            <Settings onClose={setSettings} />
+          </HStack>
+
+          {needsApproval && (
+            <Button
+              isLoading={isApproving}
+              variant="primary"
+              size="large"
+              isFullWidth
+              onClick={() => approve()}
+            >
+              Approve {swapingIn.currency.symbol}
+            </Button>
           )}
-          <GasPrice />
-          <Settings onClose={setSettings} />
-        </HStack>
 
-        {needsApproval && (
-          <Button
-            isLoading={isApproving}
-            variant="primary"
-            size="large"
-            isFullWidth
-            onClick={() => approve()}
-          >
-            Approve {swapingIn.currency.symbol}
-          </Button>
-        )}
-
-        {!isConnected && false ? (
-          // <Button variant="primary" size="large" disabled onClick={connectWithModal}>
-          //   Connect Wallet
-          // </Button>
-          <></>
-        ) : (
-          <Button
-            isDisabled={
-              !isTradeReady ||
-              needsApproval ||
-              !isConnected ||
-              +swapingIn.balance < +swapingIn.amount
-            }
-            variant="primary"
-            size="large"
-            isFullWidth
-            onClick={confirmModal.onOpen}
-          >
-            {+swapingIn.balance < +swapingIn.amount ? 'Insufficient Funds' : 'Swap'}
-          </Button>
-        )}
-      </Card>
+          {!isConnected && false ? (
+            // <Button variant="primary" size="large" disabled onClick={connectWithModal}>
+            //   Connect Wallet
+            // </Button>
+            <></>
+          ) : (
+            <Button
+              isDisabled={
+                !isTradeReady ||
+                needsApproval ||
+                !isConnected ||
+                +swapingIn.balance < +swapingIn.amount
+              }
+              variant="primary"
+              size="large"
+              isFullWidth
+              onClick={confirmModal.onOpen}
+            >
+              {+swapingIn.balance < +swapingIn.amount ? 'Insufficient Funds' : 'Swap'}
+            </Button>
+          )}
+        </Card>
       </Flex>
       <ConfirmSwapModal
         tradeInfo={tradeInfo}
@@ -254,7 +268,6 @@ export function SwapCard() {
           receiptModal.onClose()
         }}
       />
-
     </>
   )
 }
