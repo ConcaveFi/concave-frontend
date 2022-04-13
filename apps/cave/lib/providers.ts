d4a42a98fcc7693e2bd1efbe896b4648150f4b1e
gregs <ukfaeku@gmail.com>
2022-04-12 22:14:42 -0300

    fix providers

--- apps/cave/lib/providers.ts
+++ apps/cave/lib/providers.ts
@@ -11,12 +11,15 @@ export const concaveRPC = `https://api.concave.lol/node/${concaveKey}`
 export const concaveProvider = (chainId: number) =>
   new multicallProvider.MulticallProvider(
     new providers.FallbackProvider([
-      { provider: new providers.JsonRpcProvider(concaveRPC, chainId), priority: 0 },
-      { provider: new providers.AlchemyProvider(chainId, alchemy), priority: 1 },
-      { provider: new providers.InfuraProvider(chainId, infuraId), priority: 1 },
+      // { provider: new providers.JsonRpcProvider(concaveRPC, chainId), priority: 1 },
+      { provider: new providers.AlchemyProvider(chainId, alchemy), priority: 2 },
+      { provider: new providers.InfuraProvider(chainId, infuraId), priority: 2 },
       // providers.getDefaultProvider(chainId, { alchemy, etherscan, infuraId }),
     ]),
   )
+
+// export const concaveProvider = (chainId: number) =>
+//   new providers.JsonRpcProvider(concaveRPC, chainId)
 
 export const concaveWSProvider = (chainId: number) =>
   new providers.InfuraWebSocketProvider(chainId, infuraId)
