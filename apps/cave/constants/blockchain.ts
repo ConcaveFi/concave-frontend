a62a5c880ee87cf7a764eabe2ea3eb85f92abd0c
gregs <ukfaeku@gmail.com>
2022-04-13 16:41:37 -0300

    todo: implement websocket watchers

--- apps/cave/constants/blockchain.ts
+++ apps/cave/constants/blockchain.ts
@@ -2,6 +2,7 @@ import { ChainId } from 'gemswap-sdk'
 import ms from 'ms'
 
 // used to refetch data on every block
+// TODO: replace for websockets
 export const AVERAGE_BLOCK_TIME = {
   [ChainId.ETHEREUM]: ms('14s'),
 }
