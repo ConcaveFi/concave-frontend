7549a1c40ca3f97f08fe7f181a82b30b08698cef
gregs <ukfaeku@gmail.com>
2022-04-14 03:55:20 -0300

    useSwapTransaction

---
+++ apps/cave/components/Swap/hooks/useSwapTransaction.ts
@@ -0,0 +1,75 @@
+import { useMemo } from 'react'
+import { SwapSettings } from '../Settings'
+import { RouterABI, ROUTER_ADDRESS, Router, Currency, TradeType, Trade } from 'gemswap-sdk'
+import { Contract } from 'ethers'
+import { useCurrentSupportedNetworkId } from './useCurrentSupportedNetworkId'
+import { useContract, useSigner } from 'wagmi'
+import { useState } from 'react'
+
+export const useSwapTransaction = (
+  trade: Trade<Currency, Currency, TradeType>,
+  settings: SwapSettings,
+  recipient: string,
+) => {
+  const networkId = useCurrentSupportedNetworkId()
+  const [{ data: signer }] = useSigner()
+  const routerContract = useContract<Contract>({
+    addressOrName: ROUTER_ADDRESS[networkId],
+    contractInterface: RouterABI,
+    signerOrProvider: signer,
+  })
+  const txTrade = useMemo(
+    () => trade && new Trade(trade.route, trade.inputAmount, TradeType.EXACT_INPUT),
+    [trade],
+  )
+
+  const callParameters = useMemo(() => {
+    if (!txTrade || !recipient) return
+    return Router.swapCallParameters(txTrade, {
+      allowedSlippage: settings.slippageTolerance.percent,
+      ttl: +settings.deadline * 60,
+      feeOnTransfer: txTrade.tradeType === TradeType.EXACT_INPUT,
+      recipient,
+    })
+  }, [recipient, settings.slippageTolerance, settings.deadline, txTrade])
+
+  // const { data: estimatedGasFee } = useQuery(
+  //   ['swap estimated gas fee', callParameters],
+  //   () => {
+  //     const { methodName, args, value } = callParameters
+  //     return routerContract.estimateGas[methodName](...args, { value }).then((estimatedGasFee) =>
+  //       formatUnits(estimatedGasFee, 'wei'),
+  //     )
+  //   },
+  //   { enabled: !!callParameters && !!routerContract.signer, retry: false },
+  // )
+
+  const [state, setState] = useState({
+    isWaitingForConfirmation: false,
+    isError: false,
+    isTransactionSent: false,
+    data: undefined,
+    trade,
+  })
+  const submit = async () => {
+    setState((s) => ({
+      ...s,
+      trade: txTrade, // locks the tx trade
+      isWaitingForConfirmation: true,
+    }))
+    const { methodName, args, value } = callParameters
+    try {
+      const tx = await routerContract[methodName](...args, { value })
+      setState((s) => ({
+        ...s,
+        isTransactionSent: true,
+        data: tx,
+        isWaitingForConfirmation: false,
+      }))
+    } catch (error) {
+      setState((s) => ({ ...s, isError: true, error, isWaitingForConfirmation: false }))
+    }
+  }
+
+  return { submit, ...state }
+}
