f70a79e9f2c07ecefc17f5987999f27d0d93a9e1
shanghigh <95733088+shanghigh@users.noreply.github.com>
2022-04-24 12:36:58 -0400

    prettier

--- apps/cave/components/Marketplace/MarketplaceActivityCard.tsx
+++ apps/cave/components/Marketplace/MarketplaceActivityCard.tsx
@@ -6,36 +6,35 @@ function MarketplaceActivityCard(props) {
 
 
     return (
-        <div>
+<div>
 <Card
-          p={7}
-          gap={2}
-        //   variant=""
-        //   h="fit-content"
-        h="642px"
-          shadow="Block Up"
-        //   w="100%"
-        w="300px"
-          maxW="420px"
-        >
+  p={7}
+  gap={2}
+  //   variant=""
+  //   h="fit-content"
+  h="642px"
+  shadow="Block Up"
+  //   w="100%"
+  w="300px"
+  maxW="420px"
+>
     <Flex direction="row" gap={6} justify="center" mt={2}>
-    <Box mx="auto" py={5} w="90px" h="37px" shadow="down" borderRadius="16px">
+      <Box mx="auto" py={5} w="90px" h="37px" shadow="down" borderRadius="16px">
         <Text fontSize="s" color="white" fontWeight="bold">All</Text>
-    </Box> 
-    <Box mx="auto" py={5} w="90px" h="37px" shadow="down" borderRadius="16px">
-    <Text fontSize="s" color="white" fontWeight="bold">Listing</Text>
-    </Box>
-    <Box mx="auto" py={5} w="90px" h="37px" shadow="down" borderRadius="16px">
-    <Text fontSize="s" color="white" fontWeight="bold">Sale</Text>
-    </Box>
+      </Box> 
+      <Box mx="auto" py={5} w="90px" h="37px" shadow="down" borderRadius="16px">
+        <Text fontSize="s" color="white" fontWeight="bold">Listing</Text>
+      </Box>
+      <Box mx="auto" py={5} w="90px" h="37px" shadow="down" borderRadius="16px">
+        <Text fontSize="s" color="white" fontWeight="bold">Sale</Text>
+      </Box>
     </Flex>
-
     <Flex>
-    <Box mx="auto" py={5} w="250px" h="100px" shadow="up" borderRadius="16px">
-    </Box>   
+      <Box mx="auto" py={5} w="250px" h="100px" shadow="up" borderRadius="16px">
+      </Box>   
     </Flex>
-        </Card>
-        </div>
+</Card>
+</div>
     )  
 }
 
