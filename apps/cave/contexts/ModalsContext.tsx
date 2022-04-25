bc370a80d06996c7903ade20b75022adf704550b
gregs <ukfaeku@gmail.com>
2022-04-25 09:06:11 -0300

    create testnet faucet

--- apps/cave/contexts/ModalsContext.tsx
+++ apps/cave/contexts/ModalsContext.tsx
@@ -1,6 +1,7 @@
 import { useDisclosure } from '@concave/ui'
 import { ConnectWalletModal } from 'components/ConnectWallet'
 import { DevelopGateway } from 'components/DevelopGateway'
+import { TestTokensMinterModal } from 'components/TestTokensMinterModal'
 import { UnsupportedNetworkModal } from 'components/UnsupportedNetworkModal'
 import React, { createContext, useContext } from 'react'
 
@@ -19,6 +20,7 @@ export const ModalsProvider: React.FC<React.PropsWithChildren<unknown>> = ({ chi
 
   return (
     <ModalsContext.Provider value={{ connectModal }}>
+      <TestTokensMinterModal />
       <DevelopGateway />
       <UnsupportedNetworkModal />
       <ConnectWalletModal isOpen={connectModal.isOpen} onClose={connectModal.onClose} />
