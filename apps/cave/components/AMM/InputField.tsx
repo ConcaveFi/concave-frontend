b2fe38a46e43b4b92710399fb49d1dceb5efd83c
gregs <ukfaeku@gmail.com>
2022-04-25 05:45:48 -0300

    useFiatPrice -> useFiatValue

--- apps/cave/components/AMM/InputField.tsx
+++ apps/cave/components/AMM/InputField.tsx
@@ -2,13 +2,14 @@ import { HStack, Text } from '@concave/ui'
 import { Currency, CurrencyAmount } from 'gemswap-sdk'
 import { useCurrencyBalance } from 'hooks/useCurrencyBalance'
 import { Balance } from './Balance'
-import { useFiatPrice } from './hooks/useFiatPrice'
+import { useFiatValue } from './hooks/useFiatPrice'
 import { TokenInput } from './TokenInput'
+import { parseInputAmount } from './utils/parseInputAmount'
 
 type InputFieldProps = {
   currencyIn: Currency
   currencyAmountIn: CurrencyAmount<Currency>
-  updateInputValue: (value: string) => void
+  updateInputValue: (value: CurrencyAmount<Currency>) => void
   updateCurrencyIn: (currency: Currency) => void
 }
 
@@ -18,25 +19,24 @@ export const InputField = ({
   updateInputValue,
   updateCurrencyIn,
 }: InputFieldProps) => {
-  const inputFiat = useFiatPrice(currencyIn)
-  const inputFiatValue = currencyAmountIn && inputFiat.price?.quote(currencyAmountIn)
+  const inputFiat = useFiatValue(currencyAmountIn)
   const balance = useCurrencyBalance(currencyIn)
 
   return (
     <TokenInput
       currency={currencyIn}
       currencyAmount={currencyAmountIn}
-      onChangeValue={updateInputValue}
+      onChangeAmount={updateInputValue}
       onSelectCurrency={updateCurrencyIn}
     >
       <HStack justify="space-between" align="end" textColor="text.low" w="full">
         <Text isTruncated fontWeight="bold" fontSize="sm" mr={2}>
-          {!!inputFiatValue && `$${inputFiatValue.toFixed(2, { groupSeparator: ',' })}`}
+          {!!inputFiat.value && `$${inputFiat.value.toFixed(2, { groupSeparator: ',' })}`}
         </Text>
         {balance.isSuccess && (
           <Balance
             value={balance.data.formatted}
-            onClick={() => updateInputValue(balance.data.formatted)}
+            onClick={() => updateInputValue(parseInputAmount(balance.data.formatted, currencyIn))}
           />
         )}
       </HStack>
