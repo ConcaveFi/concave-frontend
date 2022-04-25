8abc604f2f430a31cfcb756de61564d57869cde7
rms rob <rr.rmsrob@gmail.com>
2022-04-25 16:11:39 +0200

    ga state

--- apps/cave/pages/_app.tsx
+++ apps/cave/pages/_app.tsx
@@ -1,10 +1,13 @@
+import { useEffect } from 'react'
+import { NextPage } from 'next'
+import { useRouter } from 'next/router'
 import type { AppProps } from 'next/app'
 import { ConcaveFonts } from '@concave/ui'
 import { Styles } from '@chakra-ui/theme-tools'
-import { NextPage } from 'next'
 import { DefaultLayout } from 'components/Layout'
 import { AppProviders } from 'contexts'
 import { MetaHead, MetaProps } from 'components/MetaHead'
+import * as gtag from '../lib/analytics'
 
 const globalStyles: Styles = {
   global: {
@@ -34,6 +37,22 @@ type AppPropsWithLayout = AppProps & {
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const Layout = Component.Layout || DefaultLayout
+  const router = useRouter()
+  const isProduction = process.env.NODE_ENV === 'production'
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      if (isProduction) gtag.trackPageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router.events])
+
   return (
     <AppProviders globalStyles={globalStyles} cookies={pageProps?.cookies}>
       <ConcaveFonts />
