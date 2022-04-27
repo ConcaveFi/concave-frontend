9ea986d5b554d1bd6adb672d36a047d62f82c3fc
Diogo <dmendoncacosta03@gmail.com>
2022-04-27 20:56:43 -0300

    dashboard updates

--- apps/cave/components/Dashboard/UserPositionCard.tsx
+++ apps/cave/components/Dashboard/UserPositionCard.tsx
@@ -10,32 +10,184 @@ const UserPositionCard = (props) => {
   const [active, setActive] = useState(false)
   return (
     <Box
+      pos={'relative'}
+      maxHeight="320px"
+      borderRadius={'2xl'}
+      width="690px"
+      height={'320px'}
+      shadow="up"
+      maxWidth={'690px'}
+      mx={4}
+      my={8}
+    >
+      <Flex
+        position={'absolute'}
+        height="full"
+        width={'full'}
+        bg="linear-gradient(239.18deg, #19394C 27.18%, #0A161F 96.11%)"
+        rounded={'2xl'}
+        shadow="down"
+        justifyContent={'end'}
+      >
+        <Flex width={'228px'} height={'300px'} rounded={'2xl'}>
+          <Flex
+            flex={1}
+            direction="column"
+            justifyContent="center"
+            maxHeight={90}
+            alignItems="center"
+            textAlign={'start'}
+            ml={'70px'}
+            fontSize={'15px'}
+            fontWeight={'700'}
+          >
+            <Text width={'full'} pl={6}>
+              Your Staking Retaking
+            </Text>
+          </Flex>
+        </Flex>
+      </Flex>
+      <Box
+        pos={'relative'}
+        height={'full'}
+        rounded="2xl"
+        maxWidth={'500'}
+        background="linear-gradient(265.73deg, #274C63 0%, #182F3E 100%)"
+      >
+        <NftPositionViewer active={false} />
+        <RedeemCardViewer />
+        <ListCardViewer />
+      </Box>
+    </Box>
+  )
+}
+export default UserPositionCard
+
+const RedeemCardViewer = () => {
+  return (
+    <Flex height={90} direction="row" gap={4} alignItems="center" justify="center" m={2}>
+      <Flex grow={1} direction={'column'} textAlign={'start'} ml="2">
+        <Text color="text.low" fontSize="sm">
+          Current Value:
+        </Text>
+        <Text fontSize="md" fontWeight="bold">
+          612.42 CNV
+        </Text>
+      </Flex>
+      <Flex grow={1} direction={'column'} textAlign={'start'} ml="1">
+        <Text color="text.low" fontSize="sm">
+          Gained:
+        </Text>
+        <Text fontSize="md" fontWeight="bold">
+          12.42 CNV
+        </Text>
+      </Flex>
+      <Flex grow={1} direction={'column'} textAlign={'start'} ml="1">
+        <Text color="text.low" fontSize="sm">
+          Initial:
+        </Text>
+        <Text fontSize="md" fontWeight="bold">
+          600 CNV
+        </Text>
+      </Flex>
+      <Flex grow={1} direction={'column'} textAlign={'start'} ml="1">
+        <Button
+          mt={5}
+          //   onClick={'s'}
+          fontWeight="bold"
+          fontSize="md"
+          variant="primary"
+          //   bgGradient="linear(90deg, #72639B 0%, #44B9DE 100%)"
+          w="160px"
+          h="40px"
+          size="large"
+          mx="auto"
+        >
+          Redeem
+        </Button>
+      </Flex>
+    </Flex>
+  )
+}
+
+const ListCardViewer = () => {
+  return (
+    <Box
       pos="relative"
-      overflowY={'auto'}
-      maxHeight={'300px'}
+      boxSizing="border-box"
+      mx={2}
+      height={'120px'}
       borderRadius="16px"
-      w="720px"
-      h="285px"
       mt={1}
       cursor="pointer"
       css={{
-        background:
-          'url(Rectangle 110 (00000).jpg), linear-gradient(265.73deg, #274C63 0%, #182F3E 100%)',
-        boxShadow: !active
-          ? `0px 5px 14px rgba(0, 0, 0, 0.47),
-            4px -7px 15px rgba(174, 177, 255, 0.13),
-            inset -1px 1px 2px rgba(128, 186, 255, 0.24)`
-          : 'only-test',
+        background: 'rgba(113, 113, 113, 0.01)',
+        boxShadow:
+          'inset 0px -5px 10px rgba(134, 175, 255, 0.05), inset -9px 12px 24px rgba(13, 17, 23, 0.4)',
       }}
     >
-        <Box pos="relative" w="570px" h="280px">
+      <Flex justify="left">
+        <Text pl="6" mt={2} pt="3" color="text.low" fontSize="lg" as="b">
+          Your Marketplace Listing
+        </Text>
+      </Flex>
+      <Flex direction="row" alignItems="center" justify="center">
+        <Flex flex={1} direction={'column'} textAlign={'start'} ml="2">
+          <Text color="text.low" fontSize="sm">
+            List Price:
+          </Text>
+          <Text fontSize="md" fontWeight="bold">
+            600 CNV
+          </Text>
+        </Flex>
+        <Flex flex={1} direction={'column'} textAlign={'start'} ml="2">
+          <Text color="text.low" fontSize="sm">
+            Discount:
+          </Text>
+          <Text fontSize="md" fontWeight="bold">
+            2.4%
+          </Text>
+        </Flex>
+        <Flex flex={1} direction={'column'} textAlign={'start'} ml="2">
+          <Text color="text.low" fontSize="sm">
+            Expiration Date:
+          </Text>
+          <Text fontSize="md" fontWeight="bold">
+            14.11.22
+          </Text>
+        </Flex>
+        <Flex flex={1} direction={'column'} textAlign={'start'} ml="2">
+          <Button
+            mt={5}
+            //   onClick={'s'}
+            fontWeight="bold"
+            fontSize="md"
+            variant="primary.outline"
+            //   bgGradient="linear(90deg, #72639B 0%, #44B9DE 100%)"
+            w="160px"
+            h="40px"
+            size="large"
+            mx="auto"
+          >
+            Unlist
+          </Button>
+        </Flex>
+      </Flex>
+    </Box>
+  )
+}
+interface NftPositionViewerProps {
+  active: boolean
+}
 
-          <Box
+const NftPositionViewer = (props: NftPositionViewerProps) => {
+  const { active } = props
+  return (
+    <Box
       pos="relative"
       overflowY={'auto'}
-      maxHeight={'500px'}
+      maxHeight={'100px'}
       borderRadius="16px"
-      mt={1}
       cursor="pointer"
       css={{
         background:
@@ -105,113 +257,30 @@ const UserPositionCard = (props) => {
         </Flex>
       </Flex>
     </Box>
-    <Flex direction="row" gap={4} alignItems="center" justify="center" m={2}>
-        <Flex flex={1} direction={'column'} textAlign={'start'} ml="2">
-            <Text color="text.low" fontSize="sm">
-                Current Value:
-            </Text>
-            <Text fontSize="md" fontWeight="bold">
-                612.42 CNV
-            </Text>
-        </Flex>
-        <Flex flex={1} direction={'column'} textAlign={'start'} ml="2">
-            <Text color="text.low" fontSize="sm">
-                Gained:
-            </Text>
-            <Text fontSize="md" fontWeight="bold">
-                12.42 CNV
-            </Text>
-        </Flex>
-        <Flex flex={1} direction={'column'} textAlign={'start'} ml="2">
-            <Text color="text.low" fontSize="sm">
-                Initial:
-            </Text>
-            <Text fontSize="md" fontWeight="bold">
-                600 CNV
-            </Text>
-        </Flex>
-        <Flex flex={1} direction={'column'} textAlign={'start'} ml="2">
-        <Button
-          mt={5}
-        //   onClick={'s'}
-          fontWeight="bold"
-          fontSize="md"
-          variant="primary"
-        //   bgGradient="linear(90deg, #72639B 0%, #44B9DE 100%)"
-          w="160px"
-          h="40px"
-          size="large"
-          mx="auto"
-        >
-          Redeem
-        </Button>
-        </Flex>
-        </Flex>
-    <Box
-      pos="relative"
-      overflowY={'auto'}
-      maxHeight={'500px'}
-      borderRadius="16px"
-      mt={1}
-      cursor="pointer"
-      css={{
-            background: 'rgba(113, 113, 113, 0.01)',
-        boxShadow:
-        'inset 0px -5px 10px rgba(134, 175, 255, 0.05), inset -9px 12px 24px rgba(13, 17, 23, 0.4)',
-      }}
-    >
-    <Flex justify="left">
-        <Text color="text.low" fontSize="lg" as='b'>
-            Your Marketplace Listing
-        </Text>
-    </Flex>
-        <Flex direction="row" gap={4} alignItems="center" justify="center" m={2}>
-        <Flex flex={1} direction={'column'} textAlign={'start'} ml="2">
-            <Text color="text.low" fontSize="sm">
-                List Price:
-            </Text>
-            <Text fontSize="md" fontWeight="bold">
-                600 CNV
-            </Text>
-        </Flex>
-        <Flex flex={1} direction={'column'} textAlign={'start'} ml="2">
-            <Text color="text.low" fontSize="sm">
-                Discount:
-            </Text>
-            <Text fontSize="md" fontWeight="bold">
-                2.4%
-            </Text>
-        </Flex>
-        <Flex flex={1} direction={'column'} textAlign={'start'} ml="2">
-            <Text color="text.low" fontSize="sm">
-                Expiration Date:
-            </Text>
-            <Text fontSize="md" fontWeight="bold">
-                14.11.22
-            </Text>
-        </Flex>
-        <Flex flex={1} direction={'column'} textAlign={'start'} ml="2">
-        <Button
-          mt={5}
-        //   onClick={'s'}
-          fontWeight="bold"
-          fontSize="md"
-          variant="primary.outline"
-        //   bgGradient="linear(90deg, #72639B 0%, #44B9DE 100%)"
-          w="160px"
-          h="40px"
-          size="large"
-          mx="auto"
-        >
-          Unlist
-        </Button>
-        </Flex>
-        </Flex>
-    </Box>
-    </Box>
-
-
-    </Box>
   )
 }
-export default UserPositionCard
+// <Box
+//   pos="relative"
+//   overflowY={'auto'}
+//   maxHeight={'300px'}
+//   borderRadius="16px"
+//   w="720px"
+//   h="300px"
+//   mt={1}
+//   cursor="pointer"
+//   css={{
+//     background:
+//       'url(Rectangle 110 (00000).jpg), linear-gradient(265.73deg, #274C63 0%, #182F3E 100%)',
+//     boxShadow: !active
+//       ? `0px 5px 14px rgba(0, 0, 0, 0.47),
+//         4px -7px 15px rgba(174, 177, 255, 0.13),
+//         inset -1px 1px 2px rgba(128, 186, 255, 0.24)`
+//       : 'only-test',
+//   }}
+// >
+//   <Flex direction={'column'} pos="relative" w="570px" h="full">
+//     <NftPositionViewer active={false} />
+//     <RedeemCardViewer />
+//     <ListCardViewer />
+//   </Flex>
+// </Box>
