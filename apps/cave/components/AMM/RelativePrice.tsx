832478d695762cb6b3be754c211f648383fec122
gregs <ukfaeku@gmail.com>
2022-04-27 14:53:11 -0300

    better error messages

--- apps/cave/components/AMM/RelativePrice.tsx
+++ apps/cave/components/AMM/RelativePrice.tsx
@@ -2,12 +2,17 @@ import { HStack, Spinner, Text } from '@concave/ui'
 import { Currency } from 'gemswap-sdk'
 import { useFiatPrice } from './hooks/useFiatPrice'
 import { usePrice } from './hooks/usePrice'
+import { NoValidPairsError } from './hooks/usePair'
 
-const PairsError = () => (
-  <Text mr="auto" fontSize="sm" color="#c32417" fontWeight="medium">
-    Error Fetching Pairs
-  </Text>
-)
+const PairsError = ({ error }) => {
+  return (
+    <Text mr="auto" fontSize="sm" color="#c32417" fontWeight="medium">
+      {{
+        [NoValidPairsError]: `No liquidity in pair`,
+      }[error] || 'Error Fetching Pairs'}
+    </Text>
+  )
+}
 
 export const RelativePrice = ({
   currencyIn,
@@ -22,9 +27,9 @@ export const RelativePrice = ({
   return (
     <HStack flexWrap="wrap" align="center" fontSize="xs" fontWeight="medium" mr="auto">
       {relativePrice.isFetching && <Spinner size="xs" />}
-      {relativePrice.isLoading && <Text fontSize="sm">Updating prices</Text>}
-      {relativePrice.isError && <PairsError />}
-      {relativePrice.price && (
+      {relativePrice.isLoading && <Text fontSize="sm">Searching trade routes</Text>}
+      {relativePrice.isError && <PairsError error={relativePrice.error} />}
+      {relativePrice.isSuccess && (
         <>
           <Text>
             1 {relativePrice.price?.quoteCurrency.symbol} ={' '}
