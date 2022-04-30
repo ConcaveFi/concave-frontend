5f140f14e50e349fc10af012f7b85c6cd7c0a00a
rms rob <rr.rmsrob@gmail.com>
2022-04-30 18:57:58 +0200

    clean old auth

--- apps/cave/contexts/index.tsx
+++ apps/cave/contexts/index.tsx
@@ -2,18 +2,13 @@ import { WagmiProvider } from 'contexts/WagmiContext'
 import { ThemeProvider } from '@concave/ui'
 import { ReactQueryProvider } from './ReactQueryContext'
 import { ModalsProvider } from './ModalsContext'
-// import { AuthProvider } from 'contexts/AuthContext'
 
 export const AppProviders = ({ children, globalStyles, cookies }) => {
   return (
     <ThemeProvider globalStyles={globalStyles} cookies={cookies}>
       <ReactQueryProvider>
         <WagmiProvider>
-          <ModalsProvider>
-            {/* <AuthProvider> */}
-            {children}
-            {/* </AuthProvider> */}
-          </ModalsProvider>
+          <ModalsProvider>{children}</ModalsProvider>
         </WagmiProvider>
       </ReactQueryProvider>
     </ThemeProvider>
