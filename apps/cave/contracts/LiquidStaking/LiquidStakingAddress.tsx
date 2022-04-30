0f0a8fc50d1639e406913a9ebb23cfae6382378f
shanghigh <95733088+shanghigh@users.noreply.github.com>
2022-04-30 15:36:18 -0400

    framework

---
+++ apps/cave/contracts/LiquidStaking/LiquidStakingAddress.tsx
@@ -0,0 +1,7 @@
+type AddressMap = { [chainId: number]: string }
+
+export const BOND_ADDRESS: AddressMap = {
+  1: '',
+  3: '0x2b7ea66d564399246da8e3d6265db8f89af834c8',
+}
+// 0x7f02ba685172f164dd1d4a9df7f932349dfddfd4
