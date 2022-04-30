7b594f2096d47c43ddd94fabedb010731a797d82
gregs <ukfaeku@gmail.com>
2022-04-30 10:57:06 -0300

    native currency input max amount gas fees

--- apps/cave/components/AMM/InputField.tsx
+++ apps/cave/components/AMM/InputField.tsx
@@ -1,5 +1,5 @@
 import { HStack, Text } from '@concave/ui'
-import { Currency, CurrencyAmount } from 'gemswap-sdk'
+import { Currency, CurrencyAmount, JSBI } from 'gemswap-sdk'
 import { useCurrencyBalance } from 'hooks/useCurrencyBalance'
 import { Balance } from './Balance'
 import { useFiatValue } from './hooks/useFiatPrice'
@@ -9,6 +9,17 @@ import { parseAmount } from './utils/parseAmount'
 type InputFieldProps = {
   currencyAmountIn: CurrencyAmount<Currency>
   onChangeAmount: (value: CurrencyAmount<Currency>) => void
+}
+
+const MIN_FOR_GAS = '0.01'
+const maxAmount = (userBalance: CurrencyAmount<Currency>) => {
+  if (userBalance.currency.isNative) {
+    const gasAmount = parseAmount(MIN_FOR_GAS, userBalance.currency)
+    return userBalance.greaterThan(gasAmount)
+      ? userBalance.subtract(gasAmount)
+      : parseAmount('0', userBalance.currency)
+  }
+  return userBalance
 }
 
 export const InputField = ({ currencyAmountIn, onChangeAmount }: InputFieldProps) => {
@@ -25,9 +36,7 @@ export const InputField = ({ currencyAmountIn, onChangeAmount }: InputFieldProps
         {balance.isSuccess && (
           <Balance
             value={balance.data.formatted}
-            onClick={() =>
-              onChangeAmount(parseAmount(balance.data.formatted, currencyAmountIn.currency))
-            }
+            onClick={() => onChangeAmount(maxAmount(balance.amount))}
           />
         )}
       </HStack>
