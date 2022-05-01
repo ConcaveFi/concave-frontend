198965ed051870a07e3c6a6f2eb975fc2481252a
concave-engineering <erickdelgado@protonmail.com>
2022-05-01 01:38:00 -0400

    redeem ux fix

--- apps/cave/components/Bond/Redeem.tsx
+++ apps/cave/components/Bond/Redeem.tsx
@@ -1,18 +1,23 @@
 import { Card, Button } from '@concave/ui'
 
 export const Redeem = ({ onConfirm, bondSigma }: { onConfirm: () => void; bondSigma }) => {
-  const parse = bondSigma?.bondSigma
-  const claimed = parse?.claimed ? parse?.claimed : true
+  const parse = bondSigma?.claimed
+  console.log(parse)
+  const claimed = parse?.claimed
+  const totalOwed = bondSigma?.totalOwed.toFixed(2)
+  console.log(claimed)
   return (
     <>
       {claimed ? (
         ''
-      ) : (
+      ) : totalOwed > 0 ? (
         <Card mb={-20} fontWeight="bold" fontSize="lg" w="250px">
           <Button variant="primary" size="lg" isFullWidth onClick={onConfirm}>
             Redeem
           </Button>
         </Card>
+      ) : (
+        ''
       )}
     </>
   )
