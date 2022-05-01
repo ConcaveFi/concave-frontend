6bd8e40c2d23778e49fee957b617be3c8ee93561
concave-engineering <erickdelgado@protonmail.com>
2022-05-01 02:41:15 -0400

    cont.

--- apps/cave/components/Bond/Redeem.tsx
+++ apps/cave/components/Bond/Redeem.tsx
@@ -1,16 +1,11 @@
 import { Card, Button } from '@concave/ui'
 
 export const Redeem = ({ onConfirm, bondSigma }: { onConfirm: () => void; bondSigma }) => {
-  const parse = bondSigma?.claimed
-  console.log(parse)
-  const claimed = parse?.claimed
-  const totalOwed = bondSigma?.totalOwed.toFixed(2)
-  console.log(claimed)
+  const totalOwed = bondSigma?.totalOwed?.toFixed(2)
+  const totalPending = bondSigma?.totalPending?.toFixed(2)
   return (
     <>
-      {claimed ? (
-        ''
-      ) : totalOwed > 0 ? (
+      {totalPending < totalOwed ? (
         <Card mb={-20} fontWeight="bold" fontSize="lg" w="250px">
           <Button variant="primary" size="lg" isFullWidth onClick={onConfirm}>
             Redeem
