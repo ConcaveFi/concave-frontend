05740c4bd55a1bb61e31fa009a0be73e40ae3165
gregs <ukfaeku@gmail.com>
2022-05-02 11:58:57 -0300

    Input Balance: better ux

--- apps/cave/components/AMM/Balance.tsx
+++ apps/cave/components/AMM/Balance.tsx
@@ -3,12 +3,14 @@ import { commify } from 'ethers/lib/utils'
 
 export const Balance = ({ value, onClick }: { value: string; onClick?: () => void }) => (
   <Button
+    as={!!onClick ? Button : Text}
     fontSize="xs"
     ml="auto"
     onClick={onClick}
-    isDisabled={!+value}
     rightIcon={!!onClick && +value && <Text textColor="#2E97E2">Max</Text>}
-    _focus={{ transform: 'scale(1.02)' }}
+    _hover={!!onClick && { transform: 'scale(1.01)', color: 'text.high' }}
+    _focus={!!onClick && { transform: 'scale(1.02)' }}
+    _active={!onClick && { transform: 'none' }}
     leftIcon={<Text>Balance:</Text>}
     iconSpacing={1}
   >
