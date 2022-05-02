2f4963d94218ba5ed7b57f72381bc59a1b15f3d9
David Schrammel <schrammel@live.com>
2022-05-02 01:21:18 -0300

    feat(cave/removeLiquidity): toogle ETH/WETH

--- apps/cave/hooks/useRemoveLiquidity.ts
+++ apps/cave/hooks/useRemoveLiquidity.ts
@@ -1,4 +1,4 @@
-import { Ether, WETH9, WETH9_ADDRESS } from 'gemswap-sdk'
+import { WETH9_ADDRESS } from 'gemswap-sdk'
 import { useCurrentSupportedNetworkId } from 'hooks/useCurrentSupportedNetworkId'
 import { LiquidityInfoData } from 'hooks/useLiquidityInfo'
 import { Router } from 'lib/Router'
@@ -18,17 +18,16 @@ export const useRemoveLiquidity = ({ liquidityInfo }: { liquidityInfo: Liquidity
     +liquidityInfo.pair.reserve1.toExact() * liquidityInfo.userPoolShare * ratioToRemove
   const [hash, setHash] = useState<string>(null)
   const [{ data }] = useSigner()
-  const [toETH, setToETH] = useState(true)
+  const [receiveInNativeToken, setReceiveInNativeToken] = useState(true)
+  const tokenAIsNativeWrapper = tokenA.address === WETH9_ADDRESS[networkId]
+  const tokenBIsNativeWrapper = tokenB.address === WETH9_ADDRESS[networkId]
+  const hasNativeToken = tokenAIsNativeWrapper || tokenBIsNativeWrapper
 
   const removeLiquidity = async () => {
-    const tokenAIsEth = tokenA.address === WETH9_ADDRESS[networkId]
-    const tokenBIsEth = tokenB.address === WETH9_ADDRESS[networkId]
-    console.log({ tokenAIsEth, tokenBIsEth })
     const router = new Router(networkId, data)
-    if (toETH && (tokenAIsEth || tokenBIsEth)) {
-      console.log('ETH')
+    if (receiveInNativeToken && (tokenAIsNativeWrapper || tokenBIsNativeWrapper)) {
       const transaction = await router.removeLiquidityETH(
-        tokenAIsEth ? tokenB : tokenA,
+        tokenAIsNativeWrapper ? tokenB : tokenA,
         liquidityInfo.userBalance.data.value.mul(percentToRemove).div(100),
         account.address,
       )
@@ -52,6 +51,11 @@ export const useRemoveLiquidity = ({ liquidityInfo }: { liquidityInfo: Liquidity
     setPercentToRemove,
     removeLiquidity,
     hash,
+    tokenAIsNativeWrapper,
+    tokenBIsNativeWrapper,
+    hasNativeToken,
+    receiveInNativeToken,
+    handleNativeToken: () => setReceiveInNativeToken(!receiveInNativeToken),
   }
 }
 export type RemoveLiquidityState = ReturnType<typeof useRemoveLiquidity>
