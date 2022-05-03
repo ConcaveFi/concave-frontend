cd9994e46b30ca9befa9db0af89d98ba50a8c428
gregs <ukfaeku@gmail.com>
2022-05-03 15:23:54 -0300

    Split trade details components

--- apps/cave/components/AMM/Swap/ConfirmSwapModal.tsx
+++ apps/cave/components/AMM/Swap/ConfirmSwapModal.tsx
@@ -6,7 +6,8 @@ import React from 'react'
 import { useFiatValue } from '../hooks/useFiatPrice'
 import { usePrice } from '../hooks/usePrice'
 import { SwapSettings } from '../Settings'
-import { computeFiatValuePriceImpact } from '../utils/computeFiatValuePriceImpact'
+import { computeFiatValuePriceImpact } from './computeFiatValuePriceImpact'
+import { ExpectedOutput, MinExpectedOutput } from './ExpectedOutput'
 
 type TradeCurrencyInfoProps = {
   currencyAmount: CurrencyAmount<Currency>
@@ -45,58 +46,6 @@ const InOutArrow = () => {
         <ExpandArrowIcon w="18px" h="18px" />
       </Box>
     </Flex>
-  )
-}
-
-const MinExpectedOutput = ({
-  trade,
-  slippageTolerance,
-}: {
-  trade: Trade<Currency, Currency, TradeType>
-  slippageTolerance: { value: string; percent: Percent }
-}) => {
-  const isExactInput = trade.tradeType === TradeType.EXACT_INPUT
-  const amount = isExactInput
-    ? trade.minimumAmountOut(slippageTolerance.percent)
-    : trade.maximumAmountIn(slippageTolerance.percent)
-  return (
-    <Box>
-      <Flex fontSize="sm" w="100%" mb={3} justify="space-between" align="center">
-        <Text maxW="200px" fontWeight="bold" textColor="text.low" mr={4}>
-          {isExactInput ? `Minimum received` : `Maximum sent`} after slippage (
-          {slippageTolerance.value}%)
-        </Text>
-        <Text fontWeight="bold" textColor="text.low">
-          {amount.toSignificant(4, { groupSeparator: ',' })} {amount.currency.symbol}
-        </Text>
-      </Flex>
-      {/* <Flex fontSize="sm" w="100%" justify="space-between">
-        <Text fontWeight="bold" textColor="text.low">
-          Network Fee
-        </Text>
-        <Text fontWeight={700} textColor="text.low">
-          {estimatedFee && `~ ${estimatedFee} wei`}
-        </Text>
-      </Flex> */}
-    </Box>
-  )
-}
-const ExpectedOutput = ({ value, symbol, priceImpact }) => {
-  return (
-    <Stack fontWeight="bold" fontSize="md" w="100%">
-      <Flex justify="space-between">
-        <Text whiteSpace={'pre-wrap'} mr={4}>
-          Expected Output
-        </Text>
-        <Text>
-          {value} {symbol}
-        </Text>
-      </Flex>
-      <Flex justify="space-between">
-        <Text>Price Impact</Text>
-        <Text>{priceImpact}%</Text>
-      </Flex>
-    </Stack>
   )
 }
 
@@ -143,11 +92,7 @@ const ConfirmSwap = ({
       </Flex>
 
       <Flex direction="column" rounded="2xl" mb={6} p={6} shadow="Down Medium">
-        <ExpectedOutput
-          value={trade.outputAmount.toSignificant(4)}
-          symbol={currencyOut.symbol}
-          priceImpact={trade.priceImpact.toSignificant(3)}
-        />
+        <ExpectedOutput outputAmount={trade.outputAmount} priceImpact={trade.priceImpact} />
         <StackDivider borderRadius="full" mx={-4} my={4} h={0.5} bg="stroke.secondary" />
         <MinExpectedOutput trade={trade} slippageTolerance={settings.slippageTolerance} />
       </Flex>
