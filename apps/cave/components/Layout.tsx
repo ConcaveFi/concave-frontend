cf726070a2006d2e97d9a9af420f71e084ffe492
gregs <ukfaeku@gmail.com>
2022-05-05 17:49:17 -0300

    revamp faucet

--- apps/cave/components/Layout.tsx
+++ apps/cave/components/Layout.tsx
@@ -1,12 +1,18 @@
 import { Flex, Container } from '@concave/ui'
+import dynamic from 'next/dynamic'
 import React from 'react'
 import { SideBar } from './SideBar/SideBar'
+
+const TestnetIndicator = dynamic(() =>
+  import('./TestTokensMinter').then((module) => module.TestnetIndicator),
+)
 
 export const DefaultLayout = ({ children }) => {
   return (
     <Flex as="main" direction={{ base: 'column', md: 'row' }}>
       <SideBar />
       <Container display="flex" mb="25vh" maxWidth="container.xl">
+        <TestnetIndicator />
         {children}
       </Container>
     </Flex>
--- apps/cave/components/TestTokensMinter.tsx
+++ apps/cave/components/TestTokensMinter.tsx
@@ -1,5 +1,17 @@
 import { ChainId, DAI, NATIVE } from '@concave/gemswap-sdk'
-import { Button, Image, Link, Modal, Stack, Text } from '@concave/ui'
+import { CloseIcon } from '@concave/icons'
+import {
+  Box,
+  Button,
+  Card,
+  Image,
+  Link,
+  Modal,
+  SlideFade,
+  Stack,
+  Text,
+  useDisclosure,
+} from '@concave/ui'
 import { Wallet } from 'ethers'
 import { hexlify, parseEther, parseUnits } from 'ethers/lib/utils'
 import { useCurrencyBalance } from 'hooks/useCurrencyBalance'
@@ -8,6 +20,7 @@ import { useEffect, useState } from 'react'
 import { useQuery } from 'react-query'
 import { chain, useAccount, useContractWrite, useNetwork } from 'wagmi'
 import { useWorthyUser } from './DevelopGateway'
+
 import { getTxExplorer } from './TransactionSubmittedDialog'
 
 const faucetKey = process.env.NEXT_PUBLIC_FAUCET_PK
@@ -30,6 +43,8 @@ const ETHFaucet = () => {
 
   const { data: ethBalance } = useCurrencyBalance(NATIVE[ChainId.ROPSTEN])
 
+  const { data: faucetBalance } = useQuery('faucet balance', () => faucet.getBalance())
+
   const {
     data: sentEthTx,
     isSuccess: ethSentSuccess,
@@ -37,10 +52,17 @@ const ETHFaucet = () => {
     refetch: sendEth,
   } = useQuery('send eth', () => sendSomeEth(account?.address), { enabled: false })
 
-  if (!ethBalance?.greaterThan(0))
+  if (faucetBalance?.lt(parseEther('0.1')))
     return (
       <Stack fontWeight="bold" rounded="2xl" shadow="down" py={3} fontSize="sm" spacing={0}>
-        <Text>Nice you already got {ethBalance?.toFixed(3, { groupSeparator: ',' })} ETH</Text>
+        <Text>Noo the ETH faucet is dry 😭</Text>
+      </Stack>
+    )
+
+  if (ethBalance?.greaterThan(0))
+    return (
+      <Stack fontWeight="bold" rounded="2xl" shadow="down" py={3} fontSize="sm" spacing={0}>
+        <Text>Nice you already got {ethBalance?.toFixed(2, { groupSeparator: ',' })} ETH</Text>
       </Stack>
     )
 
@@ -58,14 +80,14 @@ const ETHFaucet = () => {
       </Button>
       {ethSentSuccess && (
         <Stack fontWeight="bold" rounded="2xl" shadow="down" py={2} fontSize="sm" spacing={0}>
-          <Text>0.1 ETH Sent!</Text>
+          <Text>🎉 0.1 ETH Sent!</Text>
           <Link
             href={getTxExplorer(sentEthTx, chain.ropsten)}
             fontSize="sm"
             color="text.accent"
             isExternal
           >
-            check on explorer
+            View on explorer
           </Link>
         </Stack>
       )}
@@ -88,14 +110,14 @@ const DAIMinter = () => {
   if (!!mintDaiTx)
     return (
       <Stack fontWeight="bold" rounded="2xl" shadow="down" py={2} fontSize="sm" spacing={0}>
-        <Text>69420 tDAI tx sent!</Text>
+        <Text>🎉 69420 tDAI tx sent!</Text>
         <Link
           href={getTxExplorer(mintDaiTx, chain.ropsten)}
           fontSize="sm"
           color="text.accent"
           isExternal
         >
-          check on explorer
+          View on explorer
         </Link>
       </Stack>
     )
@@ -105,25 +127,16 @@ const DAIMinter = () => {
       leftIcon={<Image w="20px" src={`/assets/tokens/dai.svg`} alt="" />}
       onClick={() => mintDAI()}
       isLoading={loading}
-      loadingText="confirm in your wallet"
+      loadingText="Confirm in your wallet"
       variant="secondary"
       p={3}
     >
-      Mint some tDai
+      Mint tDai
     </Button>
   )
 }
 
-const TestTokensMinter = () => {
-  const { data: tDaiBalance } = useCurrencyBalance(DAI[ChainId.ROPSTEN])
-
-  const [isOpen, setIsOpen] = useState(false)
-  useEffect(() => {
-    setIsOpen(tDaiBalance && !tDaiBalance?.greaterThan(0))
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [tDaiBalance?.serialize()])
-  const onClose = () => setIsOpen(false)
-
+const TestTokensMinter = ({ isOpen, onClose }) => {
   return (
     <Modal
       bluryOverlay={true}
@@ -133,9 +146,7 @@ const TestTokensMinter = () => {
       onClose={onClose}
       bodyProps={{ w: '350px', gap: 3, textAlign: 'center' }}
     >
-      <Text fontWeight="bold">
-        Looks like {`you're`} in Ropsten Testnet, but {`don't`} have any tDai to test around here 😔
-      </Text>
+      <Text fontWeight="bold">{`You'll`} need tDai to test around here</Text>
       <ETHFaucet />
       <DAIMinter />
       <Text fontWeight="bold" fontSize="sm" color="text.low">
@@ -145,10 +156,75 @@ const TestTokensMinter = () => {
   )
 }
 
-export const TestTokensMinterModal = () => {
-  const [{ data }] = useNetwork()
+export const TestnetIndicator = () => {
+  const [{ data: network }] = useNetwork()
   const { isUserWorthy } = useWorthyUser()
 
-  if (!isUserWorthy || data.chain?.id !== ChainId.ROPSTEN) return null
-  return <TestTokensMinter />
+  const [isOpen, setIsOpen] = useState(network.chain?.testnet && isUserWorthy)
+  const onClose = () => setIsOpen(false)
+
+  useEffect(() => {
+    setIsOpen(network.chain?.testnet && isUserWorthy)
+  }, [isUserWorthy, network.chain?.testnet])
+
+  const minterModal = useDisclosure()
+
+  // if (!isOpen) return null
+  return (
+    <SlideFade in={isOpen} unmountOnExit offsetY={-10}>
+      <Box pos="absolute" top={{ base: 20, md: 8 }}>
+        <Card
+          variant="secondary"
+          p={5}
+          direction="row"
+          align="center"
+          rounded="3xl"
+          shadow="Up for Blocks"
+          pos="relative"
+          overflow="visible"
+        >
+          <Stack spacing={0}>
+            <Text fontWeight="bold" whiteSpace="nowrap">
+              {`You're`} currently on{' '}
+              <Text
+                as="span"
+                bgGradient="linear(to-r, #7a58d6, #7DE0FF, #7a58d6, #84E2FF)"
+                filter="drop-shadow(0px 0px 10px rgba(240, 255, 245, 0.3))"
+                bgClip="text"
+              >
+                {network.chain?.name}
+              </Text>{' '}
+              testnet
+            </Text>
+
+            <Text color="text.low" fontWeight="medium">
+              Get some test tokens on the faucet!
+            </Text>
+          </Stack>
+          <Stack spacing={1} align="center" ml={6}>
+            <Button variant="primary" px={4} py={2} h="min" onClick={minterModal.onOpen}>
+              Open Faucet
+            </Button>
+            {/* <Button fontSize="xs" color="text.accent">
+            Switch to mainnet
+          </Button> */}
+          </Stack>
+
+          <Button
+            bg="blackAlpha.700"
+            border="2px solid white"
+            p={1.5}
+            rounded="full"
+            pos="absolute"
+            top="-4px"
+            right="-4px"
+            onClick={onClose}
+          >
+            <CloseIcon w="8px" h="8px" />
+          </Button>
+        </Card>
+        <TestTokensMinter isOpen={minterModal.isOpen} onClose={minterModal.onClose} />
+      </Box>
+    </SlideFade>
+  )
 }
