40c51ab55a181aa82d5fb0f477c05c6c1b3a57d3
21Chani <dmendoncacosta03@gmail.com>
2022-05-06 20:24:07 -0300

    added first component of dashboard mobile.

---
+++ apps/cave/components/Dashboard/Mobile/DashboardMobile.tsx
@@ -0,0 +1,10 @@
+import { Card, Flex } from '@concave/ui'
+import { DividendsShareMobile } from './Utils/DividendsShare'
+
+export default function DashboardMobile() {
+  return (
+    <Flex>
+      <DividendsShareMobile />
+    </Flex>
+  )
+}
