5e8b0e555b1d623d83e603b3955c02dc5e59868a
NUnicorn <unicornewborn@gmail.com>
2022-05-06 13:26:48 -0700

    Fix totalStaked and CNVBalance

--- apps/cave/contracts/LiquidStaking/LiquidStakingAddress.tsx
+++ apps/cave/contracts/LiquidStaking/LiquidStakingAddress.tsx
@@ -2,7 +2,7 @@ type AddressMap = { [chainId: number]: string }
 
 export const LIQUID_STAKING_ADDRESS: AddressMap = {
   1: '',
-  3: '0x2b7ea66d564399246da8e3d6265db8f89af834c8',
+  3: '0x265271970C6E13a942F0F75c9d619fFe5ca2872e',
 }
 
 //api.nftport.xyz
