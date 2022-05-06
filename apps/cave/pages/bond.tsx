f0bb334ade72d9096b975c210c6d57771433f0a1
concave-engineering <erickdelgado@protonmail.com>
2022-05-06 16:10:48 -0400

    fix bond roi

--- apps/cave/pages/bond.tsx
+++ apps/cave/pages/bond.tsx
@@ -120,7 +120,7 @@ export default function Bond() {
                 icon="/assets/tokens/cnv.svg"
                 roi={`${
                   cnvMarketPrice > 0
-                    ? (1 - +(+cnvMarketPrice / +bondSpotPrice).toFixed(2)) * 100
+                    ? (1 - (+bondSpotPrice / +cnvMarketPrice) * 100).toFixed(2)
                     : '-'
                 }%`}
                 vestingTerm={`${termLength} Days`}
