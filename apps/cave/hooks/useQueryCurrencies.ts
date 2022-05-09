954250065aaa17fe2a87b5cc008a9c78e696358e
gregs <ukfaeku@gmail.com>
2022-05-09 15:41:08 -0300

    useQueryCurrencies save

--- apps/cave/hooks/useQueryCurrencies.ts
+++ apps/cave/hooks/useQueryCurrencies.ts
@@ -1,75 +1,28 @@
 import { isAddress } from 'ethers/lib/utils'
 import { ChainId, Currency, Fetcher, NATIVE, Token } from '@concave/gemswap-sdk'
 import { concaveProvider } from 'lib/providers'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-import { useNetwork } from 'wagmi'
-// import { useQueryState } from 'next-usequerystate'
+import { queryTypes, useQueryStates } from 'next-usequerystate'
 
 const getAddressOrSymbol = (currency: Currency) => {
   if (!currency) return undefined
   return currency.isNative ? currency.symbol : currency.wrapped.address
 }
 
-/*
-  keeps track of two tokens using the url query params
-  token0=<address or symbol if native>
-  token1=<address or symbol if native>
-  chainId=<like 1 for mainnet 3 for ropsten...>
-*/
-export const useQueryCurrencies = (currency0: Currency, currency1: Currency) => {
-  const router = useRouter()
-  // const [currencies, setCurrencies] = useQueryState('currencies', {  })
-  // const [chainId, setChainId] = useQueryState('chainId', {  })
-  const [{ data: network }] = useNetwork()
-  useEffect(() => {
-    router.replace(
-      {
-        query: {
-          ...(currency0 && { token0: getAddressOrSymbol(currency0) }),
-          ...(currency1 && { token1: getAddressOrSymbol(currency1) }),
-          chainId: network.chain?.id || 1,
-        },
-      },
-      undefined,
-      { shallow: true },
-    )
-  }, [currency0, currency1, network.chain?.id, router])
-}
-
-type JsonCurrency = {
-  isNative: boolean
-  chainId: ChainId
-  name: string
-  symbol: string
-  decimals: number
-  wrapped: {
-    address: string
+const currencyToJson = ({ chainId, wrapped, decimals, symbol, name, isNative }: Currency) =>
+  chainId && {
+    chainId,
+    wrapped: {
+      address: wrapped.address,
+    },
+    decimals,
+    symbol,
+    name,
+    isNative,
   }
-}
 
-const currencyToJson = (currency: Currency): JsonCurrency => {
-  if (!currency) return null
-  return {
-    isNative: currency.isNative,
-    chainId: currency.chainId,
-    name: currency.name,
-    symbol: currency.symbol,
-    decimals: currency.decimals,
-    wrapped: { address: currency.wrapped.address },
-  }
-}
-
-export const currencyFromJson = (jsonCurrency: JsonCurrency) => {
-  if (!jsonCurrency) return undefined
-  if (jsonCurrency.isNative) return NATIVE[jsonCurrency.chainId]
-  return new Token(
-    jsonCurrency.chainId,
-    jsonCurrency.wrapped.address,
-    jsonCurrency.decimals,
-    jsonCurrency.symbol,
-    jsonCurrency.name,
-  )
+const currencyFromJson = ({ chainId, wrapped, decimals, symbol, name, isNative }: Currency) => {
+  if (isNative) return NATIVE[chainId]
+  return new Token(chainId, wrapped.address, decimals, symbol, name)
 }
 
 const fetchTokenOrNativeData = (addressOrSymbol: string, chainId: ChainId) => {
@@ -81,11 +34,42 @@ const fetchTokenOrNativeData = (addressOrSymbol: string, chainId: ChainId) => {
 
 export const fetchCurrenciesFromQuery = async (query) => {
   const { token0, token1, chainId = 1 } = query
+  const _chainId: ChainId = [ChainId.ETHEREUM, ChainId.ROPSTEN].includes(chainId) ? chainId : 1
 
   const [_token0, _token1] = await Promise.all([
-    fetchTokenOrNativeData(token0, chainId),
-    fetchTokenOrNativeData(token1, chainId),
+    fetchTokenOrNativeData(token0, _chainId),
+    fetchTokenOrNativeData(token1, _chainId),
   ])
 
-  return [currencyToJson(_token0), currencyToJson(_token1)]
+  return [_token0, _token1].map(currencyToJson)
+}
+
+/*
+  keeps track of two tokens using the url query params
+  currency0=<address or symbol if native>
+  currency1=<address or symbol if native>
+  chainId=<1 for mainnet 3 for ropsten...>
+*/
+export const useQueryCurrencies = (initialCurrencies: [Currency, Currency]) => {
+  const [query, setQuery] = useQueryStates({
+    chainId: queryTypes.integer.withDefault(1),
+    currencies: {
+      parse: () => null,
+      serialize: (currencies) => {
+        console.log(currencies)
+        return currencies.map(getAddressOrSymbol)
+      },
+      defaultValue: initialCurrencies.map(currencyFromJson),
+    },
+  })
+
+  const onChangeCurrencies = (currencies: [Currency, Currency]) => {
+    console.log(currencies)
+    const chainId = [ChainId.ETHEREUM, ChainId.ROPSTEN].includes(currencies[0].chainId)
+      ? currencies[0].chainId
+      : ChainId.ETHEREUM
+    setQuery({ chainId, currencies }, { shallow: true })
+  }
+
+  return { currencies: query.currencies, onChangeCurrencies }
 }
