28411b7fd29ed1d5b5cf7cdf52b7811ca0c32574
gregs <ukfaeku@gmail.com>
2022-05-09 21:07:28 -0300

    onChangeNetwork

--- apps/cave/hooks/useCurrentSupportedNetworkId.ts
+++ apps/cave/hooks/useCurrentSupportedNetworkId.ts
@@ -1,8 +1,13 @@
+import { useUpdateEffect } from '@concave/ui'
 import { chain, useNetwork } from 'wagmi'
 
-export const useCurrentSupportedNetworkId = () => {
+export const useCurrentSupportedNetworkId = (onChangeNetwork) => {
   const [{ data }] = useNetwork()
   const isRopsten = data.chain?.id === chain.ropsten.id
+  const chainId = isRopsten ? (chain.ropsten.id as 3) : (chain.mainnet.id as 1)
+  useUpdateEffect(() => {
+    onChangeNetwork?.(chainId)
+  }, [onChangeNetwork, chainId])
   // we only support mainnet rn, so unless we testing in ropsten, default to mainnet
-  return isRopsten ? (chain.ropsten.id as 3) : (chain.mainnet.id as 1)
+  return chainId
 }
