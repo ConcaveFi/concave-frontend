0f8d4e6498ab9c945a4ecb13e0f77ae4a8bb89f3
psgb-r3763 <psgb@protonmail.com>
2022-05-10 14:46:11 -0700

    fix redeem button height  on bond page

--- apps/cave/components/Bond/Redeem.tsx
+++ apps/cave/components/Bond/Redeem.tsx
@@ -6,21 +6,31 @@ export const Redeem = ({
   bondSigma,
   largeFont,
   setBottom,
+  customHeight,
 }: {
   onConfirm: () => void
   bondSigma
   largeFont?: boolean
   setBottom?: boolean
+  customHeight?: boolean
 }) => {
   const redeemable = bondSigma?.parseRedeemable
   console.log(redeemable)
   const fontSize = largeFont ? '2xl' : 'xl'
+  const customHeightSetting = customHeight ? { height: '51.35px' } : {} // 51.35px comes out to 50px?
   const bottom = setBottom ? '-3px' : -3
   return (
     <>
       {Math.sign(redeemable) === 1 ? (
         <Card mb={-12} bottom={bottom} fontWeight="bold" fontSize={fontSize} w="100%">
-          <Button variant="primary" size="lg" isFullWidth onClick={onConfirm} fontSize={'inherit'}>
+          <Button
+            variant="primary"
+            size="lg"
+            isFullWidth
+            onClick={onConfirm}
+            fontSize={'inherit'}
+            {...customHeightSetting}
+          >
             Redeem
           </Button>
         </Card>
--- apps/cave/pages/bond.tsx
+++ apps/cave/pages/bond.tsx
@@ -156,6 +156,7 @@ export default function Bond() {
                 }}
                 largeFont
                 setBottom
+                customHeight
               ></Redeem>
             </Card>
           </Box>
