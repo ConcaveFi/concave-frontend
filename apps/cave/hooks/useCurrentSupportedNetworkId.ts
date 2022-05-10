504659a975e79b31fd22e1027cbe8490bdc99f83
gregs <ukfaeku@gmail.com>
2022-05-10 19:58:51 -0300

    useCurrentSupportedNetworkId formatting

--- apps/cave/hooks/useCurrentSupportedNetworkId.ts
+++ apps/cave/hooks/useCurrentSupportedNetworkId.ts
@@ -1,13 +1,17 @@
+import { ChainId } from '@concave/gemswap-sdk'
 import { useUpdateEffect } from '@concave/ui'
 import { chain, useNetwork } from 'wagmi'
 
-export const useCurrentSupportedNetworkId = (onChangeNetwork) => {
+export const useCurrentSupportedNetworkId = (onChangeNetwork?: (chainId: ChainId) => void) => {
   const [{ data }] = useNetwork()
+
+  // we only support mainnet rn, so unless we testing in ropsten, default to mainnet
   const isRopsten = data.chain?.id === chain.ropsten.id
   const chainId = isRopsten ? (chain.ropsten.id as 3) : (chain.mainnet.id as 1)
+
   useUpdateEffect(() => {
     onChangeNetwork?.(chainId)
   }, [onChangeNetwork, chainId])
-  // we only support mainnet rn, so unless we testing in ropsten, default to mainnet
+
   return chainId
 }
