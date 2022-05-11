4e96d2b6e8a54e25a0e39ac01bf6a8e9206be08d
David Schrammel <schrammel@live.com>
2022-05-10 23:18:04 -0300

    refactor(cave/liquidity): wip

--- apps/cave/components/AMM/AddLiquidity/useAddLiquidityState.ts
+++ apps/cave/components/AMM/AddLiquidity/useAddLiquidityState.ts
@@ -1,4 +1,4 @@
-import { useLinkedFields } from 'components/CurrencyAmountField'
+import { useLinkedCurrencyFields } from 'components/CurrencyAmountField'
 import { usePair } from 'components/AMM/hooks/usePair'
 import { toAmount } from 'utils/toAmount'
 import { Currency, CurrencyAmount, NATIVE, Pair } from '@concave/gemswap-sdk'
@@ -8,7 +8,7 @@ import { useEffect, useMemo, useRef, useState } from 'react'
 const makeCurrencyFields = (initialTokens = [], networkId) => {
   return {
     first: initialTokens[0]?.chainId === networkId ? initialTokens[0] : NATIVE[networkId],
-    second: initialTokens[1]?.chainId === networkId && initialTokens[1],
+    second: initialTokens[1]?.chainId === networkId ? initialTokens[1] : undefined,
   }
 }
 
@@ -47,23 +47,26 @@ export const useAddLiquidityState = ({
     toAmount('0', initialCurrencyFields.first),
   )
 
-  const { onChangeField, fieldCurrency, setFieldCurrency } = useLinkedFields(
-    (field) => (newAmount) => setExactAmount(newAmount),
+  const { onChangeField, currencies, setCurrencies } = useLinkedCurrencyFields(
     initialCurrencyFields,
+    (field) => {
+      setExactAmount(field)
+      console.log(field)
+    },
   )
 
   useEffect(() => {
-    setFieldCurrency(initialCurrencyFields)
+    setCurrencies(initialCurrencyFields)
     setExactAmount(toAmount('0', initialCurrencyFields.first))
     if (currency1) {
       secondFieldAmount.current = toAmount('0', currency1)
     }
-  }, [currency1, initialCurrencyFields, setFieldCurrency])
+  }, [currency1, initialCurrencyFields, setCurrencies])
   const firstFieldAmount = useRef<CurrencyAmount<Currency>>()
   const secondFieldAmount = useRef<CurrencyAmount<Currency>>()
 
-  const isExactFirst = fieldCurrency.first && exactAmount?.currency.equals(fieldCurrency.first)
-  const otherCurrency = fieldCurrency[isExactFirst ? 'second' : 'first']
+  const isExactFirst = currencies.first && exactAmount?.currency.equals(currencies.first)
+  const otherCurrency = currencies[isExactFirst ? 'second' : 'first']
 
   const pair = usePair(exactAmount?.currency.wrapped, otherCurrency?.wrapped)
   const otherAmount = isExactFirst ? firstFieldAmount.current : secondFieldAmount.current
