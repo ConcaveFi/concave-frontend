98853ef9047640aaf537f8bc74e008785b1cdf58
psgb-r3763 <psgb@protonmail.com>
2022-05-11 19:18:15 -0700

    parseInt to parseFloat

--- apps/cave/components/Bond/BondBuyCard.tsx
+++ apps/cave/components/Bond/BondBuyCard.tsx
@@ -125,9 +125,10 @@ export function BondBuyCard({ bondTransaction, setBondTransaction, setAmountInAn
             .then((tx) => {
               setBondTransaction(tx)
               confirmModal.onClose()
+              console.log('amountOut', amountOut)
               setAmountInAndOut({
                 in: parseFloat(String(amountIn.toFixed())).toFixed(2),
-                out: parseInt(amountOut).toFixed(2),
+                out: parseFloat(amountOut).toFixed(2),
               })
             })
             .catch((e) => {
