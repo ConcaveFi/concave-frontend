786927c288b2257c6ee90d92cb0e0cc13602d088
21Chani <dmendoncacosta03@gmail.com>
2022-05-14 08:34:39 -0300

    fixed name of wallet on treasury

--- apps/cave/components/Treasury/TreasuryRedeemCard.tsx
+++ apps/cave/components/Treasury/TreasuryRedeemCard.tsx
@@ -7,6 +7,7 @@ import { GlassPanel } from './TreasuryManagementCard'
 import { useEffect, useState } from 'react'
 import { useAccount, useContractWrite } from 'wagmi'
 import { aCNVredeemabi } from 'lib/contractoABI'
+import { CNV_ADDRESS } from '@concave/gemswap-sdk'
 
 function ClaimAcnvButton() {
   const [{ data: account }] = useAccount()
@@ -51,6 +52,12 @@ function TreasuryRedeemCard() {
     tokenChainId: CNV.chainId,
   })
 
+  const [walletName, setWalletName] = useState('')
+
+  useEffect(() => {
+    if (walletName === '') setWalletName(getWalletType())
+  })
+
   return (
     <GlassPanel
       width={'260px'}
@@ -61,7 +68,6 @@ function TreasuryRedeemCard() {
     >
       <Flex justify={'center'} align="center" flex={1} maxHeight="60px" mt={2}>
         <Text fontSize={'25px'} fontWeight="700">
-          {' '}
           Redeem CNV
         </Text>
       </Flex>
@@ -91,59 +97,10 @@ function TreasuryRedeemCard() {
       </Flex>
 
       <Text textColor={'text.low'} fontWeight={700} cursor="pointer" mx={'auto'} my="auto">
-        Add CNV to your Metamask
+        Add CNV to your {walletName}
       </Text>
     </GlassPanel>
   )
 }
 
 export default TreasuryRedeemCard
-{
-  /* <Flex direction={'column'} width="250px" mx={'auto'} mt={6} textAlign="center">
-        <Text fontWeight={500} mb={4} fontSize="24px">
-          Redeem CNV
-        </Text>
-        <Text fontWeight={700} textColor={'text.low'} fontSize="14px">
-          If you was the one who did that before and want your CNV please press that button
-        </Text>
-      </Flex>
-      <Card width={'182px'} height="40px" justify={'center'} align="center" mx={'auto'} my={'auto'}>
-        <Button>
-          <Text textColor={'text.low'} fontSize="18px" fontWeight={'700'}>
-            aCNV - balance
-          </Text>
-        </Button>
-      </Card>
-      <Card width={'182px'} height="40px" justify={'center'} align="center" mx={'auto'} my={'auto'}>
-        <Button>
-          <Text textColor={'text.low'} fontSize="18px" fontWeight={'700'}>
-            pCNV - balance
-          </Text>
-        </Button>
-      </Card>
-      <Card width={'182px'} height="40px" justify={'center'} align="center" mx={'auto'} my={'auto'}>
-        <Button>
-          <Text textColor={'text.low'} fontSize="18px" fontWeight={'700'}>
-            bbtCNV - balance
-          </Text>
-        </Button>
-      </Card>
-
-      <Button disabled={loadingtoWallet} onClick={addingToWallet}>
-        <HStack spacing="2px" textAlign="left" w="150px" gap={2}>
-          {isMounted && (
-            <Image
-              style={{ filter: 'saturate(0%)' }}
-              src={renderProviderText(getWalletType()).img as string}
-              alt="w"
-              ml={-3}
-              h="1rem"
-              w="1rem"
-            />
-          )}
-          <Text fontSize="base" fontWeight="bold">
-            Add CNV to wallet
-          </Text>
-        </HStack>
-      </Button> */
-}
