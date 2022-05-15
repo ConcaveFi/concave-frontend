5c6f987bb6eb2ee5a0c635e34193a65821be167e
NUnicorn <unicornewborn@gmail.com>
2022-05-15 16:36:35 -0700

    Add floating cards

--- libs/ui/components/Modal.tsx
+++ libs/ui/components/Modal.tsx
@@ -37,7 +37,7 @@ export function Modal({
         <ModalHeader textAlign={titleAlign}>{title}</ModalHeader>
         {!hideClose && <ModalCloseButton />}
         <HStack>
-          <VStack position="absolute" top="10" left="-80">
+          <VStack position="absolute" top="10" left="-80" spacing={5}>
             <Card variant="secondary" py="6" px="4" w={300}>
               <Text fontWeight="bold">Total vAPR</Text>
               <Text fontSize="sm">
@@ -45,8 +45,30 @@ export function Modal({
                 rewards from bonding activity, base emissions and the quarterly dividend.
               </Text>
             </Card>
-            <Text>Base Emissions</Text>
-            <Text>Quarterly dividends</Text>
+            <Card variant="secondary" py="6" px="4" w={300}>
+              <Text fontWeight="bold">Bonding Emissions</Text>
+              <Text fontSize="sm">
+                Anti-Dilutive bond emissions ensure staking positions are rewarded with a share of
+                any new supply minted from bonds that are purchased. Staking positions recieve a
+                share of this growth compounded at 8hr intervals.
+              </Text>
+            </Card>
+            <Card variant="secondary" py="6" px="4" w={300}>
+              <Text fontWeight="bold">Base Emissions</Text>
+              <Text fontSize="sm">
+                Base emissions ensure that staking positions receive continuous CNV rewards
+                throughout the term. Staking positions receive a boost in base emissions as a
+                function of term length.
+              </Text>
+            </Card>
+            <Card variant="secondary" py="6" px="4" w={300}>
+              <Text fontWeight="bold">Quarterly Dividend</Text>
+              <Text fontSize="sm">
+                Quarterly dividends ensure that stakers receive a share of profits in non CNV assets
+                from all yield bearing products and services. Staking positions receive a boost in
+                dividend as a function of term length.
+              </Text>
+            </Card>
           </VStack>
           <Card variant="primary" p="6" shadow="Up for Blocks" {...bodyProps}>
             {/* <HStack>
