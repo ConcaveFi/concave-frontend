6eab31f46c2a9c8c9cfc03b25a6eab487aa290d9
NUnicorn <unicornewborn@gmail.com>
2022-05-15 16:33:30 -0700

    Overwrite chakra-ui modal props

--- libs/ui/components/Modal.tsx
+++ libs/ui/components/Modal.tsx
@@ -1,10 +1,13 @@
 import {
+  HStack,
   Modal as ChakraModal,
   ModalCloseButton,
   ModalContent,
   ModalHeader,
   ModalOverlay,
   ModalProps as ChakraModalProps,
+  Text,
+  VStack,
 } from '@chakra-ui/react'
 import { ReactNode } from 'react'
 import { Card, CardProps } from './Card'
@@ -33,9 +36,25 @@ export function Modal({
       <ModalContent w="auto">
         <ModalHeader textAlign={titleAlign}>{title}</ModalHeader>
         {!hideClose && <ModalCloseButton />}
-        <Card variant="primary" p="6" shadow="Up for Blocks" {...bodyProps}>
-          {children}
-        </Card>
+        <HStack>
+          <VStack position="absolute" top="10" left="-80">
+            <Card variant="secondary" py="6" px="4" w={300}>
+              <Text fontWeight="bold">Total vAPR</Text>
+              <Text fontSize="sm">
+                Total vAPR aggregates rewards associated with each staking position including
+                rewards from bonding activity, base emissions and the quarterly dividend.
+              </Text>
+            </Card>
+            <Text>Base Emissions</Text>
+            <Text>Quarterly dividends</Text>
+          </VStack>
+          <Card variant="primary" p="6" shadow="Up for Blocks" {...bodyProps}>
+            {/* <HStack>
+            <Text>Henlo</Text> */}
+            {children}
+            {/* </HStack> */}
+          </Card>
+        </HStack>
       </ModalContent>
     </ChakraModal>
   )
