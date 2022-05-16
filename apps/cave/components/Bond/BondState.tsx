25f5d031d214fb6dd7ac6b3aade03dcc8eb0b9e1
concave-engineering <erickdelgado@protonmail.com>
2022-05-16 10:54:17 -0400

    5 day vest fix

--- apps/cave/components/Bond/BondState.tsx
+++ apps/cave/components/Bond/BondState.tsx
@@ -134,7 +134,7 @@ export const getUserBondPositions = async (
     totalPending += +(+utils.formatEther(positionData.redeemed))
     totalOwed += +(+utils.formatEther(positionData.owed))
   }
-  const fullyVestedTimestamp = oldest * 1000 + 86400000
+  const fullyVestedTimestamp = oldest * 1000 + 432000000
   const parseOldest = new Date(fullyVestedTimestamp).toString().slice(4, 21)
   const parseRedeemable = Math.sign(redeemable) === -1 ? 0 : +redeemable
   if (totalPending === totalOwed) claimed = true
