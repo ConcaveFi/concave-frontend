d1298c3ff66c85eb410db48765189998e4effe97
21Chani <dmendoncacosta03@gmail.com>
2022-05-16 16:09:53 -0300

    Modal on confirm tx in liquid stake

--- apps/cave/components/LiquidStaking/StakeModal/StakeInput.tsx
+++ apps/cave/components/LiquidStaking/StakeModal/StakeInput.tsx
@@ -1,6 +1,7 @@
 import { CNV, Currency, CurrencyAmount } from '@concave/gemswap-sdk'
-import { Box, Button, Card, Flex } from '@concave/ui'
+import { Box, Button, Card, Flex, useDisclosure } from '@concave/ui'
 import { CurrencyInputField } from 'components/CurrencyAmountField'
+import { TransactionSubmittedDialog } from 'components/TransactionSubmittedDialog'
 import { ApproveButton, useApproval } from 'hooks/useAllowance'
 import { useCurrencyBalance } from 'hooks/useCurrencyBalance'
 import { useCurrentSupportedNetworkId } from 'hooks/useCurrentSupportedNetworkId'
@@ -24,66 +25,71 @@ function StakeInput(props: { poolId: number; period: string; onClose: () => void
   const [approvedCNV] = approveStatus
   const cnvBalance = useCurrencyBalance(stakeInput?.currency, { watch: true })
 
+  const [tx, setTx] = useState(undefined)
+
+  const { isOpen, onClose, onOpen } = useDisclosure()
   return (
-    <Box>
-      <Card shadow="down" w="350px" px={0} py={0}>
-        <Flex justify="space-between" alignItems="center">
-          <CurrencyInputField currencyAmountIn={stakeInput} onChangeAmount={setStakeInput} />
-        </Flex>
-      </Card>
+    <>
+      <Box>
+        <Card shadow="down" w="350px" px={0} py={0}>
+          <Flex justify="space-between" alignItems="center">
+            <CurrencyInputField currencyAmountIn={stakeInput} onChangeAmount={setStakeInput} />
+          </Flex>
+        </Card>
 
-      <Box mt={10} width="350px">
-        {approvedCNV && (
-          <ApproveButton
-            fontWeight="bold"
-            fontSize="md"
-            variant="primary"
-            bgGradient="linear(90deg, #72639B 0%, #44B9DE 100%)"
-            w="100%"
-            h="50px"
-            size="large"
-            mx="auto"
-            useApproveInfo={approveStatus}
-          />
-        )}
-        {!approvedCNV && (
-          <Button
-            mt={5}
-            onClick={async () => {
-              const contract = new StakingV1Contract(netWorkdId, signer)
-              contract
-                .lock(account?.address, stakeInput.numerator.toString(), props.poolId)
-                .then((x) => {
-                  console.log(x)
-                  props.onClose()
-                })
-                .catch((e) => {
-                  console.log(e)
-                })
-            }}
-            fontWeight="bold"
-            fontSize="md"
-            variant="primary"
-            bgGradient="linear(90deg, #72639B 0%, #44B9DE 100%)"
-            w="100%"
-            h="50px"
-            size="large"
-            mx="auto"
-            disabled={
-              !cnvBalance.data ||
-              +cnvBalance.data?.numerator.toString() === 0 ||
-              +stakeInput.numerator.toString() === 0 ||
-              stakeInput.greaterThan(cnvBalance.data?.numerator)
-            }
-          >
-            {+stakeInput.numerator?.toString() > +cnvBalance.data?.numerator.toString() ||
-            +cnvBalance.data?.numerator.toString() === 0
-              ? 'Insufficient CNV'
-              : 'Stake CNV'}
-          </Button>
-        )}
+        <Box mt={10} width="350px">
+          {approvedCNV && (
+            <ApproveButton
+              fontWeight="bold"
+              fontSize="md"
+              variant="primary"
+              bgGradient="linear(90deg, #72639B 0%, #44B9DE 100%)"
+              w="100%"
+              h="50px"
+              size="large"
+              mx="auto"
+              useApproveInfo={approveStatus}
+            />
+          )}
+          {!approvedCNV && (
+            <Button
+              mt={5}
+              onClick={async () => {
+                const contract = new StakingV1Contract(netWorkdId, signer)
+                contract
+                  .lock(account?.address, stakeInput.numerator.toString(), props.poolId)
+                  .then((x) => {
+                    setTx(x)
+                    onOpen()
+                    console.log(x)
+                  })
+                  .catch((e) => {
+                    console.log(e)
+                  })
+              }}
+              fontWeight="bold"
+              fontSize="md"
+              variant="primary"
+              bgGradient="linear(90deg, #72639B 0%, #44B9DE 100%)"
+              w="100%"
+              h="50px"
+              size="large"
+              mx="auto"
+              disabled={
+                !cnvBalance.data ||
+                +cnvBalance.data?.numerator.toString() === 0 ||
+                +stakeInput.numerator.toString() === 0 ||
+                stakeInput.greaterThan(cnvBalance.data?.numerator)
+              }
+            >
+              {+stakeInput.numerator?.toString() > +cnvBalance.data?.numerator.toString() ||
+              +cnvBalance.data?.numerator.toString() === 0
+                ? 'Insufficient CNV'
+                : 'Stake CNV'}
+            </Button>
+          )}
 
-        {/* <Button
+          {/* <Button
           mt={5}
           onClick={() => router.push('/dashboard')}
           fontWeight="bold"
@@ -94,11 +100,13 @@ function StakeInput(props: { poolId: number; period: string; onClose: () => void
           h="40px"
           size="large"
           mx="auto"
-        >
+          >
           Check position
         </Button> */}
+        </Box>
       </Box>
-    </Box>
+      <TransactionSubmittedDialog isOpen={isOpen} tx={tx} />
+    </>
   )
 }
 
