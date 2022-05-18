e54649fd7f8759bd7b1cb65d26680d7ad0e9b817
psgb-r3763 <psgb@protonmail.com>
2022-05-18 02:53:10 -0700

    truncatenumber remove console log

--- apps/cave/utils/truncateNumber.ts
+++ apps/cave/utils/truncateNumber.ts
@@ -2,7 +2,6 @@ import { JSBI } from '@concave/gemswap-sdk'
 import { UseCurrencyBalanceData } from 'hooks/useCurrencyBalance'
 
 export const truncateNumber = (valueToTruncate: number | UseCurrencyBalanceData | JSBI) => {
-  console.log(typeof valueToTruncate, valueToTruncate)
   const truncatedBalance = parseFloat(
     (+valueToTruncate.toString() / 10 ** 18).toString().match(/^-?\d+(?:.\d{0,2})?/)[0],
   ).toLocaleString()
