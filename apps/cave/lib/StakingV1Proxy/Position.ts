daed4a170fbe1906ea26cdf39a58ae1c7da9dde8
David Schrammel <schrammel@live.com>
2022-05-18 17:17:53 -0300

    feat(cave/lib/ConcaveMarketPlace): migrate position class to type

--- apps/cave/lib/StakingV1Proxy/Position.ts
+++ apps/cave/lib/StakingV1Proxy/Position.ts
@@ -1,22 +1,9 @@
 import { BigNumber } from 'ethers'
 
-export class Position {
-  public readonly deposit: BigNumber
-  public readonly maturity: number
-  public readonly poolID: number
-  public readonly rewardDebt: BigNumber
-  public readonly shares: BigNumber
-  constructor(obj: {
-    deposit: BigNumber
-    maturity: number
-    poolID: number
-    rewardDebt: BigNumber
-    shares: BigNumber
-  }) {
-    this.deposit = obj.deposit
-    this.maturity = obj.maturity
-    this.poolID = obj.poolID
-    this.rewardDebt = obj.rewardDebt
-    this.shares = obj.shares
-  }
+export type Position = {
+  readonly deposit: BigNumber
+  readonly maturity: number
+  readonly poolID: number
+  readonly rewardDebt: BigNumber
+  readonly shares: BigNumber
 }
