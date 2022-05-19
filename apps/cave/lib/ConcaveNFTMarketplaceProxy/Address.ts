a384ae976d22d892117afa08bf8a9d8a8f554503
David Schrammel <schrammel@live.com>
2022-05-18 22:32:32 -0300

    Revert "feat(cave/lib/ConcaveMarketPlace): update contract address"
    This reverts commit 5f31d19e6130c8d0e06e7a843c3a8d12f4419c2f.

--- apps/cave/lib/ConcaveNFTMarketplaceProxy/Address.ts
+++ apps/cave/lib/ConcaveNFTMarketplaceProxy/Address.ts
@@ -1,5 +1,5 @@
 import { chain } from 'wagmi'
 
 export const ConcaveNFTMarketplaceProxy = {
-  [chain.ropsten.id]: '0x07327A7a062E7776780074D4E028aA57C28E753F',
+  [chain.ropsten.id]: '0x6dB3Bf5cD3Eb84Fbe0985418f383ef08f4d3c039',
 }
