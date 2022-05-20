4417fc66f3751fa8d6b7978eae503d0a82ef1ef9
concave-engineering <erickdelgado@protonmail.com>
2022-05-20 10:48:40 -0400

    ...

--- apps/cave/lib/StakingV1Proxy/Address.ts
+++ apps/cave/lib/StakingV1Proxy/Address.ts
@@ -1,5 +1,5 @@
 import { chain } from 'wagmi'
 
 export const StakingV1ProxyAddress = {
-  [chain.ropsten.id]: '0x56dbf6D32357AAc484f038F34216A1f1551f6598',
+  [chain.ropsten.id]: '0x533BB1236D14c8477d4248442fD4b062b1cbc463',
 }
