fd1aa6cff43c560ddfe944cac3a098f5bd2c9c20
21Chani <dmendoncacosta03@gmail.com>
2022-05-20 23:53:21 -0300

    your wallet clean ups && second confirmation dialog

---
+++ apps/cave/components/YourWallet/Containers/RecentTransactionsContainer.tsx
@@ -0,0 +1,110 @@
+import { CheckIcon, CloseIcon, SpinnerIcon } from '@concave/icons'
+import { Flex, keyframes, Text, useDisclosure } from '@concave/ui'
+import SecondConfirmModal from 'components/SecondConfirmModal'
+import { commify } from 'ethers/lib/utils'
+import { RecentTransaction, useRecentTransactions } from 'hooks/useRecentTransactions'
+import { useWaitForTransaction } from 'wagmi'
+
+export default function RecentTransactionsContainer() {
+  const { data: recentTransactions, clearRecentTransactions } = useRecentTransactions()
+
+  const { isOpen: isDialogOpen, onOpen: onOpenDialog, onClose: onCloseDialog } = useDisclosure()
+
+  return (
+    <Flex flex={1} direction={'column'} mt={8} mb={4}>
+      <Flex justify={'space-between'}>
+        <Text fontWeight={'700'} fontSize="md" textColor={'text.low'}>
+          Recent Transactions:
+        </Text>
+        <Text
+          cursor={'pointer'}
+          onClick={onOpenDialog}
+          fontWeight={'700'}
+          fontSize="md"
+          textColor={'text.low'}
+        >
+          Clear all
+        </Text>
+      </Flex>
+
+      {/* Confimation Modal --------------- */}
+      <SecondConfirmModal
+        onConfirm={() => {
+          clearRecentTransactions()
+          onCloseDialog()
+        }}
+        onClose={onCloseDialog}
+        title="You are sure?"
+        isOpen={isDialogOpen}
+      >
+        <Flex
+          m={4}
+          width={'300px'}
+          justify="center"
+          height="90%"
+          shadow={'Down Medium'}
+          rounded="2xl"
+        >
+          <Flex mt={6} maxW={'240px'} direction="column" justify={'space-between'}>
+            <Text fontSize={'16px'} textAlign="center" textColor={'text.low'} fontWeight="bold">
+              If you confirm all your recents transactions will be cleared from your localstorage
+            </Text>
+            <Text textColor={'text.low'} opacity={0.6} textAlign="center" mb={2} fontWeight="bold">
+              This action can not be reverted
+            </Text>
+          </Flex>
+        </Flex>
+      </SecondConfirmModal>
+      {/* -------------------------- */}
+
+      <Flex direction={'column'} mt={3} gap={1}>
+        {recentTransactions.map((value, index) => (
+          <TransactionInfo key={index} recentTransaction={value} />
+        ))}
+      </Flex>
+    </Flex>
+  )
+}
+
+const TransactionInfo = ({ recentTransaction }: { recentTransaction: RecentTransaction }) => {
+  const { type, amount, transaction, purchase, stakePool } = recentTransaction
+  const [{ data: txData, loading, error }] = useWaitForTransaction({ hash: transaction.hash })
+
+  console.log(loading)
+
+  const info =
+    type === 'Stake'
+      ? `${commify(amount.toFixed(2))} CNV in ${stakePool} Position`
+      : type === 'Bond'
+      ? `${commify(amount.toFixed(2))} DAI bonded for ${commify(purchase.toFixed(2))} CNV`
+      : `${commify(amount.toFixed(2))} Swaped`
+
+  return (
+    <Flex justify={'space-between'}>
+      <Flex fontWeight={'bold'} gap={1} align="center">
+        <Text>{recentTransaction.type}</Text>
+        <Text fontSize={'14px'} textColor={'text.low'}>
+          {info + ' ->'}
+        </Text>
+      </Flex>
+      {/* Status 0 = Fail  */}
+      {/* Status 1 = Success  */}
+      {txData?.status == 0 && (
+        <CloseIcon width={'12px'} height="12px" color={'red.300'} justifySelf="end" />
+      )}
+      {loading && (
+        <SpinnerIcon
+          animation={`${spin} 3s linear infinite`}
+          color={'text.low'}
+          justifySelf="end"
+        />
+      )}
+      {txData?.status == 1 && <CheckIcon color={'green.300'} justifySelf="end" />}
+    </Flex>
+  )
+}
+
+const spin = keyframes({
+  '0%': { transform: 'rotate(0deg)' },
+  '100%': { transform: 'rotate(360deg)' },
+})
--- apps/cave/components/YourWalletModal.tsx
+++ apps/cave/components/YourWalletModal.tsx
@@ -1,11 +1,20 @@
-import { Modal, ModalContent, ModalOverlay, ModalBody, Avatar, keyframes } from '@chakra-ui/react'
-import { CheckIcon, CloseIcon, SpinIcon, SpinnerIcon } from '@concave/icons'
-import { Button, Card, Flex, Text, useDisclosure, Image } from '@concave/ui'
-import { useAccount, useConnect, useNetwork, useWaitForTransaction } from 'wagmi'
-import { ConnectWalletModal, ellipseAddress } from './ConnectWallet'
-import ChangeNetWorkdModal from './ChangeNetworkModal'
-import { RecentTransaction, useRecentTransactions } from 'hooks/useRecentTransactions'
-import { commify } from 'ethers/lib/utils'
+import {
+  Modal,
+  ModalContent,
+  ModalOverlay,
+  ModalBody,
+  useDisclosure,
+  Flex,
+  Button,
+  Text,
+  Card,
+} from '@concave/ui'
+import { useAccount } from 'wagmi'
+import { useRecentTransactions } from 'hooks/useRecentTransactions'
+import YourWalletContainer from './YourWallet/Containers/YourWalletContainer'
+import ConnectedAreasContainer from './YourWallet/Containers/ConnectedsAreaContainer'
+import RecentTransactionsContainer from './YourWallet/Containers/RecentTransactionsContainer'
+import { ellipseAddress } from './ConnectWallet'
 
 interface YourWalletModalProps {
   isOpen: boolean
@@ -14,212 +23,46 @@ interface YourWalletModalProps {
 
 export default function YourWalletModal(props: YourWalletModalProps) {
   const [{ data: account }, disconnect] = useAccount()
-  const { isOpen, onClose, onOpen } = useDisclosure()
-  const {
-    isOpen: isChangeNetworkOpen,
-    onClose: onCloseChangeNetwork,
-    onOpen: onOpenChangeNetwork,
-  } = useDisclosure()
-  const [{ data: networkData }, switchNetwork] = useNetwork()
-  const [{ data: connectorData, loading: loadingWallet }] = useConnect()
-
-  const { data: recentTransactions, clearRecentTransactions } = useRecentTransactions()
 
   return (
     <Modal
+      title=""
+      hideClose
       motionPreset="slideInBottom"
       preserveScrollBarGap
       isOpen={props.isOpen}
       onClose={props.onClose}
       isCentered
     >
-      <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(10px)" />
-      <ModalContent>
-        <ModalBody>
-          <Card
-            variant="primary"
-            direction={'column'}
-            width={'400px'}
-            minHeight="500px"
-            textShadow={'0px 0px 27px rgba(129, 179, 255, 0.31)'}
-          >
-            <Flex
-              rounded={'2xl'}
-              width={'380px'}
-              height="320px"
-              mx={'auto'}
-              mt={2}
-              direction="column"
-              shadow={'up'}
-            >
-              {/* Your Wallet Container */}
-              <Flex height="52%" justify="space-between">
-                <Flex ml={8} direction={'column'} align={'start'} justify="center">
-                  <Text fontWeight={'bold'} textColor={'text.low'} fontSize="20px">
-                    Your Wallet
-                  </Text>
-                  <Text fontWeight={'bold'} fontSize={'3xl'}>
-                    {ellipseAddress(account?.address)}
-                  </Text>
-                </Flex>
-                <Flex align={'start'} height={'full'} mt={3} mr={3}>
-                  <CloseIcon color={'text.low'} cursor="pointer" onClick={props.onClose} />
-                </Flex>
-              </Flex>
-              {/* ------------------------- */}
-              {/* Connected areas Container */}
-              <Flex direction={'column'} gap={4} mx="auto">
-                <Flex
-                  width={'340px'}
-                  rounded="3xl"
-                  height={'60px'}
-                  shadow="down"
-                  align={'center'}
-                  justify="space-between"
-                >
-                  <Text ml={4} fontSize="13px" fontWeight="700" textColor={'text.low'}>
-                    Connected with {connectorData?.connector?.name}
-                  </Text>
-                  <Button
-                    mr={2}
-                    rounded={'3xl'}
-                    onClick={onOpen}
-                    _focus={{}}
-                    width={'102px'}
-                    height="40px"
-                    boxShadow="Up Big"
-                  >
-                    <Text my={'auto'} mx="auto" fontWeight={'bold'} fontSize="lg">
-                      Change
-                    </Text>
-                  </Button>
-                </Flex>
-                <Flex
-                  width={'340px'}
-                  justify="space-between"
-                  rounded="3xl"
-                  height={'60px'}
-                  shadow="down"
-                  align={'center'}
-                >
-                  <Text fontSize="13px" ml={4} fontWeight="700" textColor={'text.low'}>
-                    Change Network
-                  </Text>
-                  <Button
-                    onClick={onOpenChangeNetwork}
-                    rounded={'3xl'}
-                    _focus={{}}
-                    height="40px"
-                    boxShadow="Up Big"
-                    mr={2}
-                  >
-                    <Flex justify={'end'} flex={1} gap={2}>
-                      <Avatar
-                        shadow={'0px 0px 20px 0px #111'}
-                        ml={3}
-                        width="25px"
-                        height={'25px'}
-                        mt={'2px'}
-                        src={
-                          networkData?.chain.id == 1
-                            ? 'https://images-ext-2.discordapp.net/external/6xv9IUyAOWmlbQE8LfBQNr5pBjHIADMgOmbHWKMbutg/https/raw.githubusercontent.com/ConcaveFi/assets/master/blockchains/ethereum/info/logo.png'
-                            : 'https://raw.githubusercontent.com/ConcaveFi/assets/master/blockchains/arbitrum/info/logo.png'
-                        }
-                      />
-                      <Text my={'auto'} mr={4} fontWeight={'bold'} fontSize="lg">
-                        {networkData?.chain?.name}
-                      </Text>
-                    </Flex>
-                  </Button>
-                  <ChangeNetWorkdModal
-                    isOpen={isChangeNetworkOpen}
-                    onClose={onCloseChangeNetwork}
-                  />
-                </Flex>
-              </Flex>
-              {/* ------------------------- */}
-            </Flex>
-            <ConnectWalletModal isOpen={isOpen} onClose={onClose} />
-            {/* Transactions Container */}
-            <Flex flex={1} direction={'column'} mx={8} my={6}>
-              <Flex justify={'space-between'}>
-                <Text fontWeight={'700'} fontSize="md" textColor={'text.low'}>
-                  Recent Transactions:
-                </Text>
-                <Text
-                  cursor={'pointer'}
-                  onClick={clearRecentTransactions}
-                  fontWeight={'700'}
-                  fontSize="md"
-                  textColor={'text.low'}
-                >
-                  Clear all
-                </Text>
-              </Flex>
-              <Flex direction={'column'} mt={3} gap={1}>
-                {recentTransactions.map((value, index) => (
-                  <TransactionInfo key={index} recentTransaction={value} />
-                ))}
-              </Flex>
-            </Flex>
-            <Button
-              _focus={{}}
-              mx={'auto'}
-              width={'180px'}
-              height="40px"
-              rounded="16px 16px 0px 0px"
-              variant={'primary'}
-              onClick={disconnect}
-            >
-              <Text fontSize={'xl'} fontWeight="bold">
-                Disconnect
-              </Text>
-            </Button>
-          </Card>
-        </ModalBody>
-      </ModalContent>
+      <Flex
+        direction={'column'}
+        rounded={'2xl'}
+        w={'380px'}
+        h="320px"
+        mx={'auto'}
+        m={-3}
+        shadow={'up'}
+      >
+        <YourWalletContainer onClose={props.onClose} value={ellipseAddress(account?.address)} />
+        <ConnectedAreasContainer />
+      </Flex>
+
+      <RecentTransactionsContainer />
+
+      <Button
+        _focus={{}}
+        mx={'auto'}
+        width={'180px'}
+        height="40px"
+        rounded="16px 16px 0px 0px"
+        variant={'primary'}
+        onClick={disconnect}
+        mb={-6}
+      >
+        <Text fontSize={'xl'} fontWeight="bold">
+          Disconnect
+        </Text>
+      </Button>
     </Modal>
   )
 }
-const TransactionInfo = ({ recentTransaction }: { recentTransaction: RecentTransaction }) => {
-  const { type, amount, transaction, purchase, stakePool } = recentTransaction
-  const [{ data: txData, loading, error }] = useWaitForTransaction({ hash: transaction.hash })
-
-  console.log(loading)
-
-  const info =
-    type === 'Stake'
-      ? `${commify(amount.toFixed(2))} CNV in ${stakePool} Position`
-      : type === 'Bond'
-      ? `${commify(amount.toFixed(2))} DAI bonded for ${commify(purchase.toFixed(2))} CNV`
-      : `${commify(amount.toFixed(2))} Swaped`
-
-  return (
-    <Flex justify={'space-between'}>
-      <Flex fontWeight={'bold'} gap={1} align="center">
-        <Text>{recentTransaction.type}</Text>
-        <Text fontSize={'14px'} textColor={'text.low'}>
-          {info + ' ->'}
-        </Text>
-      </Flex>
-      {/* Status 0 = Fail  */}
-      {/* Status 1 = Success  */}
-      {txData?.status == 0 && (
-        <CloseIcon width={'12px'} height="12px" color={'red.300'} justifySelf="end" />
-      )}
-      {loading && (
-        <SpinnerIcon
-          animation={`${spin} 3s linear infinite`}
-          color={'text.low'}
-          justifySelf="end"
-        />
-      )}
-      {txData?.status == 1 && <CheckIcon color={'green.300'} justifySelf="end" />}
-    </Flex>
-  )
-}
-
-const spin = keyframes({
-  '0%': { transform: 'rotate(0deg)' },
-  '100%': { transform: 'rotate(360deg)' },
-})
