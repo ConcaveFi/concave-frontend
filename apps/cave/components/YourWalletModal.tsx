0e298ae6c150e3a77e1831c7c17d6459a9b7bf83
21Chani <dmendoncacosta03@gmail.com>
2022-05-22 09:02:57 -0300

    Disconnect dialog

--- apps/cave/components/YourWalletModal.tsx
+++ apps/cave/components/YourWalletModal.tsx
@@ -15,6 +15,7 @@ import YourWalletContainer from './YourWallet/Containers/YourWalletContainer'
 import ConnectedAreasContainer from './YourWallet/Containers/ConnectedsAreaContainer'
 import RecentTransactionsContainer from './YourWallet/Containers/RecentTransactionsContainer'
 import { ellipseAddress } from './ConnectWallet'
+import SecondConfirmModal from './SecondConfirmModal'
 
 interface YourWalletModalProps {
   isOpen: boolean
@@ -23,6 +24,8 @@ interface YourWalletModalProps {
 
 export default function YourWalletModal(props: YourWalletModalProps) {
   const [{ data: account }, disconnect] = useAccount()
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   return (
     <Modal
@@ -58,13 +61,27 @@ export default function YourWalletModal(props: YourWalletModalProps) {
         height="40px"
         rounded="16px 16px 0px 0px"
         variant={'primary'}
-        onClick={disconnect}
+        onClick={onOpen}
         mb={-6}
       >
         <Text fontSize={'xl'} fontWeight="bold">
           Disconnect
         </Text>
       </Button>
+      <SecondConfirmModal
+        isOpen={isOpen}
+        onClose={onClose}
+        onConfirm={disconnect}
+        title="You want disconnet?"
+      >
+        <Flex shadow="Down Medium" height={'90%'} w={'250px'} rounded="2xl" align={'center'}>
+          <Flex mx={8} textAlign="center">
+            <Text fontWeight={'bold'} fontSize="md" textColor={'text.low'}>
+              If you click on confirm you will disconnet this current wallet
+            </Text>
+          </Flex>
+        </Flex>
+      </SecondConfirmModal>
     </Modal>
   )
 }
