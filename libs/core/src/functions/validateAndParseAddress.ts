32793d68a31bcd6257b9f1fc56df73a347949300
gregs <ukfaeku@gmail.com>
2022-05-23 20:14:27 -0300

    validateAddress output checksummed address in warning

--- libs/core/src/functions/validateAndParseAddress.ts
+++ libs/core/src/functions/validateAndParseAddress.ts
@@ -6,7 +6,10 @@ import warning from 'tiny-warning'
 export function validateAndParseAddress(address: string): string {
   try {
     const checksummedAddress = getAddress(address)
-    warning(address === checksummedAddress, `${address} is not checksummed.`)
+    warning(
+      address === checksummedAddress,
+      `${address} is not checksummed.\nPlease use ${checksummedAddress} instead.`,
+    )
     return checksummedAddress
   } catch (error) {
     invariant(false, `${address} is not a valid address.`)
