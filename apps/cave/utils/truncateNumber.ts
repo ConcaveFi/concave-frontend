6fe0c28de97242672c5e84261ccbd91e7370783c
jgwentworth92 <95505687+jgwentworth92@users.noreply.github.com>
2022-05-24 15:57:41 -0500

    updated trunctation number function

--- apps/cave/utils/truncateNumber.ts
+++ apps/cave/utils/truncateNumber.ts
@@ -1,10 +1,27 @@
 import { JSBI } from '@concave/gemswap-sdk'
-import { UseCurrencyBalanceData } from 'hooks/useCurrencyBalance'
+import { useCurrencyBalance, UseCurrencyBalanceData } from 'hooks/useCurrencyBalance'
 
-export const truncateNumber = (valueToTruncate: number | UseCurrencyBalanceData | JSBI) => {
-  const truncatedBalance = parseFloat(
-    (+valueToTruncate.toString() / 10 ** 18).toString().match(/^-?\d+(?:.\d{0,2})?/)[0],
-  ).toLocaleString()
+export const truncateNumber = (
+  valueToTruncate: number | UseCurrencyBalanceData | JSBI,
+  decimal?: number,
+) => {
+  let decimalPlaces = decimal || 2
+  let value = (Number(valueToTruncate.toString()) / 10 ** 18).toString()
 
-  return truncatedBalance
+  const pointIndex = value.indexOf('.')
+
+  let trunctNum = value
+    .slice(0, pointIndex > -1 ? decimalPlaces + pointIndex + 1 : undefined)
+    .toLocaleString()
+  const countAfterDecimal = pointIndex > -1 ? trunctNum.substring(pointIndex).length : 0
+
+  if (decimalPlaces >= countAfterDecimal) {
+    const padLength =
+      pointIndex > -1 ? trunctNum.length - countAfterDecimal + decimalPlaces + 1 : decimalPlaces + 1
+
+    trunctNum =
+      pointIndex > -1 ? trunctNum.padEnd(padLength, '0') : trunctNum + '.'.padEnd(padLength, '0')
+  }
+
+  return trunctNum.toLocaleString()
 }
