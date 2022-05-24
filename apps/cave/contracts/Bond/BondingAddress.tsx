8e870f4e000b30e32399a68a2457dfcb0decc553
psgb-r3763 <psgb@protonmail.com>
2022-05-23 21:18:28 -0700

    update addresses

--- apps/cave/contracts/Bond/BondingAddress.tsx
+++ apps/cave/contracts/Bond/BondingAddress.tsx
@@ -3,5 +3,5 @@ type AddressMap = { [chainId: number]: string }
 export const BOND_ADDRESS: AddressMap = {
   1: '',
   3: '0x82335F5F77D04b95AA09543Bc37Fa70269b0E1d1',
-  4: '0xc4c92269618ec31E5dCDA7b201298f172735D883',
+  4: '0x5738218e81eFe2cc0a87f52eb43a0aed7722a576',
 }
