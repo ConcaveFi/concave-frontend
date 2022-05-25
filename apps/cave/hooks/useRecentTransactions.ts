1082d44cd863d4030fa416b8d7834123e266c78a
21Chani <dmendoncacosta03@gmail.com>
2022-05-24 22:04:52 -0300

    useRecentTransactions updates

--- apps/cave/hooks/useRecentTransactions.ts
+++ apps/cave/hooks/useRecentTransactions.ts
@@ -1,5 +1,6 @@
 import { Transaction } from 'ethers'
 import { useEffect, useState } from 'react'
+import { useQuery } from 'react-query'
 import { useWaitForTransaction } from 'wagmi'
 import { useIsMounted } from './useIsMounted'
 
@@ -9,48 +10,63 @@ export function useRecentTransactions() {
   const wait = useWaitForTransaction()[1]
   const isMounted = useIsMounted()
   const data = isMounted ? getRecentTransactions() : new Map<String, RecentTransaction>()
+  const [txData, setTxData] = useState<RecentTransaction[]>(
+    JSON.parse(localStorage.getItem('recentTransactions2') || '[]'),
+  )
   const [verified, setVerified] = useState(false)
 
   const [test, setTest] = useState(0)
-  const [anyPendingTx, setAnyPendingTx] = useState(false)
 
   const addRecentTransaction = (recentTx: RecentTransaction) => {
     data.set(recentTx.transaction.hash, recentTx)
+    setTxData((oldData) => {
+      const newData = [...oldData, recentTx]
+      localStorage.setItem('recentTransactions2', JSON.stringify(newData))
+      return newData
+    })
     localStorage.setItem('recentTransactions', JSON.stringify(Array.from(data)))
-    setTimeout(() => {
-      setVerified(false)
-    }, 200)
   }
 
-  useEffect(() => {
-    if (!verified && data.size > 0) {
-      const hasUnNotLoaded = Array.from(data).filter((value) => value[1].loading).length > 0
-      if (hasUnNotLoaded) {
-        data.forEach((value) => {
-          if (!value.loading) return
-          wait({ hash: value.transaction.hash })
-            .then((e) => {
-              console.log('finished')
-              data.set(value.transaction.hash, { ...value, loading: e.data.status === 2 })
-              localStorage.setItem('recentTransactions', JSON.stringify(Array.from(data)))
-            })
-            .catch((e) => {})
-        })
-      }
-    }
+  useQuery(['transactions'], async () => {
+    if (data.size === 0) return false
+    if (anyPendingTx) return false
 
-    setVerified(true)
-  }, [data])
-
-  useEffect(() => {
-    setInterval(() => {
-      console.log(
-        'current value ->' + (Array.from(data).filter((value) => value[1].loading).length > 0),
+    console.log('entered here')
+    txData
+      .filter((recentTx) => recentTx.loading)
+      .map(
+        (value) => wait({ hash: value.transaction.hash }),
+        // .then((tx) => {
+        //   console.log('finished')
+        //   data.set(value.transaction.hash, { ...value, loading: tx.data.status === 2 })
+        //   localStorage.setItem('recentTransactions', JSON.stringify(Array.from(data)))
+        // })
+        // .catch((e) => {}),
       )
+  })
 
-      setAnyPendingTx(Array.from(data).filter((value) => value[1].loading).length > 0)
-    }, 3000)
-  }, [])
+  const anyPendingTx = Array.from(data).filter((value) => value[1].loading).length > 0
+
+  // useEffect(() => {
+  //   if (!verified && data.size > 0 && anyPendingTx) {
+  //     console.log('entered here')
+  //     data.forEach((value) => {
+  //       if (!value.loading) return
+
+  //       wait({ hash: value.transaction.hash })
+  //         .then((e) => {
+  //           console.log('finished')
+  //           data.set(value.transaction.hash, { ...value, loading: e.data.status === 2 })
+  //           localStorage.setItem('recentTransactions', JSON.stringify(Array.from(data)))
+  //         })
+  //         .catch((e) => {})
+  //     })
+  //   }
+
+  //   setVerified(true)
+  // }, [data])
+
+  // console.log(anyPendingTx)
 
   return {
     test,
