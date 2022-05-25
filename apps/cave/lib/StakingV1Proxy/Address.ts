969f09439d1a990b1ddf1bfabb0995771da07b79
concave-engineering <erickdelgado@protonmail.com>
2022-05-25 15:44:37 -0400

    config fix

--- apps/cave/lib/StakingV1Proxy/Address.ts
+++ apps/cave/lib/StakingV1Proxy/Address.ts
@@ -1,5 +1,5 @@
 import { chain } from 'wagmi'
 
 export const StakingV1ProxyAddress = {
-  [chain.rinkeby.id]: '0xE2885eB13a53D45b714c7624830aa42585fe3693',
+  [chain.rinkeby.id]: '0xb92CeB858d33fe4E8288C84482F0d709002A9f09',
 }
