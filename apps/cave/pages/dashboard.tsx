b3757e92d236df0060a9010f7202fb1dc8e14a82
21Chani <dmendoncacosta03@gmail.com>
2022-05-26 18:13:10 -0300

    trying discover dashboard error (2)

--- apps/cave/pages/dashboard.tsx
+++ apps/cave/pages/dashboard.tsx
@@ -6,7 +6,7 @@ import { useDashBoardState } from 'contracts/DashBoard/DashBoardState'
 import React from 'react'
 
 export function Dashboard() {
-  const data = useDashBoardState()
+  // const data = useDashBoardState()
 
   return (
     <Flex align={'center'} justify="start" direction={'column'} width={'full'} textAlign="center">
