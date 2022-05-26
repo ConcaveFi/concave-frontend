e970e725c7b448c01cb408b0c4c2a801b03aa08d
concave-engineering <erickdelgado@protonmail.com>
2022-05-25 21:00:31 -0400

    ..

--- apps/cave/contracts/Bond/BondingAddress.tsx
+++ apps/cave/contracts/Bond/BondingAddress.tsx
@@ -3,5 +3,5 @@ type AddressMap = { [chainId: number]: string }
 export const BOND_ADDRESS: AddressMap = {
   1: '',
   3: '0x82335F5F77D04b95AA09543Bc37Fa70269b0E1d1',
-  4: '0xed9429512B29cDB0C383D56A1bF918811E663442',
+  4: '0xeF47C75e0a146e0B201788B7F61B1bf186b096a7',
 }
--- apps/cave/lib/StakingV1Proxy/Address.ts
+++ apps/cave/lib/StakingV1Proxy/Address.ts
@@ -1,5 +1,5 @@
 import { chain } from 'wagmi'
 
 export const StakingV1ProxyAddress = {
-  [chain.rinkeby.id]: '0x8414488C48bF56cD200D483273568162Fd924559',
+  [chain.rinkeby.id]: '0x9182df388D84ceBcb72Fab40B9045A5e01dA8299',
 }
