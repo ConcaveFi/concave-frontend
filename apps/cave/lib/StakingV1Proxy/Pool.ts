77bb2a9527eb499f1b5db7c55f15fa69329642df
shanghigh <95733088+shanghigh@users.noreply.github.com>
2022-05-26 20:27:48 -0400

    Update Pool.ts

--- apps/cave/lib/StakingV1Proxy/Pool.ts
+++ apps/cave/lib/StakingV1Proxy/Pool.ts
@@ -3,7 +3,7 @@ import { BigNumber } from 'ethers'
 export type Pool = {
   readonly balance: BigNumber
   readonly excessRatio: BigNumber
-  readonly g: BigNumber
+  readonly g: BigNumber | any
   readonly rewardsPerShare: BigNumber
   readonly supply: BigNumber
   readonly term: BigNumber
