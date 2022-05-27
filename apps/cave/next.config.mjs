1c0a1382eb58356e2ad483a83e476ae0c79fafd3
gregs <ukfaeku@gmail.com>
2022-05-27 16:43:31 -0300

    fix config

--- apps/cave/next.config.mjs
+++ apps/cave/next.config.mjs
@@ -1,12 +1,15 @@
-const path = require('path')
-
 /** * @type {import('next').NextConfig} **/
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   trailingSlash: false,
   images: {
     formats: ['image/avif', 'image/webp'],
+  },
+  async rewrites() {
+    return [
+      { source: '/', destination: '/gemswap' },
+      { source: '/swap', destination: '/gemswap' },
+    ]
   },
 }
 
