587991d053d3ebb7b0a060066ce863bbf076e3ba
shanghigh <95733088+shanghigh@users.noreply.github.com>
2022-05-26 20:37:15 -0400

    Revert "Update Pool.ts"
    This reverts commit 77bb2a9527eb499f1b5db7c55f15fa69329642df.

--- apps/cave/lib/StakingV1Proxy/Pool.ts
+++ apps/cave/lib/StakingV1Proxy/Pool.ts
@@ -3,7 +3,7 @@ import { BigNumber } from 'ethers'
 export type Pool = {
   readonly balance: BigNumber
   readonly excessRatio: BigNumber
-  readonly g: BigNumber | any
+  readonly g: BigNumber
   readonly rewardsPerShare: BigNumber
   readonly supply: BigNumber
   readonly term: BigNumber
