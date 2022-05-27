8f69261267787c274d9f393433ea4c481866aaf5
shanghigh <95733088+shanghigh@users.noreply.github.com>
2022-05-26 22:56:43 -0400

    Update Address.ts

--- apps/cave/lib/StakingV1Proxy/Address.ts
+++ apps/cave/lib/StakingV1Proxy/Address.ts
@@ -1,6 +1,5 @@
 import { chain } from 'wagmi'
 
-
 export const StakingV1ProxyAddress = {
-  [chain.rinkeby.id]: '0xa77f1E35Afa1d777930281c4D7439e57DF785086',
+  [chain.rinkeby.id]: '0x8414488C48bF56cD200D483273568162Fd924559',
 }
