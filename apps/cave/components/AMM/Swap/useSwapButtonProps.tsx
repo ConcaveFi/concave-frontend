a94b8af5436834ddb3d95458cb9ca59a3c17e674
David Schrammel <schrammel@live.com>
2022-05-28 11:44:21 -0300

    fix(cave/swap): remove approve feature of swap button props

--- apps/cave/components/AMM/Swap/useSwapButtonProps.tsx
+++ apps/cave/components/AMM/Swap/useSwapButtonProps.tsx
@@ -1,12 +1,8 @@
-import { ROUTER_ADDRESS } from '@concave/gemswap-sdk'
 import { ButtonProps } from '@concave/ui'
-import { useModals } from 'contexts/ModalsContext'
 import { isAddress } from 'ethers/lib/utils'
-import { useApprove } from 'hooks/useApprove'
 import { useCurrencyBalance } from 'hooks/useCurrencyBalance'
-import { usePermit } from 'hooks/usePermit'
 import { swapSupportedChains } from 'pages/gemswap'
-import { useAccount, useNetwork } from 'wagmi'
+import { useNetwork } from 'wagmi'
 import { NoValidPairsError } from '../hooks/usePair'
 import { InsufficientLiquidityError, UseTradeResult } from '../hooks/useTrade'
 
@@ -19,26 +15,16 @@ export const useSwapButtonProps = ({
   recipient: string
   onSwapClick: () => void
 }): ButtonProps => {
-  const [account] = useAccount()
   const [network] = useNetwork()
-
   const inputAmount = trade.data.inputAmount
   const outputAmount = trade.data.outputAmount
-
   const currencyIn = inputAmount?.currency
   const currencyInBalance = useCurrencyBalance(currencyIn, { watch: true })
-
-  const [token, spender] = [currencyIn?.wrapped, ROUTER_ADDRESS[currencyIn?.chainId]]
-  const permit = usePermit(token, spender)
-  const { allowance, ...approve } = useApprove(token, spender)
-
-  const { connectModal } = useModals()
 
   /*
     Not Connected
   */
-  if (account.loading || network.loading) return { isLoading: true }
-  if (!account.data?.address) return { children: 'Connect Wallet', onClick: connectModal.onOpen }
+  if (network.loading) return { isLoading: true }
 
   /*
     Select a token
@@ -84,20 +70,6 @@ export const useSwapButtonProps = ({
     return { children: `Not enough liquidity`, isDisabled: true }
 
   /*
-    Fetching user data (Allowance & Balance)
-  */
-  if (allowance.isLoading || currencyInBalance.isLoading) return { isLoading: true }
-  if (allowance.isError || (currencyInBalance.isError && !currencyInBalance.isFetching))
-    return { children: `Error fetching balances`, fontSize: 'lg', isDisabled: true }
-
-  /*
-    Return approve button first if no approval
-  */
-  if (currencyIn.isToken && allowance.value.isZero()) {
-    return permitOrApprove()
-  }
-
-  /*
     Enter an amount
   */
   if (!inputAmount || inputAmount?.equalTo(0))
@@ -111,13 +83,6 @@ export const useSwapButtonProps = ({
       children: `Insufficient ${inputAmount.currency.symbol}`,
       isDisabled: true,
     }
-
-  /*
-    Permit / Approve
-  */
-  if (currencyIn.isToken) {
-    permitOrApprove()
-  }
 
   if (recipient && !isAddress(recipient)) return { children: 'Invalid recipient', isDisabled: true }
 
@@ -136,27 +101,5 @@ export const useSwapButtonProps = ({
   return {
     children: 'Swap',
     onClick: onSwapClick,
-  }
-
-  function permitOrApprove() {
-    if (approve.isWaitingForConfirmation)
-      return { loadingText: 'Approve in wallet', isLoading: true }
-    if (approve.isWaitingTransactionReceipt)
-      return { loadingText: 'Waiting for approval', isLoading: true }
-    if (permit.isLoading) return { loadingText: 'Sign in wallet', isLoading: true }
-
-    const permitErroredOrWasNotInitializedYet = permit.isError || permit.isIdle
-    const allowanceIsNotEnough =
-      allowance.isSuccess &&
-      (!!allowance.amount?.lessThan(inputAmount) || +allowance.formatted === 0)
-
-    if (
-      (permitErroredOrWasNotInitializedYet && allowanceIsNotEnough) ||
-      allowanceIsNotEnough ||
-      allowance.value.isZero()
-    )
-      return !permit.isSupported || permit.isError
-        ? { children: `Approve ${currencyIn.symbol}`, onClick: () => approve.sendApproveTx() }
-        : { children: `Permit ${currencyIn.symbol}`, onClick: () => permit.signPermit() }
   }
 }
