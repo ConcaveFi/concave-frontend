71692a4c77c1e2da0d57e28dde6f56cd1c7c8f74
21Chani <dmendoncacosta03@gmail.com>
2022-05-28 11:36:43 -0300

    page not founds changes.

--- apps/cave/pages/404.tsx
+++ apps/cave/pages/404.tsx
@@ -1,11 +1,23 @@
-import { Card, Flex, Heading } from '@concave/ui'
+import { Card, Flex, Heading, Image } from '@concave/ui'
+import { useEffect, useState } from 'react'
 
 const NotFoundPage = () => {
+  const [image, setImage] = useState('')
+
+  useEffect(() => {
+    if (!image)
+      fetch('https://api.concave.lol/memes/random', { method: 'GET' })
+        .then((res) => res.json())
+        .then((json) => json.meme[0]['full_file_path'])
+        .then(setImage)
+        .catch((e) => {})
+  }, [image])
+
   return (
-    <Flex justify="center" align="center" w="100%">
-      <Card variant="secondary" p={6} gap={4} textAlign="center">
-        <Heading fontSize="2xl">Page not found</Heading>
-      </Card>
+    <Flex justify="center" align="center" w="100%" direction={'column'} gap="10">
+      <Image src="https://concave.lol/static/logo.png" boxSize={'15%'}></Image>
+      <Heading fontSize="5xl">404</Heading>
+      <Image src={image && image} boxSize="400px" rounded={'2xl'} />
     </Flex>
   )
 }
