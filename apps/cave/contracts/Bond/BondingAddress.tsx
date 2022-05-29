f28a49352b7559fd79eda268fb5d48fb8c5a8955
concave-engineering <97050605+concave-engineering@users.noreply.github.com>
2022-05-29 17:38:56 -0400

    update addy

--- apps/cave/contracts/Bond/BondingAddress.tsx
+++ apps/cave/contracts/Bond/BondingAddress.tsx
@@ -3,5 +3,5 @@ type AddressMap = { [chainId: number]: string }
 export const BOND_ADDRESS: AddressMap = {
   1: '',
   3: '0x82335F5F77D04b95AA09543Bc37Fa70269b0E1d1',
-  4: '0x7F27c6396c84E792b8420FD68628f1428cA20FB7',
+  4: '0x796f8D199D0750A14C599453DD20C8de7DA5f9bB',
 }
--- apps/cave/lib/StakingV1Proxy/Address.ts
+++ apps/cave/lib/StakingV1Proxy/Address.ts
@@ -1,5 +1,6 @@
 import { chain } from 'wagmi'
 
 export const StakingV1ProxyAddress = {
-  [chain.rinkeby.id]: '0x1e9C2Dc380a1aCfC4be6d02138167C5B6eDd2bE5',
+  1: "",
+  4: '0xB9E431Fc34152246BB28453b6ce117829E8A5B0C',
 }
