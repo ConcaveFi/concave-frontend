6478e3ed789239e20fe0df0598f9b8ca90fde2ff
21Chani <dmendoncacosta03@gmail.com>
2022-05-30 22:18:31 -0300

    Finishied rinkeby bbtCNV redemption

--- apps/cave/components/Treasury/VestedTokensDialogs/BBBTCNVRedemptionDialog.tsx
+++ apps/cave/components/Treasury/VestedTokensDialogs/BBBTCNVRedemptionDialog.tsx
@@ -1,27 +1,47 @@
-import { Box, Button, Card, Flex, Modal, Text, Input, NumericInput } from '@concave/ui'
+import { Button, Card, Flex, Modal, Text, NumericInput, useDisclosure } from '@concave/ui'
+import { TransactionErrorDialog } from 'components/TransactionErrorDialog'
+import { TransactionSubmittedDialog } from 'components/TransactionSubmittedDialog'
+import { WaitingConfirmationDialog } from 'components/WaitingConfirmationDialog'
+import { RedeemBBT_CNV_Abi } from 'contracts/VestedTokens/RedeemBbtCNVAbi'
+import { Contract, ethers, Transaction } from 'ethers'
+import { concaveProvider as provider } from 'lib/providers'
 import { useState } from 'react'
+import { useAccount, useSigner } from 'wagmi'
 import useBBTCNVRedeemable from '../Hooks/useBBTCNVRedeemable'
-import { GlassPanel } from '../TreasuryManagementCard'
-
+import useVestedTokens from '../Hooks/useVestedTokens'
 interface BBBTCNVRedemptionDialogProps {
   onClose: () => void
   isOpen: boolean
 }
 
 export default function BBBTCNVRedemptionDialog(props: BBBTCNVRedemptionDialogProps) {
+  const { isOpen: isConfirmOpen, onOpen: onOpenConfirm, onClose: onCloseConfirm } = useDisclosure()
+  const { isOpen: isSubOpen, onOpen: onOpenSub, onClose: onCloseSub } = useDisclosure()
+  const { isOpen: isErrorOpen, onOpen: onOpenError, onClose: onCloseError } = useDisclosure()
   const { onClose, isOpen } = props
-  const [value, setValue] = useState(0)
+  const [{ data: signer }] = useSigner()
+  const [{ data: account }] = useAccount()
 
-  const balance = 70
-  const redeemable = 120
-
-  const redeemableExceeded = value > redeemable && value <= balance
-  const insufficientValue = value > balance
-  const invalidValue = !value
-  const validValue = !insufficientValue && !invalidValue && !redeemableExceeded
+  const [value, setValue] = useState<number>()
+  const [tx, setTx] = useState<Transaction>()
+  const [error, setError] = useState('')
 
   const { data: redeemableValue, isLoading } = useBBTCNVRedeemable()
+  const { bbtCNVData } = useVestedTokens({ chainId: 4 })
 
+  const balance = parseFloat(bbtCNVData?.formatted)
+  const redeemableExceeded = value > +redeemableValue?.toString() && value <= balance
+  const insufficientFounds = value > balance
+  const invalidValue = !value
+  const validValue = !insufficientFounds && !invalidValue && !redeemableExceeded
+
+  // It's only working on rinkeby for now, it's necessary make it on mainnet too
+  // provider(4) it's for rinkeby network.
+  const bbtCNVContract = new Contract(
+    '0x1e61c955e49e4Ba3020a316739A3472BA07F1964',
+    RedeemBBT_CNV_Abi,
+    provider(4),
+  )
   return (
     <>
       <Modal
@@ -54,7 +74,7 @@ export default function BBBTCNVRedemptionDialog(props: BBBTCNVRedemptionDialogPr
               />
               <Button
                 _focus={{}}
-                onClick={() => setValue(redeemable)}
+                onClick={() => setValue(parseFloat(redeemableValue?.toString()))}
                 variant={'primary.outline'}
                 width="110px"
               >
@@ -85,10 +105,28 @@ export default function BBBTCNVRedemptionDialog(props: BBBTCNVRedemptionDialogPr
               Redeemable:
             </Text>
             <Text textColor={'text.accent'} fontSize={'15'} fontWeight="bold">
-              ${redeemable.toFixed(2)}
+              ${!isLoading && ethers.utils.formatEther(redeemableValue)}
+              {isLoading && 'Loading...'}
             </Text>
           </Flex>
           <Button
+            onClick={() => {
+              if (!validValue) return
+              onOpenConfirm()
+              bbtCNVContract
+                .connect(signer)
+                .redeem(value, account?.address, account.address, false, { gasLimit: 5000000 })
+                .then((tx) => {
+                  onCloseConfirm()
+                  setTx(tx)
+                  onOpenSub()
+                })
+                .catch((e) => {
+                  setError('Transaction rejected')
+                  onOpenError()
+                  onCloseConfirm()
+                })
+            }}
             shadow={validValue ? 'up' : 'down'}
             fontSize={'20'}
             height={'55px'}
@@ -97,14 +135,20 @@ export default function BBBTCNVRedemptionDialog(props: BBBTCNVRedemptionDialogPr
             variant="primary.outline"
             my={4}
             textColor={!validValue && 'text.low'}
+            _active={validValue && { transform: 'scale(0.9)' }}
+            _hover={{}}
+            _focus={{}}
           >
             {redeemableExceeded && 'Redeemable Exceeded'}
             {invalidValue && 'Invalid value'}
             {validValue && 'Redeem'}
-            {insufficientValue && 'Insufficient Value'}
+            {insufficientFounds && 'Insufficient Founds'}
           </Button>
         </Card>
       </Modal>
+      <TransactionSubmittedDialog isOpen={isSubOpen} tx={tx} closeParentComponent={onCloseSub} />
+      <WaitingConfirmationDialog isOpen={isConfirmOpen} />
+      <TransactionErrorDialog error={error} isOpen={isErrorOpen} />
     </>
   )
 }
