6e797d46475a3203cc3cb49d31972b42a579ebce
21Chani <dmendoncacosta03@gmail.com>
2022-05-31 13:12:13 -0300

    Clean up

--- apps/cave/components/Treasury/Hooks/useVestedTokens.tsx
+++ apps/cave/components/Treasury/Hooks/useVestedTokens.tsx
@@ -13,19 +13,8 @@ export default function useVestedTokens({ chainId }: { chainId: number }) {
     token: '0x0000000012a0592C154D552C410030E724b2eA00',
   })
 
-  const pCNVData = false
-  //   const [{ data: pCNVData }] = useBalance({
-  //     addressOrName: account?.address,
-  //     token: pCNV_ADDRESS[networkId],
-  //   })
   return {
     aCNVData: networkId == 1 && aCNVData,
-    pCNVData,
     bbtCNVData,
   }
-}
-
-export const pCNV_ADDRESS = {
-  1: '0',
-  4: '0',
 }
