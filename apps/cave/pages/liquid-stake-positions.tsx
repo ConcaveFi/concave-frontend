ece688e872294a5a25df6d0e89bf6c60858778dc
David Schrammel <schrammel@live.com>
2022-06-01 15:51:49 -0300

    feat(cave/dashboard): header

--- apps/cave/pages/liquid-stake-positions.tsx
+++ apps/cave/pages/liquid-stake-positions.tsx
@@ -1,26 +1,16 @@
-import { Flex, Heading, Text } from '@concave/ui'
+import { Flex } from '@concave/ui'
+import UserStacksPositionsBox from 'components/Dashboard/DashboardBody/UserDashboardCard'
+import { DashboardHeader } from 'components/Dashboard/DashboardHeader'
 import DashboardMobile from 'components/Dashboard/Mobile/DashboardMobile'
-import UserStacksPositionsBox from 'components/Dashboard/UserDashboardCard'
 import { withPageTransition } from 'components/PageTransition'
 import { useStackPositions } from 'contracts/DashBoard/DashBoardState'
 import React from 'react'
 
 export function LiquidStakePositions() {
   const data = useStackPositions()
-
-  // commit
-
   return (
     <Flex align={'center'} justify="start" direction={'column'} width={'full'} textAlign="center">
-      <Heading as="h1" mt={8} mb={3} fontSize="5xl">
-        Liquid Stake Positions
-      </Heading>
-      <Flex my={3} justify={'center'} maxWidth={{ sm: '358px', lg: '1000px' }}>
-        <Text textAlign="center">
-          This is the user dashboard to claim dividends and manage your liquid NFT positions.
-        </Text>
-      </Flex>
-
+      <DashboardHeader />
       <Flex justify={'center'} position="relative">
         <UserStacksPositionsBox data={data} />
         <DashboardMobile data={data} />
