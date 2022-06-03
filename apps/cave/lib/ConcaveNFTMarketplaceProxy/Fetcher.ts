6974a1afcb131845e9185dd52e86fce3560da939
concave-engineering <97050605+concave-engineering@users.noreply.github.com>
2022-06-03 15:45:30 -0400

    Merge pull request #301 from ConcaveFi/pagination_staking
    Pagination staking

--- apps/cave/lib/ConcaveNFTMarketplaceProxy/Fetcher.ts
+++ apps/cave/lib/ConcaveNFTMarketplaceProxy/Fetcher.ts
@@ -1,4 +1,4 @@
-import { createAlchemyWeb3 } from '@alch/alchemy-web3'
+import { createAlchemyWeb3, Nft } from '@alch/alchemy-web3'
 import { NEXT_PUBLIC_ALCHEMY_ID } from 'lib/env.conf'
 import { StakingV1ProxyAddress } from 'lib/StakingV1Proxy/Address'
 import { StakingV1Contract } from 'lib/StakingV1Proxy/StakingV1Contract'
@@ -10,15 +10,29 @@ export const listAllNonFungibleTokensOnAddress = async (
   owner: string,
   chainId: number,
   contractAddresses?: string[],
+  pageKey?: string,
 ) => {
   const network = chainId === 1 ? 'mainnet' : 'rinkeby'
   const web3 = createAlchemyWeb3(`https://eth-${network}.alchemyapi.io/v2/${nftapi}`)
-  const { ownedNfts, pageKey, totalCount } = await web3.alchemy.getNfts({
+  const response = await web3.alchemy.getNfts({
     owner,
     contractAddresses,
+    pageKey,
   })
-  return ownedNfts
+  if (!response.pageKey) return response.ownedNfts
+
+  const nexPage = await listAllNonFungibleTokensOnAddress(
+    owner,
+    chainId,
+    contractAddresses,
+    response.pageKey,
+  )
+  const nfts: Nft[] = []
+  nfts.push(...response.ownedNfts)
+  nfts.push(...nexPage)
+  return nfts
 }
+
 export const listUserNonFungibleTokenInfo = async (userAddress: string, chainId: number) => {
   const stakingV1Contract = new StakingV1Contract(chainId)
   const usersNft = await listAllNonFungibleTokensOnAddress(userAddress, chainId, [
