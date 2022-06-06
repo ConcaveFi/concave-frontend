07b5198e5bde479edbf55919437c530d6f781b3e
gregs <ukfaeku@gmail.com>
2022-06-06 20:37:48 -0300

    fix /api/cnv stalled requests

--- apps/cave/pages/api/cnv.ts
+++ apps/cave/pages/api/cnv.ts
@@ -1,5 +1,4 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { json } from 'stream/consumers'
 
 type Data = {
   name: String
@@ -26,22 +25,17 @@ type MainType = {
   data: Data
 }
 
+const fetchJson = (url: RequestInfo, options?: RequestInit) =>
+  fetch(url, options).then((d) => d.json())
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  // await fetch('http://localhost:3001/api/hello')
-
-  const fetchJson = (url: RequestInfo, options?: RequestInit) =>
-    fetch(url, options).then((d) => d.json())
-  const fetchCnvData = () =>
-    fetchJson(`https://cnv-data-api.vercel.app/api/v1/cnv-spot`) as Promise<MainType>
-
-  fetchCnvData()
-    .then((cnv) => {
-      res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate')
-      res.status(200).json(JSON.stringify(cnv, null, 2))
-    })
-    .catch((error) => {
-      res.status(500).send(error)
-    })
+  try {
+    const cnvData: MainType = await fetchJson(`https://cnv-data-api.vercel.app/api/v1/cnv-spot`)
+    res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate')
+    res.status(200).json(JSON.stringify(cnvData, null, 2))
+  } catch (error) {
+    res.status(500).send(error)
+  }
 }
 
 export default handler
