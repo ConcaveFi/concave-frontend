b744df4aced79ef2ae532945c7c320019879da7c
21Chani <dmendoncacosta03@gmail.com>
2022-06-07 14:24:12 -0300

    Added filters on dashboardpage, removed from marketplace.

---
+++ apps/cave/components/NftFilters/hooks/useNftSort.tsx
@@ -0,0 +1,36 @@
+import { utils } from 'ethers'
+
+export enum NftSorter {
+  REDEEM = 'maturity',
+  DISCOUNT = 'discount',
+  PRICE = 'price',
+  TOKEN_ID = 'tokenId',
+  GAINED = 'gained',
+  INITIAL = 'initialValue',
+  CURRENT_VALUE = 'currentValue',
+  STAKE = 'poolID',
+}
+export type NftSortOrder = 'highest' | 'lowest' | 'none'
+
+export type NftSorters = { [key: string]: NftSortOrder }
+export const useNftSort = (sorters: NftSorters) => ({
+  sorter: Object.entries(sorters)
+    .map(mapToSorterFunction)
+    .reduce(reduceAllFunctionsInOne, () => 0),
+})
+
+const mapToSorterFunction =
+  ([type, order]: [NftSorter, NftSortOrder]) =>
+  (current, before) => {
+    const currentFormatted = +utils.formatEther(current[type])
+    const beforeFormatted = +utils.formatEther(before[type])
+    if (currentFormatted === beforeFormatted) return 0
+    return sortOrder(currentFormatted, beforeFormatted)[order] || -1
+  }
+const reduceAllFunctionsInOne = (currentSorter, sorterBefore) => (firstValue, secondValue) =>
+  currentSorter(firstValue, secondValue) || sorterBefore(firstValue, secondValue)
+
+const sortOrder = (first, second) => ({
+  highest: first < second && 1,
+  lowest: first > second && 1,
+})
