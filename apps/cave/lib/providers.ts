23965dbe72b1d699de010fefccbdd48ecbd73304
gregs <ukfaeku@gmail.com>
2022-06-07 02:02:56 -0300

    update wagmi context & providers

--- apps/cave/lib/providers.ts
+++ apps/cave/lib/providers.ts
@@ -6,48 +6,51 @@ import {
   NEXT_PUBLIC_INFURA_ID,
 } from './env.conf'
 
-export const infuraId = NEXT_PUBLIC_INFURA_ID
 export const concaveRPC = `https://rpc.concave.lol/v1/${NEXT_PUBLIC_CONCAVE_RPC_KEY}`
 export const concaveWSS = `wss://rpc.concave.lol/ws/v1/${NEXT_PUBLIC_CONCAVE_RPC_KEY}`
 
 const NoProviderKeyError = `Concave Provider
   You need to set at least one provider key in your .env file, examples in .env.example`
 
+const stallTimeout = 500 // timeout in milliseconds after which another provider will be attempted.
 const getFallbackProviders = (chainId: number) => {
-  const fallbackProviderConfigs = []
+  const providerConfigs = []
 
   if (NEXT_PUBLIC_CONCAVE_RPC_KEY && chainId === 1)
-    fallbackProviderConfigs.push({
-      provider: new providers.JsonRpcProvider(concaveRPC, chainId),
+    providerConfigs.push({
+      provider: new providers.StaticJsonRpcProvider(concaveRPC, chainId),
+      priority: 0,
+      stallTimeout,
+    })
+
+  if (NEXT_PUBLIC_INFURA_ID)
+    providerConfigs.push({
+      provider: new providers.InfuraProvider(chainId, NEXT_PUBLIC_INFURA_ID),
       priority: 1,
-      weight: 2,
+      stallTimeout,
     })
 
   if (NEXT_PUBLIC_ALCHEMY_ID)
-    fallbackProviderConfigs.push({
+    providerConfigs.push({
       provider: new providers.AlchemyProvider(chainId, NEXT_PUBLIC_ALCHEMY_ID),
       priority: 2,
+      stallTimeout,
     })
 
-  if (infuraId)
-    fallbackProviderConfigs.push({
-      provider: new providers.InfuraProvider(chainId, infuraId),
-      priority: 2,
-    })
+  if (providerConfigs.length === 0) throw NoProviderKeyError
 
-  if (fallbackProviderConfigs.length === 0) throw NoProviderKeyError
-
-  return fallbackProviderConfigs
+  return providerConfigs
 }
 
 export const concaveProvider = (chainId: number) =>
-  new multicallProvider.MulticallProvider(
-    new providers.FallbackProvider(getFallbackProviders(chainId), 1),
-  )
+  // new multicallProvider.MulticallProvider(
+  new providers.FallbackProvider(getFallbackProviders(chainId), 1)
+// )
 
 export const concaveWSProvider = (chainId: number) => {
   if (NEXT_PUBLIC_CONCAVE_RPC_KEY) return new providers.WebSocketProvider(concaveWSS, chainId)
-  if (infuraId) return new providers.InfuraWebSocketProvider(chainId, infuraId)
+  if (NEXT_PUBLIC_INFURA_ID)
+    return new providers.InfuraWebSocketProvider(chainId, NEXT_PUBLIC_INFURA_ID)
   if (NEXT_PUBLIC_ALCHEMY_ID)
     return new providers.AlchemyWebSocketProvider(chainId, NEXT_PUBLIC_ALCHEMY_ID)
   throw NoProviderKeyError
