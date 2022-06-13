7f69a90bcb1f03b75a51b760c52f961af0f4c97e
David Schrammel <schrammel@live.com>
2022-06-13 18:34:45 -0300

    refactor(cave/staking): variable name

--- apps/cave/pages/liquid-stake-positions.tsx
+++ apps/cave/pages/liquid-stake-positions.tsx
@@ -2,16 +2,15 @@ import { Flex } from '@concave/ui'
 import { UserDashboardCard } from 'components/Dashboard/DashboardBody/UserDashboardCard'
 import { DashboardHeader } from 'components/Dashboard/DashboardHeader'
 import { withPageTransition } from 'components/PageTransition'
-import { useStackPositions } from 'contracts/DashBoard/DashBoardState'
+import { useStakePositions } from 'contracts/DashBoard/DashBoardState'
 import React from 'react'
-
 export function LiquidStakePositions() {
-  const data = useStackPositions()
+  const stakePositions = useStakePositions()
   return (
     <Flex align={'center'} justify="start" direction={'column'} width={'full'} textAlign="center">
       <DashboardHeader />
       <Flex justify={'center'} position="relative">
-        <UserDashboardCard data={data} />
+        <UserDashboardCard stakePositions={stakePositions} />
         {/* <DashboardMobile data={data} /> */}
       </Flex>
     </Flex>
