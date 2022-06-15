e2fbd070a22f7fb7af5fedd485c4a83a45f3cca7
Diogo <dmendoncacosta03@gmail.com>
2022-06-15 15:34:55 -0300

    added ToggleButton.tsx

---
+++ apps/cave/components/ToggleButton.tsx
@@ -0,0 +1,38 @@
+import { Box, Flex, useDisclosure } from '@concave/ui'
+
+type ToggleButton = {
+  onActivate: VoidFunction
+  onDisable: VoidFunction
+}
+
+export const ToggleButton = ({ onActivate, onDisable }: ToggleButton) => {
+  const { isOpen, onToggle } = useDisclosure()
+  return (
+    <Flex
+      width={'36px'}
+      height="20px"
+      shadow={!isOpen && 'down'}
+      rounded="2xl"
+      p={'2px'}
+      bg={isOpen && 'green.400'}
+      cursor="pointer"
+      onClick={() => {
+        if (isOpen) onDisable()
+        else onActivate()
+        onToggle()
+      }}
+      transition={'0.3s all'}
+    >
+      <Box
+        boxShadow={''}
+        transition={'0.3s all'}
+        ml={isOpen && '15px'}
+        rounded={'full'}
+        width="16px"
+        height="full"
+        bg="gray.100"
+        shadow={'0px 0px 10px 0px #222'}
+      ></Box>
+    </Flex>
+  )
+}
