c5a8151748e873af598f946661dba7eccdb02344
Diogo <dmendoncacosta03@gmail.com>
2022-06-14 22:55:33 -0300

    Added sorterButton.tsx

---
+++ apps/cave/components/NftFilters/Sorters/SorterButton.tsx
@@ -0,0 +1,37 @@
+import { Flex, Text } from '@chakra-ui/react'
+import { NftSorter } from './hooks/useNftSorter'
+
+type SorterButton = {
+  title: string
+  sorter: NftSorter
+  enabled: boolean
+  onClick: (sorter: NftSorter) => void
+}
+
+export const SorterButton = ({ sorter, title, enabled, onClick }: SorterButton) => {
+  return (
+    <Flex
+      width="full"
+      p={1}
+      textColor={enabled ? 'white' : 'text.low'}
+      _hover={{ bg: 'secondary.125', textColor: 'gray.300' }}
+      px={2}
+      cursor="pointer"
+      transition={'.4s all'}
+      borderBottom={'1px '}
+      borderColor="text.low"
+      bg="secondary.75"
+      onClick={() => onClick(sorter)}
+    >
+      <Text
+        userSelect="none"
+        fontWeight={'bold'}
+        fontSize={'14px'}
+        textAlign={'start'}
+        width="full"
+      >
+        {title}
+      </Text>
+    </Flex>
+  )
+}
