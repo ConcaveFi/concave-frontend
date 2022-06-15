ebc12698c637fa13d611f5a6400d68f6096e7eae
Diogo <dmendoncacosta03@gmail.com>
2022-06-15 09:53:44 -0300

    changed boolean logic

--- apps/cave/components/NftFilters/Filters/hooks/useFilterByRange.ts
+++ apps/cave/components/NftFilters/Filters/hooks/useFilterByRange.ts
@@ -4,7 +4,7 @@ import { utils } from 'ethers'
 export type RangeFilter = { min?: number; max?: number }
 export const useFilterByRange = ({ min, max }: RangeFilter) => ({
   filterByRange: (nftPosition: NonFungibleTokenInfo) =>
-    !!min && !!max
+    !!min || !!max
       ? +utils.formatEther(nftPosition.initialValue) >= min &&
         +utils.formatEther(nftPosition.initialValue) <= max
       : () => true,
