80ec8aa9060fe8d0b04d283a003bf4860f4ca8f0
Diogo <dmendoncacosta03@gmail.com>
2022-06-15 15:39:44 -0300

    finished mock

--- apps/cave/components/Treasury/VestedTokensDialogs/bbtCNV/BBTCNVRedemptionDialog.tsx
+++ apps/cave/components/Treasury/VestedTokensDialogs/bbtCNV/BBTCNVRedemptionDialog.tsx
@@ -1,4 +1,6 @@
-import { Button, Card, Flex, Modal, Text, useDisclosure } from '@concave/ui'
+import { InfoIcon } from '@concave/icons'
+import { Button, Card, Flex, Modal, Text, Tooltip, useDisclosure } from '@concave/ui'
+import { ToggleButton } from 'components/ToggleButton'
 import { TransactionErrorDialog } from 'components/TransactionErrorDialog'
 import { TransactionSubmittedDialog } from 'components/TransactionSubmittedDialog'
 import { WaitingConfirmationDialog } from 'components/WaitingConfirmationDialog'
@@ -7,9 +9,11 @@ import { Contract, Transaction, utils } from 'ethers'
 import { useTransactionRegistry } from 'hooks/TransactionsRegistry'
 import { concaveProvider as provider } from 'lib/providers'
 import { useState } from 'react'
+import { MdInfo } from 'react-icons/md'
 import { useAccount, useConnect, useSigner } from 'wagmi'
-import useBBTCNVRedeemable from '../Hooks/useBBTCNVRedeemable'
-import useVestedTokens from '../Hooks/useVestedTokens'
+import useBBTCNVRedeemable from '../../Hooks/useBBTCNVRedeemable'
+import useVestedTokens from '../../Hooks/useVestedTokens'
+import { BBT_CNVDialogInput } from './BbtCNVDialogInput'
 interface BBBTCNVRedemptionDialogProps {
   onClose: () => void
   isOpen: boolean
@@ -41,10 +45,13 @@ export default function BBBTCNVRedemptionDialog(props: BBBTCNVRedemptionDialogPr
   const nothingToRedeem = (redeemable === 0 || redeemable === balance) && !insufficientFounds
   const validValue = !insufficientFounds && !nothingToRedeem
 
+  const [useMax, setUseMax] = useState(false)
+  const [value, setValue] = useState<number>()
+
   const bbtCNVContract = new Contract(
     '0xCf6B82Ca69bE4272d457c246FAF380f88af34f69',
     RedeemBBT_CNV_Abi,
-    provider(1),
+    provider(4),
   )
 
   return (
@@ -58,41 +65,43 @@ export default function BBBTCNVRedemptionDialog(props: BBBTCNVRedemptionDialogPr
         isCentered
         motionPreset="slideInBottom"
       >
-        <Card width={'300px'} height="200px" m={-6}>
-          {/* Input Container */}
-          <Flex
-            mt={4}
-            width={'270px'}
-            height="124px"
-            shadow={'Down Medium'}
-            rounded="2xl"
-            mx={'auto'}
-            direction="column"
-            py={2}
-            px={4}
-            gap={2}
-          >
-            <Flex width={'full'} justify="space-between" fontWeight={'bold'}>
-              <Text textColor={'text.low'}>Current Balance:</Text>
-              <Text textColor={'text.accent'}>{balance.toFixed(2)} bbtCNV</Text>
-            </Flex>
-            <Flex width={'full'} justify="space-between" fontWeight={'bold'}>
-              <Text textColor={'text.low'}>Redeemable:</Text>
-              <Text textColor={'text.accent'}>
-                {!isLoading && '' + redeemable.toFixed(8)}
-                {isLoading && 'Loading...'}
-              </Text>
-            </Flex>
-            <Flex width={'full'} justify="space-between" fontWeight={'bold'}>
-              <Text textColor={'text.low'}>Redeemed:</Text>
-              <Text textColor={'text.accent'}>
-                {!isLoading && '' + redeemed.toFixed(8)}
-                {isLoading && 'Loading...'}
-              </Text>
-            </Flex>
+        <Card width={'340px'} height="280px" m={-6} px={6} gap={2} justify="center">
+          <BBT_CNVDialogInput
+            balance={balance}
+            redeemable={redeemable}
+            onChangeValue={setValue}
+            value={value}
+          />
+          <Flex direction={'column'} fontSize="14px">
+            <Info title="Redeemable:" value={isLoading ? 'Loading...' : redeemable} />
+            <Info title="Redeemed:" value={isLoading ? 'Loading...' : redeemed} />
           </Flex>
-
+          <Flex gap={2} fontWeight={'bold'} pl={2} align="center">
+            <Text textColor={'gray.200'}>Try redeem max?</Text>
+            <ToggleButton onActivate={() => setUseMax(true)} onDisable={() => setUseMax(false)} />
+            <Tooltip
+              textColor={'white'}
+              bg="text.low"
+              textShadow={'0px 0px 10px #333'}
+              fontWeight={'bold'}
+              fontSize="13px"
+              textAlign="center"
+              label="If this option it's enabled, the contract will try to redeem all your redeemable amount"
+            >
+              <InfoIcon color={'text.low'} cursor="pointer" />
+            </Tooltip>
+          </Flex>
           <Button
+            cursor={!validValue && 'default'}
+            shadow={validValue ? 'Up Small' : 'down'}
+            fontSize={'20'}
+            height={'55px'}
+            width="full"
+            variant="primary.outline"
+            textColor={!validValue && 'text.low'}
+            _active={validValue && { transform: 'scale(0.9)' }}
+            _hover={validValue && { shadow: 'up' }}
+            _focus={{}}
             onClick={() => {
               if (!validValue) return
               onOpenConfirm()
@@ -115,18 +124,6 @@ export default function BBBTCNVRedemptionDialog(props: BBBTCNVRedemptionDialogPr
                   onCloseConfirm()
                 })
             }}
-            cursor={!validValue && 'default'}
-            shadow={validValue ? 'Up Small' : 'down'}
-            fontSize={'20'}
-            height={'55px'}
-            width="270px"
-            mx={'auto'}
-            variant="primary.outline"
-            my={4}
-            textColor={!validValue && 'text.low'}
-            _active={validValue && { transform: 'scale(0.9)' }}
-            _hover={validValue && { shadow: 'up' }}
-            _focus={{}}
           >
             {isConnected ? (
               <Text>
@@ -146,3 +143,10 @@ export default function BBBTCNVRedemptionDialog(props: BBBTCNVRedemptionDialogPr
     </>
   )
 }
+
+const Info = ({ title, value }: { title: string; value: string | number }) => (
+  <Flex gap={2} fontWeight={'bold'} pl={2}>
+    <Text textColor={'text.low'}>{title}</Text>
+    <Text textColor={'text.accent'}>{value}</Text>
+  </Flex>
+)
