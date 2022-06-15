4388a3c3cb8a14cc968b80e98dfa59d471cf6399
Diogo <dmendoncacosta03@gmail.com>
2022-06-14 22:41:12 -0300

    added UseNftSorter.ts

---
+++ apps/cave/components/NftFilters/Sorters/hooks/useNftSorter.ts
@@ -0,0 +1,27 @@
+import { NonFungibleTokenInfo } from '@concave/marketplace'
+import { utils } from 'ethers'
+
+export enum NftSorterType {
+  STAKE_POOL = 'poolID',
+  REDEEM_DATE = 'maturity',
+  INITIAL = 'initialValue',
+}
+
+export type NftOrder = 'ASC' | 'DESC'
+
+export type NftSorter = { sorter: NftSorterType; order: NftOrder }
+
+export const useNFtSorter = (sorter: NftSorter) => ({
+  sorter: sorter
+    ? (current, before) => {
+        const currentFormatted = +utils.formatEther(current[sorter.sorter])
+        const beforeFormatted = +utils.formatEther(before[sorter.sorter])
+        if (currentFormatted === beforeFormatted) return 0
+        return sortByOrder(currentFormatted, beforeFormatted)[sorter.order] || -1
+      }
+    : () => 0,
+})
+const sortByOrder = (first, second) => ({
+  DESC: first < second && 1,
+  ASC: first > second && 1,
+})
