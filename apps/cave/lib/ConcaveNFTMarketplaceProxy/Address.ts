5e2b6f4a3a2f8be0f58c0a3942fe37bb3d5dc113
shanghigh <95733088+shanghigh@users.noreply.github.com>
2022-06-17 20:57:19 -0400

    Create Address.ts

---
+++ apps/cave/lib/ConcaveNFTMarketplaceProxy/Address.ts
@@ -0,0 +1,6 @@
+import { chain } from 'wagmi'
+//commit
+export const ConcaveNFTMarketplaceProxy = {
+  [chain.mainnet.id]: '0x9556CcfC01542b33CFA2553c38147de712C3Ac9E',
+  [chain.rinkeby.id]: '0x9556CcfC01542b33CFA2553c38147de712C3Ac9E',
+}
