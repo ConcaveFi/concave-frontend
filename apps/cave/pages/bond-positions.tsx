eeca072b1c3704e89bd112ca6783aa1e947903ab
Diogo <dmendoncacosta03@gmail.com>
2022-06-17 22:27:33 -0300

    added components on bond-positions page

--- apps/cave/pages/bond-positions.tsx
+++ apps/cave/pages/bond-positions.tsx
@@ -1,5 +1,11 @@
 import { Flex } from '@chakra-ui/react'
+import { UserBondPosition } from 'components/BondPositions/UserBondPosition'
 
 export default function BondsPositions() {
-  return <Flex></Flex>
+  return (
+    <Flex direction={'column'} align={'center'} gap={6} justify="center" width={'full'}>
+      <UserBondPosition variant={'pending'} />
+      <UserBondPosition variant={'listed'} />
+    </Flex>
+  )
 }
