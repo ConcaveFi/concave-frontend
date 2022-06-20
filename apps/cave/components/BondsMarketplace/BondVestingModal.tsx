ebf8e9738aa5268888a8fecb0da8e8ac58982442
Diogo <dmendoncacosta03@gmail.com>
2022-06-20 10:25:55 -0300

    Fixes & changes on Bond vesting modal

--- apps/cave/components/BondsMarketplace/BondVestingModal.tsx
+++ apps/cave/components/BondsMarketplace/BondVestingModal.tsx
@@ -1,15 +1,20 @@
-import { Modal, ModalContent, ModalOverlay, useDisclosure } from '@chakra-ui/react'
+import { Flex, Modal, ModalContent, ModalOverlay, useDisclosure } from '@chakra-ui/react'
 import { BondMarketplacePosition } from './BondMarketplacePosition'
+import { BondPreferredCard } from './BondPreferredCard'
 import { BondVesting } from './BondVesting'
 
 export const BondVestingModal = () => {
   const { isOpen, onClose } = useDisclosure()
+
   return (
-    <Modal motionPreset="slideInBottom" isOpen={true} onClose={onClose} isCentered>
+    <Modal size={'5xl'} motionPreset="slideInBottom" isOpen={true} onClose={onClose} isCentered>
       <ModalOverlay backdropBlur={'8px'} />
-      <ModalContent>
-        <BondMarketplacePosition variant="primary" />
-        <BondVesting mt={'-80px'} />
+      <ModalContent flexDirection={'row'} m="auto">
+        <Flex direction={'column'} maxW="1200px">
+          <BondMarketplacePosition variant="primary" />
+          <BondVesting mt={'-60px'} />
+        </Flex>
+        <BondPreferredCard />
       </ModalContent>
     </Modal>
   )
