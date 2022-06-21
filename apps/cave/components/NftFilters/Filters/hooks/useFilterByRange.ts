a9b0b29bc0200a23015c84954ca6fe581f6c46e7
Diogo <dmendoncacosta03@gmail.com>
2022-06-21 18:51:41 -0300

    fixed Type

--- apps/cave/components/NftFilters/Filters/hooks/useFilterByRange.ts
+++ apps/cave/components/NftFilters/Filters/hooks/useFilterByRange.ts
@@ -1,9 +1,9 @@
-import { NonFungibleTokenInfo } from '@concave/marketplace'
+import { StakingPosition } from '@concave/marketplace'
 import { utils } from 'ethers'
 
 export type RangeFilter = { min?: number; max?: number }
 export const useFilterByRange = ({ min, max }: RangeFilter) => ({
-  filterByRange: (nftPosition: NonFungibleTokenInfo) =>
+  filterByRange: (nftPosition: StakingPosition) =>
     !!min || !!max
       ? +utils.formatEther(nftPosition.initialValue) >= min &&
         +utils.formatEther(nftPosition.initialValue) <= max
