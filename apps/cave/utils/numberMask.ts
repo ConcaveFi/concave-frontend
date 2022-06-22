c33b0b1fb7a563165ff919cdbb4fb2e4dd277bf1
jgwentworth92 <95505687+jgwentworth92@users.noreply.github.com>
2022-06-22 18:48:42 -0500

    peebs refactored code

--- apps/cave/utils/numberMask.ts
+++ apps/cave/utils/numberMask.ts
@@ -1,18 +1,30 @@
 import { commify } from 'ethers/lib/utils'
 
-const numSplice = (s: string | Number, decimals?: number) => {
-  const a = s.toString()
-  const _decimals = decimals + 1
-  return a.indexOf('.') > -1 ? a.slice(0, a.indexOf('.') + _decimals) : a
+/**
+ * Adds decimals if '.' character is found
+ * @param number - number to modify
+ * @param decimalCount - amount of decimal places
+ * @returns a number in string format
+ */
+const numSplice = (number: string | Number, decimalCount?: number): string => {
+  const numStr = number.toString()
+  const decimalIndex = decimalCount + 1
+  return numStr.indexOf('.') > -1 ? numStr.slice(0, numStr.indexOf('.') + decimalIndex) : numStr
 }
 
-export const numberMask = (number: Number, decimals?: number) => {
+/**
+ * Format normal numbers to `X` decimal places to make them readable.
+ * @param number - a number to be formatted
+ * @param decimals - the number of decimal places to use
+ * @returns a number in string format
+ */
+export const numberMask = (number: Number, decimals?: number): string => {
   if (number === 0) {
     return `0`
   }
   if (number < 0.01) {
     return `<.01`
   }
-  const _decimals = decimals || 2
-  return commify(numSplice(number, _decimals))
+  const decimalCount = decimals || 2
+  return commify(numSplice(number, decimalCount))
 }
