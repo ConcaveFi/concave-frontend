d7b023c9adc56e155a22311cbab3c15c6d97536d
jgwentworth92 <95505687+jgwentworth92@users.noreply.github.com>
2022-06-23 21:36:39 -0500

    small updates on two buttons on top asset cart

--- apps/cave/components/PermissionalisBonding/bondableAssetsCard.tsx
+++ apps/cave/components/PermissionalisBonding/bondableAssetsCard.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Flex, Heading, Text } from '@concave/ui'
+import { Button, Card, Flex, Heading, HStack, Input, Text } from '@concave/ui'
 import { ButtonLink } from 'components/ButtonLink'
 import React from 'react'
 import { MdOutlineDashboard } from 'react-icons/md'
@@ -7,7 +7,7 @@ export function BondableAssets() {
   return (
     <Card
       p={5}
-      gap={2}
+      gap={4}
       variant="primary"
       h="fit-content"
       shadow="Block Up"
@@ -15,21 +15,75 @@ export function BondableAssets() {
       maxW="506px"
       height="352px"
     >
-      <Flex flexDir={'column'} align="center">
+      <Flex gap={4} flexDir={'column'} align="center">
         <Flex
           shadow="down"
+          justify={'center'}
           width="474px"
           height="110px"
           alignContent={'center'}
           rounded={'2xl'}
-        ></Flex>
+          flexDirection="column"
+        >
+          <Text fontSize="18px" fontWeight="600" textColor="text.high" mb={4}>
+            Bondable Assets
+          </Text>
+          <HStack justify="center">
+            <Card variant="primary" w="100%" maxW="170px" height="34px">
+              <HStack>
+                <Flex
+                  rounded={'2xl'}
+                  width="107px"
+                  height="31px"
+                  shadow="up"
+                  bgColor="secondary.100"
+                >
+                  <Text>test</Text>
+                </Flex>
+                <Text>40%</Text>
+              </HStack>
+            </Card>
+
+            <Card variant="primary" w="100%" maxW="170px" height="34px">
+              <HStack>
+                <Flex
+                  rounded={'2xl'}
+                  width="107px"
+                  height="31px"
+                  shadow="up"
+                  bgColor="secondary.100"
+                >
+                  <Text>test</Text>
+                </Flex>
+                <Text>60%</Text>
+              </HStack>
+            </Card>
+          </HStack>
+        </Flex>
         <Flex
           shadow="down"
           width="474px"
           height="110px"
-          alignContent={'center'}
+          justifyContent="center"
           rounded={'2xl'}
-        ></Flex>
+          flexDirection="column"
+        >
+          <Text textColor="text.high" fontWeight="600" fontSize="18px">
+            Recepient Address
+          </Text>
+          <HStack mt={4} mb={4} ml={8}>
+            <Text textColor={'text.low'}>Contract:</Text>
+
+            <Input
+              alignContent={'center'}
+              justifyContent="center"
+              rounded={'2xl'}
+              shadow="down"
+              width="332px"
+              height="26px"
+            />
+          </HStack>
+        </Flex>
         <Button
           my={3}
           variant="primary"
