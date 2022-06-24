40d8f9c1c988b184cef534dfc59f022b0590fb33
Diogo <dmendoncacosta03@gmail.com>
2022-06-24 18:44:16 -0300

    fixed inital state empty

--- apps/cave/components/NftFilters/Filters/hooks/useFilterByRange.ts
+++ apps/cave/components/NftFilters/Filters/hooks/useFilterByRange.ts
@@ -4,14 +4,12 @@ import { utils } from 'ethers'
 export type RangeFilter = { min?: number; max?: number }
 export const useFilterByRange = ({ min, max }: RangeFilter) => ({
   filterByRange: (nftPosition: StakingPosition) => {
-    const allValuesEmpty = !!min && !!max
-    if (allValuesEmpty) {
-      return true
-    } else {
-      return (
-        +utils.formatEther(nftPosition.initialValue) >= min &&
-        +utils.formatEther(nftPosition.initialValue) <= max
-      )
-    }
+    const allValuesEmpty = !min && !max
+
+    if (allValuesEmpty) return () => true
+    return (
+      +utils.formatEther(nftPosition.initialValue) >= min &&
+      +utils.formatEther(nftPosition.initialValue) <= max
+    )
   },
 })
