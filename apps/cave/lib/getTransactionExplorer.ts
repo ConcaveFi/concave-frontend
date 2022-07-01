90f4cbe64b7d78827511c38762fc3f516204f67b
concave-engineering <97050605+concave-engineering@users.noreply.github.com>
2022-07-01 18:49:50 -0400

    Update getTransactionExplorer.ts

--- apps/cave/lib/getTransactionExplorer.ts
+++ apps/cave/lib/getTransactionExplorer.ts
@@ -2,5 +2,5 @@ import { ChainId, CHAIN_NAME } from '@concave/core'
 import { etherscanBlockExplorers } from 'wagmi'
 
 export const getTxExplorer = (hash: string, chainId: ChainId) => {
-  return etherscanBlockExplorers[CHAIN_NAME[chainId]].url + `/tx/${hash}`
+  return etherscanBlockExplorers[CHAIN_NAME[chainId]]?.url + `/tx/${hash}`
 }
