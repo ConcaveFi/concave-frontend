51c9a82387a849a69ba1c35348e4081375c1027f
Diogo <dmendoncacosta03@gmail.com>
2022-07-03 13:42:59 -0300

    removed unnused variables, log && cleanup

--- apps/cave/components/LiquidStaking/hooks/useLiquidValues.ts
+++ apps/cave/components/LiquidStaking/hooks/useLiquidValues.ts
@@ -1,6 +1,4 @@
 import { StakingV1Contract } from '@concave/marketplace'
-import { utils } from 'ethers'
-import { useCurrentSupportedNetworkId } from 'hooks/useCurrentSupportedNetworkId'
 import { concaveProvider } from 'lib/providers'
 import { useQuery } from 'react-query'
 
@@ -9,15 +7,10 @@ export const useLiquidValues = (chainId: number, poolId: number) => {
     const stakingV1Contract = new StakingV1Contract(concaveProvider(+chainId))
     const stakingV1Pools = await stakingV1Contract.pools(String(poolId))
     const stakingV1Cap = await stakingV1Contract.viewStakingCap(String(poolId))
-    const stakingPoolFormatted = utils.formatEther(stakingV1Pools?.balance || '0')
-    const stakingCapFormatted = utils.formatEther(stakingV1Cap || '0')
     return {
       stakingV1Pools,
       stakingV1Cap,
-      stakingPoolFormatted,
-      stakingCapFormatted,
     }
   })
-  console.log(data?.stakingPoolFormatted)
   return { data, isLoading }
 }
