6d35e23f3ac78da7176c30b821000dab041231f3
Diogo <dmendoncacosta03@gmail.com>
2022-07-05 11:52:15 -0300

    added PCNVRedemptonDialog

---
+++ apps/cave/components/Treasury/VestedTokensDialogs/PCNVRedemptionDialog.tsx
@@ -0,0 +1,25 @@
+import { Flex, Modal, Text } from '@concave/ui'
+import { VestedTokenDialodProps } from '../TreasuryRedeemCard'
+
+export const PCNVRedemptionDialog: React.FC<VestedTokenDialodProps> = ({ isOpen, onClose }) => {
+  return (
+    <Modal
+      isCentered
+      motionPreset="slideInBottom"
+      preserveScrollBarGap
+      title={''}
+      hideClose
+      onClose={onClose}
+      isOpen={isOpen}
+    >
+      <Flex width={'220px'} height="140px" direction={'column'} px="3">
+        <Text fontSize={'2xl'} fontWeight="bold" mx={'auto'} mt="2">
+          {`pCNV`}
+        </Text>
+        <Text textAlign={'center'} textColor={'text.low'} fontWeight="bold" fontSize={'lg'}>
+          {`We're busy mining the pCNV token, come back later.`}
+        </Text>
+      </Flex>
+    </Modal>
+  )
+}
