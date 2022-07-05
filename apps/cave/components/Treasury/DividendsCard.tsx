13e2eaf9a358445caf591e0964edb2c9d0e45d96
Diogo <dmendoncacosta03@gmail.com>
2022-07-05 16:08:01 -0300

    dividends card cleanup

--- apps/cave/components/Treasury/DividendsCard.tsx
+++ apps/cave/components/Treasury/DividendsCard.tsx
@@ -6,38 +6,28 @@ interface DividendsCardProps {
 export default function DividendsCard() {
   return (
     <Card
-      width={{ xl: '900px', base: '510px' }}
-      height={{ base: '300px', xl: '179px' }}
-      bg={'#111e'}
-      shadow={'0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 0px 20px rgba(87, 124, 255, 0.3)'}
-      direction={{ base: 'column', xl: 'row' }}
-      px="10"
-      mb={6}
+      direction={{ base: 'column', lg: 'row' }}
+      align="center"
+      width={{ base: '340px', md: '540px', lg: '800px', xl: '900px' }}
+      variant="secondary"
+      height={{ base: '260px', lg: '179px' }}
+      py={{ base: 2, lg: 0 }}
     >
-      <Flex
-        direction={{ base: 'column', xl: 'row' }}
-        flex={{ base: 0, xl: 1 }}
-        gap={{ base: 0, xl: 5 }}
-        py={2}
-      >
-        <Flex justify="center" align={'center'} height="full">
-          <Text my={2} fontSize={'4xl'} fontWeight="700">
-            Dividends
-          </Text>
-        </Flex>
-        <Flex flex={1} justify="center" align={'center'}>
-          <Text
-            textAlign={{ base: 'center', xl: 'start' }}
-            fontWeight={700}
-            textColor={'text.low'}
-            fontSize="14px"
-          >
-            Dividends are distributed on a quarterly basis to the holders. You may redeem these
-            dividends upon the distribution date on the My Positions page.
-          </Text>
-        </Flex>
+      <Flex direction={{ base: 'column', xl: 'row' }} align="center" mx={'auto'}>
+        <Text my={'auto'} mx="10" fontSize={'4xl'} fontWeight="700">
+          Dividends
+        </Text>
+        <Text
+          textAlign={{ base: 'center', xl: 'start' }}
+          fontWeight={700}
+          textColor={'text.low'}
+          fontSize="14px"
+          maxW={'350px'}
+        >
+          Dividends are distributed on a quarterly basis to the holders. You may redeem these
+          dividends upon the distribution date on the Your Stake Position page.
+        </Text>
       </Flex>
-      {/* <Flex flex={0.5} justify="center" align={'center'} height="full"> */}
       <Card
         width={'250px'}
         height="105px"
@@ -54,7 +44,6 @@ export default function DividendsCard() {
           Next Distribution Date
         </Text>
       </Card>
-      {/* </Flex> */}
     </Card>
   )
 }
