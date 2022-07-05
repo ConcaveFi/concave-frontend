8c1a0540ee9209d663247923de78e4606c6cffef
Diogo <dmendoncacosta03@gmail.com>
2022-07-05 15:23:07 -0300

    added info on useTreasuryData

--- apps/cave/components/Treasury/Hooks/useTreasuryData.ts
+++ apps/cave/components/Treasury/Hooks/useTreasuryData.ts
@@ -45,9 +45,10 @@ export const useTreasuryData = () => {
   const treasuryValue = total || 0
   const cnvTotalSupply = cnvData?.cnvData?.data?.totalSupply || 0
 
+  const convexToken = treasuryData?.treasury?.find((token) => token.name === 'cvxDOLA3POOL')
   return {
     lastBondSolds,
-    revenueData: {
+    treasuryData: {
       firstRow: {
         marketCap: '$' + numberMask(marketCap),
         cnvPrice: '$' + cnvPrice?.toFixed(2) || ' 0',
@@ -60,7 +61,40 @@ export const useTreasuryData = () => {
       },
     },
     isloading: false,
+    assets: {
+      tokens: treasuryData?.treasury?.filter(
+        (token, index) => token.name !== 'cvxDOLA3POOL' && index !== 0,
+      ),
+      convex: {
+        total: convexToken?.total || 0,
+        tokenImage:
+          'https://static.debank.com/image/eth_token/logo_url/0x4e3fbd56cd56c3e72c1403e103b45db9da5b9d2b/be2a9b05a223d6dfca3dc88b1838fcd4.png',
+        images: [
+          convertGithubUrl(convexToken?.image || ''),
+          convertGithubUrl(convexToken?.imageP1 || ''),
+          convertGithubUrl(convexToken?.imageP2 || ''),
+          convertGithubUrl(convexToken?.imageP3 || ''),
+        ],
+      },
+    },
   }
+}
+
+export type TreasuryTokenInfo = {
+  __typename?: 'treasury'
+  updated_at: any
+  contract: string
+  chainId?: string
+  name?: string
+  amount?: any
+  value?: any
+  rewards?: any
+  total?: any
+  image?: string
+  isLP?: boolean
+  imageP1?: string
+  imageP2?: string
+  imageP3?: string
 }
 
 export type LastBondSolds = {
@@ -68,3 +102,6 @@ export type LastBondSolds = {
   inputAmount: string
   outputAmount: string
 }[]
+
+const convertGithubUrl = (url: string) =>
+  url.replace('github', 'raw.githubusercontent').replace('blob/', '')
