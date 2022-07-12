89ce6d8638dace362175d42c807b2fb003a5108c
gregs <ukfaeku@gmail.com>
2022-07-12 11:16:35 -0300

    remove unused

--- apps/cave/utils/isMobile.ts
+++
@@ -1,8 +0,0 @@
-export const isAndroid = () =>
-  typeof navigator !== 'undefined' && /android/i.test(navigator.userAgent)
-export const isSmallIOS = () =>
-  typeof navigator !== 'undefined' && /iPhone|iPod/.test(navigator.userAgent)
-export const isLargeIOS = () => typeof navigator !== 'undefined' && /iPad/.test(navigator.userAgent)
-
-export const isIOS = () => isSmallIOS() || isLargeIOS()
-export const isMobile = () => isAndroid() || isSmallIOS()
