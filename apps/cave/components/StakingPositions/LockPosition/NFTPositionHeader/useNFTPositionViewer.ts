a2b448a471b7d6c0d2a1ee459130a60460cbb88b
Diogo <dmendoncacosta03@gmail.com>
2022-07-14 14:53:52 -0300

    added units to redeem date

--- apps/cave/components/StakingPositions/LockPosition/NFTPositionHeader/useNFTPositionViewer.ts
+++ apps/cave/components/StakingPositions/LockPosition/NFTPositionHeader/useNFTPositionViewer.ts
@@ -13,7 +13,10 @@ export const useNFTLockedPositionState = ({
   toogleActive,
 }: NFTPositionHeaderProps) => {
   const { poolID, maturity, tokenId } = stakingPosition
-  const redeemInDays = !maturity || formatDistanceToNowStrict(maturity * 1000, { unit: 'day' })
+  const redeemInDays =
+    !maturity ||
+    formatDistanceToNowStrict(maturity * 1000, { unit: getUnit(maturity * 1000 - Date.now()) })
+
   const period = {
     0: '360 Days',
     1: '180 Days',
@@ -39,4 +42,12 @@ export const useNFTLockedPositionState = ({
     active,
     toogleActive,
   }
+}
+
+function getUnit(time: number): 'hour' | 'minute' | 'day' {
+  // 86400000 = 24 hours in miliseconds
+  // 3600000 = 1 hours in miliseconds
+  if (time < 86400000) return 'hour'
+  if (time < 3600000) return 'minute'
+  return 'day'
 }
