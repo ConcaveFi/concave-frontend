a43770d082f1f525ff21fc8987ef809a49d0b4d2
Diogo <dmendoncacosta03@gmail.com>
2022-07-18 21:33:34 -0300

    human readable ABI's

--- libs/core/src/abis/AcnvRedemptionAbi.ts
+++ libs/core/src/abis/AcnvRedemptionAbi.ts
@@ -1,40 +1,7 @@
 export const ACNV_REDEMPTION_ABI = [
-  {
-    inputs: [
-      { internalType: 'address', name: '_CNV', type: 'address' },
-      { internalType: 'address', name: '_aCNV', type: 'address' },
-    ],
-    stateMutability: 'nonpayable',
-    type: 'constructor',
-  },
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: true, internalType: 'address', name: 'redeemer', type: 'address' },
-      { indexed: false, internalType: 'uint256', name: 'output', type: 'uint256' },
-    ],
-    name: 'Redemption',
-    type: 'event',
-  },
-  {
-    inputs: [],
-    name: 'CNV',
-    outputs: [{ internalType: 'contract IERC20', name: '', type: 'address' }],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'aCNV',
-    outputs: [{ internalType: 'contract IERC20', name: '', type: 'address' }],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [{ internalType: 'address', name: 'to', type: 'address' }],
-    name: 'redeem',
-    outputs: [{ internalType: 'uint256', name: 'output', type: 'uint256' }],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
+  'constructor(address _CNV, address _aCNV)',
+  'event Redemption(address indexed redeemer, uint256 output)',
+  'function CNV() view returns (address)',
+  'function aCNV() view returns (address)',
+  'function redeem(address to) returns (uint256 output)',
 ]
