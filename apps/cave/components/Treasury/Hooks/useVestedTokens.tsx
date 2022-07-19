7608afa935a7f85cba50a7b39d4f4c061211ba75
Diogo <dmendoncacosta03@gmail.com>
2022-07-19 11:21:08 -0300

    ...

--- apps/cave/components/Treasury/Hooks/useVestedTokens.tsx
+++ apps/cave/components/Treasury/Hooks/useVestedTokens.tsx
@@ -1,15 +1,17 @@
-import { PCNV_CONTRACT } from '@concave/core'
+import { ACNV_ADDRESS, BBTCNV_ADDRESS, PCNV_CONTRACT } from '@concave/core'
 import { useCurrentSupportedNetworkId } from 'hooks/useCurrentSupportedNetworkId'
 import { useAccount, useBalance } from 'wagmi'
 
 export default function useVestedTokens() {
   const { address } = useAccount()
   const networkId = useCurrentSupportedNetworkId()
-  const { data: bbtCNVData, isLoading: loadingACNV } = useBalance({
+  const { data: bbtCNVData, isLoading: loadingBBTCNV } = useBalance({
     addressOrName: address,
     token: BBTCNV_ADDRESS[networkId],
   })
-  const { data: aCNVData, isLoading: loadingBBTCNV } = useBalance({
+  // aCNV token it's not deployed on rinkeby, so it's better pass the
+  // networkd hardcoded intead using the networkId
+  const { data: aCNVData, isLoading: loadingACNV } = useBalance({
     addressOrName: address,
     token: ACNV_ADDRESS[networkId],
   })
