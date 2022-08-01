9c30f0072e3d99a72b6ed2bbd59e488d2c8daa89
Diogo <dmendoncacosta03@gmail.com>
2022-08-01 11:55:29 -0300

    added pCNV amounts to pCNVNotification

--- apps/cave/components/AMM/Swap/PcnvNotification.tsx
+++ apps/cave/components/AMM/Swap/PcnvNotification.tsx
@@ -1,12 +1,23 @@
 import { Currency, CurrencyAmount } from '@concave/core'
 import { Flex, Text } from '@concave/ui'
+import { formatFixed } from '@ethersproject/bignumber'
+import { parseEther } from 'ethers/lib/utils'
+import { useGet_Amm_Cnv_InfosQuery } from 'graphql/generated/graphql'
 import React from 'react'
 
 type PcnvNotificationProps = {
   isOpen?: boolean
   tokenAmount: CurrencyAmount<Currency>
 }
-export const PcnvNotification: React.FC<PcnvNotificationProps> = ({ isOpen }) => {
+export const PcnvNotification: React.FC<PcnvNotificationProps> = ({ isOpen, tokenAmount }) => {
+  const { data } = useGet_Amm_Cnv_InfosQuery()
+  const pCNVInitialSupply = 33300000
+  const pCNV10PercentClaim = (data?.cnvData?.data?.totalSupply || 0) * 0.1
+  const pCNVToCNVDifference = pCNV10PercentClaim / pCNVInitialSupply
+
+  const pCNVAmount = parseFloat(tokenAmount?.toSignificant())
+  const CNVAmount = pCNVAmount * pCNVToCNVDifference
+
   return (
     <Flex
       transition="all 0.6s"
@@ -26,12 +37,18 @@ export const PcnvNotification: React.FC<PcnvNotificationProps> = ({ isOpen }) =>
         p={6}
       >
         <Info title={'Unlocked pCNV:'} info={'50%'} />
-        <TokenInfoContainer label="11.552 pCNV" />
+        <TokenInfoContainer label={`${pCNVAmount} pCNV`} />
         <Text color={'text.low'} fontSize="sm">
           pCNV purchased is unlocked over time. CNV can be redeemed with unlocked pCNV.
         </Text>
-        <Info title={'Exchange Rate:'} info={'1 pCNV = 0.01 CNV'} />
-        <TokenInfoContainer label="0.0421 CNV" />
+        <Info
+          title={'Exchange Rate:'}
+          info={`1 pCNV = ${formatFixed(
+            parseEther(pCNVToCNVDifference.toString().slice(0, 8)),
+            18,
+          )} CNV`}
+        />
+        <TokenInfoContainer label={`${CNVAmount} CNV`} />
         <Text color={'text.low'} fontSize="sm">
           The amount of CNV redeemable for each pCNV grows as the total supply of CNV increases. For
           more infromation on these mechanics
