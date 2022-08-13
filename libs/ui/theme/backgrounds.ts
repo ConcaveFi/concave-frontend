5edf4ab2096f684226b165659bafffd04f7a1277
Diogo <dmendoncacosta03@gmail.com>
2022-08-13 08:29:39 -0300

    updated backgrounds

--- libs/ui/theme/backgrounds.ts
+++ libs/ui/theme/backgrounds.ts
@@ -15,33 +15,21 @@ export const textures = {
 export const backgrounds = {
   metal: {
     ...textures.metal,
-    bg: `${textures.metal.bg}, linear-gradient(to bottom left, ${colors.secondary[75]} 30%, ${colors.secondary[150]} 96%)`,
+    bg: `${textures.metal.bg}, linear-gradient(225deg, #142E3D 9.27%, #08141D 92.62%)`,
   },
   metalBrighter: {
     ...textures.metal,
-    bg: `${textures.metal.bg}, linear-gradient(to right, ${colors.secondary[125]} 0%, ${colors.secondary[50]} 100%)`,
+    bg: `${textures.metal.bg}, linear-gradient(225deg, #2F4359 0.16%, #162534 100.16%)`,
+  },
+  'metal(ALT)': {
+    ...textures.metal,
+    bg: `${textures.metal.bg}, linear-gradient(225deg, #2B3C50 0.16%, #0A1722 100.16%)`,
   },
   sidebar: {
     ...textures.metal,
     bg: `${textures.metal.bg}, radial-gradient(circle farthest-corner at 100% 50%, ${colors.secondary[75]} 20%, ${colors.secondary[150]})`,
   },
   glass: {
-    pos: 'relative',
-    '::after': {
-      ...textures.glass,
-      borderRadius: 'inherit',
-      content: '""',
-      opacity: 0.3,
-      inset: 0,
-      pos: 'absolute',
-      zIndex: -1,
-      bgSize: 'cover',
-      pointerEvents: 'none',
-    },
-    backdropFilter: 'blur(8px)', // it's technically blur(15px) on figma but visually 8px looks better
-    filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
-  },
-  glassBig: {
     pos: 'relative',
     '::after': {
       borderRadius: 'inherit',
