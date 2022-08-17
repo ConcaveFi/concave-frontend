dae0f66d3289f510e96f356c2912c6f419ec825c
Diogo <dmendoncacosta03@gmail.com>
2022-08-17 10:31:24 -0300

    added prevents

--- apps/cave/components/StakingPositions/DashboardBody/DashBoardState.tsx
+++ apps/cave/components/StakingPositions/DashboardBody/DashBoardState.tsx
@@ -11,31 +11,34 @@ export const useStakePositions = () => {
   const { address } = useAccount()
   const chainId = useCurrentSupportedNetworkId()
 
-  const { data: unlockedTokenIds } = useQuery(['listUnlockedIds', chainId], async () => {
-    const provider = new ethers.providers.EtherscanProvider(chainId)
-    const history = await provider.getHistory(address)
-    const iface = new ethers.utils.Interface(StakingV1Abi)
-    const unlockedFuncHash = '0x7eee288d'
-    return history
-      .filter((data) => data.data.includes(unlockedFuncHash))
-      .map((data) => {
-        const transaction = iface.parseTransaction({ data: data.data, value: data.value })
-        return +transaction.args.tokenId
-      })
-  })
+  const { data: unlockedTokenIds, isLoading: loadingTokens } = useQuery(
+    ['listUnlockedIds', chainId, address],
+    async () => {
+      const provider = new ethers.providers.EtherscanProvider(chainId)
+      const history = await provider.getHistory(address)
+      const iface = new ethers.utils.Interface(StakingV1Abi)
+      const unlockedFuncHash = '0x7eee288d'
+      return history
+        .filter((data) => data.data.includes(unlockedFuncHash))
+        .map((data) => {
+          const transaction = iface.parseTransaction({ data: data.data, value: data.value })
+          return +transaction.args.tokenId
+        })
+    },
+  )
 
   const { data: stakingPositions, isLoading } = useQuery(
-    ['listUserPositions', address, chainId],
+    ['listUserPositions', address, chainId, unlockedTokenIds],
     () => listPositons({ owner: address, provider: concaveProvider(chainId) }),
     { enabled: !!address && !!chainId },
   )
   const totalLocked = getTotalLocked(stakingPositions, CNV[chainId])
   const filteredPositions = stakingPositions?.filter(
-    ({ tokenId }) => !unlockedTokenIds.includes(+tokenId.toString()),
+    ({ tokenId }) => !unlockedTokenIds?.includes(+tokenId.toString()),
   )
 
   return {
-    isLoading,
+    isLoading: isLoading || loadingTokens,
     totalLocked,
     userNonFungibleTokensInfo: filteredPositions || [],
     netWorkId: chainId,
