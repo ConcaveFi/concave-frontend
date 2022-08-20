ce478d77fad3ef7edbe962e3e287df63a4ee97a1
Diogo <62969153+21Chani@users.noreply.github.com>
2022-08-20 19:14:56 -0300

    Increase & update ui library (#499)
    * added font preset
    
    * added colors and glassBig
    
    * updated backgrounds
    
    * updates
    
    * updated colors and backgrounds
    
    * updated glasses background
    
    Co-authored-by: shanghigh <95733088+shanghigh@users.noreply.github.com>

--- libs/ui/theme/colors.ts
+++ libs/ui/theme/colors.ts
@@ -11,6 +11,9 @@ export const colors = {
     medium: '#D7E9FF',
     low: '#5f7a99',
     accent: '#2E97E2',
+    small: '#748CA9',
+    bright: '#79B2F4',
+    brightGreen: '#24C9B5',
   },
 
   stroke: {
@@ -18,8 +21,8 @@ export const colors = {
       'linear-gradient(41.89deg, #53399B 0.69%, #7DE0FF 38.19%, #504179 72.85%, #84E2FF 100%)',
     secondary:
       'linear-gradient(57deg, #171E27 -7.38%, #435F81 29.46%, #202D3D 71.39%, #93C5FF 107.62%)',
-
-    accent: `linear-gradient(90deg, #72639B 0%, #44B9DE 100%)`,
+    accent: 'linear-gradient(90deg, #72639B 0%, #44B9DE 100%)',
+    brightGreen: 'linear-gradient( 180deg, #7AF0CD 0%, #24C9B5 100% )',
   },
 
   primary: /* purple & blue */ {
@@ -28,11 +31,14 @@ export const colors = {
   },
 
   secondary: /* green */ {
-    50: '#364E6A',
-    75: '#19394C',
-    100: '#1B3442',
-    125: '#1C2E3E',
-    150: '#0A161F',
+    200: '#2F4359',
+    175: '#2B3C50',
+    150: '#142E3D',
+    125: '#142C39',
+    100: '#162534',
+    75: '#101D26',
+    50: '#0A1722',
+    25: '#08141D',
   },
 
   subtle: '#2A425F',
