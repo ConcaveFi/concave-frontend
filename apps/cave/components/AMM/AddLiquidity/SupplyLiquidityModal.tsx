9bedaa4d138af8a9ef387ca0d8d2f186ceadb556
Schrammel <schrammel@live.com>
2022-09-14 00:00:11 -0300

    fix(liquidity): fix white screen on ETH/WETH

--- apps/cave/components/AMM/AddLiquidity/SupplyLiquidityModal.tsx
+++ apps/cave/components/AMM/AddLiquidity/SupplyLiquidityModal.tsx
@@ -21,68 +21,59 @@ const SupplyLiquidityContent = ({
   lpData: UseLiquidityData
   onConfirm: () => void
 }) => {
-  const approve0 = useCurrencyApprove(
-    lpData.amount0,
-    ROUTER_ADDRESS[lpData.amount0.wrapped.currency.chainId],
-  )
-  const approve1 = useCurrencyApprove(
-    lpData.amount1,
-    ROUTER_ADDRESS[lpData.amount0.wrapped.currency.chainId],
-  )
+  const { amount0, amount1, token0, token1, poolShare, pair } = lpData
+  const approve0 = useCurrencyApprove(amount0, ROUTER_ADDRESS[amount0.wrapped.currency.chainId])
+  const approve1 = useCurrencyApprove(amount1, ROUTER_ADDRESS[amount0.wrapped.currency.chainId])
+  if (!pair) {
+    return
+  }
   return (
     <>
       <Text fontSize="2xl"> You will receive</Text>
       <HStack>
         <Text fontWeight={'bold'} lineHeight={'48px'} fontSize={32}>
-          {lpData.poolShare?.amount.toSignificant(6, { groupSeparator: ',' })}
+          {poolShare?.amount.toSignificant(6, { groupSeparator: ',' })}
         </Text>
-        <CurrencyIcon h={10} w={10} currency={lpData.amount0.currency} />
-        <CurrencyIcon h={10} w={10} currency={lpData.amount1.currency} />
+        <CurrencyIcon h={10} w={10} currency={amount0.currency} />
+        <CurrencyIcon h={10} w={10} currency={amount1.currency} />
       </HStack>
       <HStack>
-        <Text fontSize="2xl">{`${lpData.amount0.currency.symbol}/${lpData.amount1.currency.symbol} Pool Tokens`}</Text>
+        <Text fontSize="2xl">{`${amount0.currency.symbol}/${amount1.currency.symbol} Pool Tokens`}</Text>
       </HStack>
       <Text
         fontStyle={'italic'}
         fontSize={14}
         textColor={'#5F7A99'}
-      >{`Output is estimated. You will receive approximately ${lpData.poolShare?.amount.toSignificant(
-        6,
-        {
-          groupSeparator: ',',
-        },
-      )} ${lpData.pair.liquidityToken.symbol} or the transaction will revert.`}</Text>
+      >{`Output is estimated. You will receive approximately ${poolShare?.amount.toSignificant(6, {
+        groupSeparator: ',',
+      })} ${pair.liquidityToken.symbol} or the transaction will revert.`}</Text>
       <Box borderRadius={'2xl'} p={6} shadow={'down'}>
         <PositionInfoItem
           label="Rates"
-          value={`1  ${lpData.token0.symbol} = ${lpData.pair.token0Price.toSignificant(6, {
+          value={`1  ${token0.symbol} = ${pair.token0Price.toSignificant(6, {
             groupSeparator: ',',
-          })} ${lpData.token1.symbol}`}
+          })} ${token1.symbol}`}
         />
         <PositionInfoItem
-          value={`1  ${lpData.token1.symbol} = ${lpData.pair.token1Price.toSignificant(6, {
+          value={`1  ${token1.symbol} = ${pair.token1Price.toSignificant(6, {
             groupSeparator: ',',
-          })}  ${lpData.token0.symbol}`}
+          })}  ${token0.symbol}`}
         />
         <PositionInfoItem
           mt={8}
           color={'text.low'}
-          label={`${lpData.token0.symbol} Deposited`}
-          value={`${lpData.amount0.toSignificant(8, { groupSeparator: ',' })} ${
-            lpData.token0.symbol
-          }`}
+          label={`${token0.symbol} Deposited`}
+          value={`${amount0.toSignificant(8, { groupSeparator: ',' })} ${token0.symbol}`}
         />
         <PositionInfoItem
           color={'text.low'}
-          label={`${lpData.token1.symbol} Deposited`}
-          value={`${lpData.amount1.toSignificant(8, { groupSeparator: ',' })} ${
-            lpData.token1.symbol
-          }`}
+          label={`${token1.symbol} Deposited`}
+          value={`${amount1.toSignificant(8, { groupSeparator: ',' })} ${token1.symbol}`}
         />
         <PositionInfoItem
           color={'text.low'}
           label="Share Pool"
-          value={`${lpData.poolShare?.percent?.toSignificant(4)}%`}
+          value={`${poolShare?.percent?.toSignificant(4)}%`}
         />
       </Box>
 
