1e0e9083042ffa98220f77fc2ae28f56a444234d
Schrammel <schrammel@live.com>
2022-09-19 14:59:36 -0300

    refactor(cave/connect): typo (#573)

--- apps/cave/components/UserWallet/ConnectWalletModal/WhatConnectWalletMeans.tsx
+++ apps/cave/components/UserWallet/ConnectWalletModal/WhatConnectWalletMeans.tsx
@@ -6,7 +6,7 @@ export const WhatConnectWalletMeans = (props: StackProps) => (
       Connecting your wallet means the app can see your address and ask for signatures
     </Text>
     <Text color="text.low" opacity={0.6} fontWeight="medium" w="320px">
-      {`Don't`} worry no funds can be transfered without your explicit concent
+      {`Don't`} worry no funds can be transferred without your explicit consent
     </Text>
   </Stack>
 )
