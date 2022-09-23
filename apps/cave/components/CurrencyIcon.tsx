ba6a3d1a603a9100bb719900654af8eb7a069f7a
Schrammel <schrammel@live.com>
2022-09-23 14:30:03 -0300

    refactor(currency): add link possibility

--- apps/cave/components/CurrencyIcon.tsx
+++ apps/cave/components/CurrencyIcon.tsx
@@ -1,29 +1,43 @@
-import { CHAIN_NAME, Currency } from '@concave/core'
-import { AvatarProps, Box, calc, Flex, Image } from '@concave/ui'
+import { ChainId, CHAIN_NAME, Currency, Token } from '@concave/core'
+import { AvatarProps, Box, calc, Flex, Image, Link } from '@concave/ui'
 
 export type CurrencyIconProps = {
   size?: string
   currency: Currency
+  link?: boolean
 } & AvatarProps
 
 const concaveAssetsSrc = 'https://cdn.jsdelivr.net/gh/concavefi/assets@latest/blockchains/'
 export const getCurrencyLogoURI = (currency: Currency) => {
   const networkName = CHAIN_NAME[currency.chainId]
-
   if (networkName === 'rinkeby') return `/assets/tokens/${currency.symbol.toLowerCase()}.svg`
   if (currency.isNative) return `${concaveAssetsSrc}/${networkName}/info/logo.png`
   return `${concaveAssetsSrc}/${networkName}/assets/${currency.wrapped.address}/logo.png`
+}
+
+const etherscanCurrencyLink = (currency: Token) => {
+  const base = `etherscan.io/token/${currency.address}`
+  console.log(currency.chainId)
+  if (currency.chainId === 1) return `https://${base}`
+  return `https://${ChainId[currency.chainId].toLocaleLowerCase()}.${base}`
 }
 
 const sizes = {
   xs: '24px',
   sm: '32px',
 }
-export const CurrencyIcon = ({ currency, size = 'sm', ...props }: CurrencyIconProps) => {
+export const CurrencyIcon = ({ currency, size = 'sm', link, ...props }: CurrencyIconProps) => {
   const src = currency && getCurrencyLogoURI(currency)
   const _size = sizes[size]
   return (
-    <Box as="span" w={_size} {...props}>
+    <Box
+      as={link ? Link : `span`}
+      href={link && etherscanCurrencyLink(currency.wrapped)}
+      w={_size}
+      target="_blank"
+      cursor={link && `pointer`}
+      {...props}
+    >
       <Image
         src={src}
         alt={`${currency?.symbol} icon`}
