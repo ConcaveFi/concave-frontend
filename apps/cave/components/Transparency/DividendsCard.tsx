19de28f44ac9b53b78734f746d60b7515dc5a784
psgb-r3763 <96499579+psgb-r3763@users.noreply.github.com>
2022-10-21 16:48:11 -0400

    CE-905: refactor redeem and treasury dividend card (#610)
    * redeem and treasury card
    
    * adjust mobile spacing

--- apps/cave/components/Transparency/DividendsCard.tsx
+++ apps/cave/components/Transparency/DividendsCard.tsx
@@ -1,57 +1,50 @@
 import { Card, Flex, Text } from '@concave/ui'
-interface DividendsCardProps {
-  onChange: () => void
-}
 
-export default function DividendsCard({ width }: { width: string }) {
+export default function DividendsCard() {
   return (
     <Card
       direction={{ base: 'column', lg: 'row' }}
       align="center"
-      w={width}
+      w={'100%'}
       variant="secondary"
-      height={{ base: '260px', lg: '179px' }}
-      py={{ base: 2, lg: 0 }}
-      px={6}
+      height={'auto'}
+      p={6}
+      justifyContent={'center'}
+      gap={{ base: 4, xl: 0 }}
     >
-      <Flex
-        direction={{ base: 'column', xl: 'row' }}
-        align="center"
-        mx={'auto'}
-        gap={{ base: 0, xl: 0 }}
-      >
-        <Text fontSize={'4xl'} fontWeight="700">
-          Dividends
-        </Text>
+      <Text textAlign={'center'} w={{ base: '100%', lg: '33%' }} fontSize={'3xl'} fontWeight={700}>
+        Dividends
+      </Text>
+      <Flex justifyContent={'center'} w={{ base: '100%', lg: '33%' }} mb={{ base: 3, lg: 0 }}>
         <Text
-          textAlign={{ base: 'center', xl: 'justify' }}
+          textAlign={'center'}
           fontWeight={700}
           textColor={'text.low'}
           fontSize="14px"
-          maxW={'350px'}
-          px={{ base: 12, md: 0, xl: 6 }}
+          w={{ base: '70%', lg: '90%' }}
         >
           Dividends are distributed on a quarterly basis to the holders. You may redeem these
           dividends upon the distribution date on the Your Stake Position page.
         </Text>
       </Flex>
-      <Card
-        width={'250px'}
-        height="105px"
-        rounded={'2xl'}
-        direction="column"
-        justify={'center'}
-        align="center"
-        fontWeight="700"
-        my={'auto'}
-        mx="auto"
-        shadow={'up'}
-      >
-        <Text fontSize={'2xl'}>Coming Soon</Text>
-        <Text textColor={'text.low'} fontSize="14px">
-          Distribution date TBD
-        </Text>
-      </Card>
+      <Flex justifyContent={'center'} w={{ base: '100%', lg: '33%' }}>
+        <Card
+          width={'250px'}
+          height="105px"
+          rounded={'2xl'}
+          direction="column"
+          justify={'center'}
+          align="center"
+          fontWeight={700}
+          my={'auto'}
+          shadow={'up'}
+        >
+          <Text fontSize={'2xl'}>Coming Soon</Text>
+          <Text textColor={'text.low'} fontSize="14px">
+            Distribution date TBD
+          </Text>
+        </Card>
+      </Flex>
     </Card>
   )
 }
