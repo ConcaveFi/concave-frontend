e77caa1bfe49749c1a7e505774a714e6d6381ce5
Schrammel <schrammel@live.com>
2022-11-27 00:44:40 -0300

    fix(cave/airdropClain): fix airdrop

---
+++ apps/cave/components/UserDashboard/SnapshotOptions.ts
@@ -0,0 +1,10 @@
+export enum SnapshotOptions {
+  Global = 'Global',
+  LiquidStaking = 'LiquidStaking',
+  DynamicBonds = 'DynamicBonds',
+  Marketplace = 'Marketplace',
+  TxHistory = 'TxHistory',
+  Liquidity = 'Liquidity',
+  DeltaNeutral = 'DeltaNeutral',
+  Redeem = 'Redeem',
+}
