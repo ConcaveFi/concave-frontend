d124e95c9242028346d7a35897204a9c8857935c
21Chani <dmendoncacosta03@gmail.com>
2022-11-28 05:48:49 -0300

    format...

--- apps/cave/components/Transparency/Charts/LockedCNVSeriesChart.tsx
+++ apps/cave/components/Transparency/Charts/LockedCNVSeriesChart.tsx
@@ -59,7 +59,6 @@ export const LockedCNVSeriesChart = () => {
               tick={(tickData) => {
                 const customValue = data.ticks.ticksObject[tickData.payload.value]
                 return <CustomizedAxisTick customValue={customValue} isMobile {...tickData} />
-
               }}
               axisLine={{ stroke: CHART_COLORS.TextLow }}
               tickLine={{ stroke: CHART_COLORS.TextLow }}
