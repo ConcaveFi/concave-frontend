557ec9c8c1f234afc0a0288b1d5bb95f223a53f2
Schrammel <schrammel@live.com>
2022-11-29 09:41:12 -0300

    fix_sidebar (#665)

--- apps/cave/components/ButtonLink.tsx
+++ apps/cave/components/ButtonLink.tsx
@@ -5,7 +5,7 @@ export type ButtonLinkProps = ButtonProps & LinkProps
 
 export function ButtonLink({ href, ...props }: ButtonLinkProps) {
   return (
-    <Link href={href} passHref>
+    <Link style={{ width: '100%' }} href={href} passHref>
       <Button as="a" {...props} />
     </Link>
   )
