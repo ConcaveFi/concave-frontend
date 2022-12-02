b434a9b853c0410fbdc8cce5f77d4cac192e20a3
psgb-r3763 <psgb@protonmail.com>
2022-12-02 15:25:42 -0500

    user dashboard mock

---
+++ apps/cave/components/UserDashboard/NavButton.tsx
@@ -0,0 +1,79 @@
+import { Flex, gradientBorder, Text } from '@concave/ui'
+import { useAccount } from 'wagmi'
+
+export const NavButton = ({
+  title,
+  isSelected,
+  onClick,
+  summaryArray,
+  isDisabled,
+}: {
+  title: string
+  isSelected?: boolean
+  onClick?: VoidFunction
+  summaryArray?: { label: string; data: string }[]
+  isDisabled?: boolean
+}) => {
+  const { isConnected } = useAccount()
+
+  let settings = {
+    alignItems: 'unset',
+    justifyContent: 'unset',
+    flexMl: 2,
+    textMt: 2,
+    textMb: 1,
+    textColor: 'white',
+  }
+
+  if (isDisabled) {
+    settings = {
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexMl: 0,
+      textMt: 0,
+      textMb: 0,
+      textColor: 'text.low',
+    }
+  }
+
+  return (
+    <button onClick={onClick}>
+      <Flex
+        minH={'100px'}
+        height="full"
+        rounded={'3xl'}
+        w={'100%'}
+        p={3}
+        shadow={isSelected ? 'up' : 'down'}
+        fontSize={'sm'}
+        cursor={isDisabled ? 'default' : 'pointer'}
+        sx={isSelected && { ...gradientBorder({ borderWidth: 2 }) }}
+        alignItems={settings.alignItems}
+        justifyContent={settings.justifyContent}
+      >
+        <Flex direction={'column'} align="start" lineHeight={'1.25rem'} ml={settings.flexMl}>
+          <Text
+            color={settings.textColor}
+            fontSize={'lg'}
+            fontWeight={'bold'}
+            mt={settings.textMt}
+            mb={settings.textMb}
+          >
+            {title}
+          </Text>
+          {isConnected ? (
+            <>
+              {summaryArray?.map((summaryObject, index) => (
+                <Text key={index + title} fontSize={'sm'}>
+                  {summaryObject.label}: {summaryObject.data}
+                </Text>
+              ))}
+            </>
+          ) : (
+            <></>
+          )}
+        </Flex>
+      </Flex>
+    </button>
+  )
+}
---
+++ apps/cave/components/UserDashboard/Summary/Bonding/BondingSnapshotChart.tsx
@@ -0,0 +1,29 @@
+import { Flex } from '@concave/ui'
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
+
+export function BondingSnapshotChart({ data }) {
+  return (
+    <Flex w={'75%'} height={'100%'}>
+      <ResponsiveContainer width={'90%'} height={350}>
+        <LineChart data={data} margin={{ top: 55, right: 0, left: 0, bottom: 0 }}>
+          <CartesianGrid strokeDasharray="3 3" opacity={0.25} />
+          <XAxis dataKey="date" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line dot={false} type="monotone" dataKey="CNV Price" stroke="#D49F60" strokeWidth={4} />
+          <Line dot={false} type="monotone" dataKey="Bond Price" stroke="#4E7DB5" strokeWidth={4} />
+        </LineChart>
+      </ResponsiveContainer>
+    </Flex>
+  )
+}
