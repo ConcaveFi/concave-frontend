2021d486053ad563774ff09390c3769fdfe7d141
psgb-r3763 <psgb@protonmail.com>
2022-12-06 20:56:45 -0500

    ce-1044: update wallet buttons, re-order tokens

--- apps/cave/components/UserDashboard/UserDashboardWallet.tsx
+++ apps/cave/components/UserDashboard/UserDashboardWallet.tsx
@@ -1,4 +1,4 @@
-import { CNV, DAI, NATIVE } from '@concave/core'
+import { CNV, Currency, DAI, NATIVE } from '@concave/core'
 import { Button, Flex, Text } from '@concave/ui'
 import useVestedTokens from 'components/Transparency/Hooks/useVestedTokens'
 import { ConnectedUserButton } from 'components/UserWallet/ConnectedUserButton'
@@ -6,23 +6,29 @@ import { useCurrencyBalance } from 'hooks/useCurrencyBalance'
 import { useCurrentSupportedNetworkId } from 'hooks/useCurrentSupportedNetworkId'
 import { useAccount } from 'wagmi'
 
+function formatNumber2DP(value: number) {
+  return value.toLocaleString(undefined, {
+    maximumFractionDigits: 2,
+  })
+}
+
+function getFormattedCurrencyBalance(address: Currency) {
+  const tokenData = useCurrencyBalance(address, { watch: true })
+  return formatNumber2DP(+tokenData.data.toSignificant(6, { groupSeparator: ',' }))
+}
+
 export function UserDashboardWallet() {
   const { isConnected } = useAccount()
   const networkId = useCurrentSupportedNetworkId()
 
-  const cnvData = useCurrencyBalance(CNV[networkId], { watch: true })
-  const cnvBalance = cnvData.data.toSignificant(6, { groupSeparator: ',' })
-
-  const ethData = useCurrencyBalance(NATIVE[networkId], { watch: true })
-  const ethBalance = ethData.data.toSignificant(6, { groupSeparator: ',' })
-
-  const daiData = useCurrencyBalance(DAI[networkId], { watch: true })
-  const daiBalance = daiData.data.toSignificant(6, { groupSeparator: ',' })
+  const cnvBalance = getFormattedCurrencyBalance(CNV[networkId])
+  const ethBalance = getFormattedCurrencyBalance(NATIVE[networkId])
+  const daiBalance = getFormattedCurrencyBalance(DAI[networkId])
 
   const { aCNV: aCNVData, bbtCNV: bbtCNVData, pCNV: pCNVData } = useVestedTokens()
-  const aCNVBalance = +aCNVData?.data?.formatted
-  const bbtCNVBalance = +bbtCNVData?.data?.formatted
-  const pCNVBalance = +pCNVData?.data?.formatted
+  const aCNVBalance = formatNumber2DP(+aCNVData?.data?.formatted)
+  const bbtCNVBalance = formatNumber2DP(+bbtCNVData?.data?.formatted)
+  const pCNVBalance = formatNumber2DP(+pCNVData?.data?.formatted)
 
   return (
     <WalletSurface>
@@ -30,11 +36,11 @@ export function UserDashboardWallet() {
         <>
           <ConnectedUserButton w={'190px'} />
           <TokenButton>{cnvBalance} CNV</TokenButton>
-          <TokenButton>{aCNVBalance} aCNV</TokenButton>
           <TokenButton>{bbtCNVBalance} bbtCNV</TokenButton>
           <TokenButton>{pCNVBalance} pCNV</TokenButton>
-          <TokenButton>{daiBalance} DAI</TokenButton>
-          <TokenButton>{ethBalance} ETH</TokenButton>
+          <TokenButton>{aCNVBalance} aCNV</TokenButton>
+          <TokenButton isDisabled>{daiBalance} DAI</TokenButton>
+          <TokenButton isDisabled>{ethBalance} ETH</TokenButton>
         </>
       ) : (
         <Text>Please connect wallet</Text>
@@ -61,15 +67,25 @@ const WalletSurface = ({ children }: { children: JSX.Element | JSX.Element[] }) 
   </Flex>
 )
 
-const TokenButton = ({ children }) => (
+const TokenButton = ({
+  children,
+  isDisabled,
+}: {
+  children: JSX.Element | JSX.Element[] | any
+  isDisabled?: boolean
+}) => (
   <Button
     height="40px"
     shadow="up"
     fontFamily="heading"
     _active={{ shadow: 'down' }}
-    w={'120px'}
+    w={'160px'}
     rounded="2xl"
+    px={4}
+    disabled={isDisabled}
   >
-    {children}
+    <Text fontWeight={'normal'} overflow={'hidden'} textOverflow={'ellipsis'}>
+      {children}
+    </Text>
   </Button>
 )
