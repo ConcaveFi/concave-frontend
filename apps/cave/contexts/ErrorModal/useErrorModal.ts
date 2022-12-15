3f8c2f36be18a4f81379d4d5e2010627f07cd8b6
Schrammel <schrammel@live.com>
2022-12-15 18:03:46 -0300

    feat(cave/errorModal): add discord field on submit error

--- apps/cave/contexts/ErrorModal/useErrorModal.ts
+++ apps/cave/contexts/ErrorModal/useErrorModal.ts
@@ -1,14 +1,15 @@
-import { useContext } from 'react'
+import { detect } from 'detect-browser'
+import { useContext, useState } from 'react'
 import { useAccount, useNetwork } from 'wagmi'
 import { ErrorModalContext } from './ErrorModalContext'
 import { sendReport } from './sendReport'
-import { detect } from 'detect-browser'
 
 export const useErrorModal = () => {
   const errorContext = useContext(ErrorModalContext)
-  const { name, os, version } = detect();
+  const { name, os, version } = detect()
   const chain = useNetwork()
   const account = useAccount()
+  const [username, setUsername] = useState('anonymous')
   const error = errorContext.error || {}
   const infos = {
     Address: account?.address,
@@ -16,18 +17,21 @@ export const useErrorModal = () => {
     OS: os,
     Browser: `${name} ${version}`,
     Chain: chain.chain?.name,
+    Discord: username,
   }
   return {
     infos,
     error,
+    username,
     isOpen: errorContext.isOpen,
+    setUsername,
     onOpen: errorContext.onOpen,
     onClose: errorContext.onClose,
-    onConfirm: (report: { userDescription: string, extra: Record<string, string> }) => {
+    onConfirm: (report: { userDescription: string; extra: Record<string, string> }) => {
       sendReport({
         userDescription: report.userDescription,
         error: JSON.stringify(error),
-        extra: { ...errorContext.extra, ...infos, ...report.extra }
+        extra: { ...errorContext.extra, ...infos, ...report.extra },
       })
       errorContext.onClose()
     },
