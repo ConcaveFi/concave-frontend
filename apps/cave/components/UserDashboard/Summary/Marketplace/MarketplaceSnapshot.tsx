3f247ed26110797310569b315eda2e665d9be31f
psgb-r3763 <psgb@protonmail.com>
2022-12-15 19:11:33 -0500

    userdashboard marketplace

---
+++ apps/cave/components/UserDashboard/Summary/Marketplace/MarketplaceSnapshot.tsx
@@ -0,0 +1,7 @@
+import { MarketplaceDashboard } from 'components/Marketplace/Main/MarketplaceDashboard'
+
+export function MarketplaceSnapshot() {
+  return (
+    <MarketplaceDashboard filterUserPositions w={'100%'} h={'auto'} justifyContentSort={'center'} />
+  )
+}
