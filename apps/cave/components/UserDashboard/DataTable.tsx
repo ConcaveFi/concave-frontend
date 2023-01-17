5cff859277b0b3cd674dc83c58ff0612ddb061f4
21Chani <dmendoncacosta03@gmail.com>
2023-01-17 15:44:56 -0300

    WIP

--- apps/cave/components/UserDashboard/DataTable.tsx
+++ apps/cave/components/UserDashboard/DataTable.tsx
@@ -3,21 +3,24 @@ import { Box } from '@concave/ui'
 export function DataTable({
   children,
   h = 'unset',
+  isExpanded,
 }: {
   children: JSX.Element | JSX.Element[]
   h?: string
+  isExpanded?: boolean
 }) {
   return (
     <Box
-      h="400px"
+      h="full"
       w={'100%'}
-      overflowY={'visible'}
+      overflowY={'auto'}
       overflowX="hidden"
       borderRadius="3xl"
       px={'0.5rem'}
       mx={4}
       shadow={'down'}
       apply="scrollbar.big"
+      maxH={{ base: '', lg: isExpanded ? '664px' : '280x' }}
     >
       {children}
     </Box>
