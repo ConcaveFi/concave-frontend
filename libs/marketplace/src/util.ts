3d8e0f85639a3e772f9007a1fd1a5ce19fe5641b
Schrammel <schrammel@live.com>
2023-01-27 18:41:25 -0300

    feat(apps/cave): improve chart

--- libs/marketplace/src/util.ts
+++
@@ -1,5 +0,0 @@
-export const fetcher = async <T>(input: RequestInfo | URL, init?: RequestInit) => {
-  const request = await fetch(input, init)
-  const data: T = await request.json()
-  return data
-}
