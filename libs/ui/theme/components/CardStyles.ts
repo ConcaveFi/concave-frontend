1e7127558e3f1c00617ac790e8394790c862f9b9
Schrammel <schrammel@live.com>
2023-02-02 16:44:09 -0300

    wip

--- libs/ui/theme/components/CardStyles.ts
+++ libs/ui/theme/components/CardStyles.ts
@@ -1,20 +1,23 @@
 import { ComponentStyleConfig } from '@chakra-ui/react'
 import { gradientBorder } from '../utils/gradientBorder'
 
+const variants = {
+  primary: { bg: 'bg.primary', },
+  secondary: { apply: 'background.glass' },
+}
+
 export const CardStyles: ComponentStyleConfig = {
-  baseStyle: ({ borderWidth = 2, borderRadius = '2xl', borderGradient, ...props }) => ({
-    borderRadius,
-    ...gradientBorder({ borderRadius, borderWidth, ...props, variant: borderGradient }),
-  }),
-  sizes: {},
-  variants: {
-    primary: (props) => ({
-      bg: 'bg.primary',
-    }),
-    secondary: {
-      apply: 'background.glass',
-    },
+  baseStyle: ({ borderWidth = 2, borderRadius = '2xl', borderGradient, ...props }) => {
+    const variant = variants[props.variant]
+    return {
+      borderRadius,
+      ...gradientBorder({ borderRadius, borderWidth, ...props, variant: borderGradient }),
+      ...variant
+    }
   },
+  sizes: {},
+  //it is not working
+  variants,
   defaultProps: {},
 }
 
