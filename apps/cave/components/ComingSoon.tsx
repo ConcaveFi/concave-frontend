a7563ace30db751a00eb470b090070ca47e8df42
Schrammel <schrammel@live.com>
2023-02-10 20:16:28 -0300

    feat(apps/cave/comingsoon): create components

--- apps/cave/components/ComingSoon.tsx
+++ apps/cave/components/ComingSoon.tsx
@@ -1,29 +1,5 @@
-import { Box, Flex, Text } from '@concave/ui'
+import { BlurComponent } from './BlurComponent'
 
-export const ComingSoom = ({children = <></>}:{ children: JSX.Element }) => {
-return <Flex
-  position="relative"
-  width="full"
-  height="full"
-  justifyContent="center"
-  alignItems="center">
-  {children}
-  <Box   
-    width={'100%'} 
-    height={`100%`} 
-    position={'absolute'} 
-    backdropFilter="blur(8px)"
-    backdropBlur={'16px'}
-    top={0} 
-    right={0}  
-  />
-  <Text 
-    position={'absolute'} 
-    fontSize={'lg'}
-    fontWeight={'bold'}
-    >
-      Coming Soon
-  </Text>
-  
-</Flex>
+export const ComingSoom = ({ children = <></> }: { children: JSX.Element }) => {
+  return <BlurComponent message="Coming Soon">{children}</BlurComponent>
 }
