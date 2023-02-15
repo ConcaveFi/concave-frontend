7c980d59e1c2342804b226928762311e589c7579
Schrammel <schrammel@live.com>
2023-02-15 16:20:59 -0300

    remove add-liquidity

--- apps/cave/hooks/useQueryParams.ts
+++ apps/cave/hooks/useQueryParams.ts
@@ -2,9 +2,9 @@
 import { useRouter } from 'next/router'
 import { useQuery } from 'react-query'
 
-export const useQueryParams = <T>(def: T) => {
+export const useQueryParams = <T>(def?: T) => {
   const router = useRouter()
-  const query = router.isReady ? router.query : def
+  const query = router.isReady ? router.query : def || {}
   return useQuery(
     ['queryParams', JSON.stringify(query)],
     async () => query as T,
