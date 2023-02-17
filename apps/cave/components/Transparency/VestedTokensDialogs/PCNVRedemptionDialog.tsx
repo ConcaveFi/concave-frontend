1561eab3a6e55b7671c37ed04e3c01e343608596
Schrammel <schrammel@live.com>
2023-02-17 17:38:39 -0300

    dead code

--- apps/cave/components/Transparency/VestedTokensDialogs/PCNVRedemptionDialog.tsx
+++ apps/cave/components/Transparency/VestedTokensDialogs/PCNVRedemptionDialog.tsx
@@ -1,10 +1,11 @@
 import { BoxProps, Modal } from '@concave/ui'
 
-import { ModalType } from '../../UserDashboard/redeem/RedeemTokensCard'
 import { RedeemCard } from '../../UserDashboard/redeem/RedeemCard'
 import { useRedeemPCNVCard } from '../../UserDashboard/redeem/useRedeemPCNVCard'
 
-export const PCNVRedemptionDialog: React.FC<ModalType> = (props) => {
+export const PCNVRedemptionDialog: React.FC<{ onClose: VoidFunction; isOpen: boolean }> = (
+  props,
+) => {
   return (
     <>
       <Modal
--- apps/cave/components/UserDashboard/redeem/RedeemTokens.tsx
+++
@@ -1,6 +0,0 @@
-import { VStack } from '@concave/ui'
-import { RedeemTokensCard } from 'components/UserDashboard/redeem/RedeemTokensCard'
-
-export function RedeemTokens() {
-  return <RedeemTokensCard></RedeemTokensCard>
-}
