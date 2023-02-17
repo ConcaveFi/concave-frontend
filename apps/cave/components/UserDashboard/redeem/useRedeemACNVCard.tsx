fdacb06017320d2bd5d401b21871b246f6d6ed8c
Schrammel <schrammel@live.com>
2023-02-17 18:00:44 -0300

    dead code

--- apps/cave/components/UserDashboard/redeem/useRedeemACNVCard.tsx
+++ apps/cave/components/UserDashboard/redeem/useRedeemACNVCard.tsx
@@ -2,13 +2,15 @@ import { ACNV, CNV, Token } from '@concave/core'
 import { useRedeemFields } from 'components/UserDashboard/redeem/useRedeemFields'
 import { useRedeemStatus } from 'components/UserDashboard/redeem/useRedeemStatus'
 import { useCurrentSupportedNetworkId } from 'hooks/useCurrentSupportedNetworkId'
+import { useAccount } from 'wagmi'
 import { RedeemCard } from './RedeemCard'
+import { useRedeemableACNV } from './useRedeemable'
 import { useRedeemACNV } from './useRedeemACNV'
 
 export const useRedeemACNVCard = () => {
   const chainId = useCurrentSupportedNetworkId()
-  const tokenOut = ACNV[chainId]
-  const redeemStatus = useRedeemStatus(tokenOut)
+  const { address } = useAccount()
+  const { data: redeemStatus } = useRedeemableACNV({ address })
   const redeemFields = useRedeemFields({
     amountOut: redeemStatus.redeemable,
     tokenIn: CNV[chainId],
