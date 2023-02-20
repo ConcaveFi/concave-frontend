8a9635abca56c7c05ab82ac76fe1c7de96fb115e
Schrammel <schrammel@live.com>
2023-02-20 16:42:42 -0300

    wip

--- apps/cave/components/ComingSoon.tsx
+++ apps/cave/components/ComingSoon.tsx
@@ -1,5 +1,11 @@
+import { Text } from '@concave/ui'
 import { BlurComponent } from './BlurComponent'
 
+const Message = () => (
+  <Text fontSize={'lg'} noOfLines={1} fontWeight={'bold'}>
+    Comming Soon
+  </Text>
+)
 export const ComingSoom = ({ children = <></> }: { children: JSX.Element }) => {
-  return <BlurComponent message="Coming Soon">{children}</BlurComponent>
+  return <BlurComponent overlayElement={Message()}>{children}</BlurComponent>
 }
