271b4af6008919c32c39455a57ff86096969b4d8
Schrammel <schrammel@live.com>
2023-03-03 12:27:46 -0300

    improve mobile ui

--- apps/cave/components/UserDashboard/Summary/Redeem/RedeemSnapshot.tsx
+++ apps/cave/components/UserDashboard/Summary/Redeem/RedeemSnapshot.tsx
@@ -1,9 +1,13 @@
-import { Flex, useBreakpointValue } from '@concave/ui'
+import { Flex, Text, useBreakpointValue } from '@concave/ui'
 import { ACNVChart } from 'components/Transparency/Charts/ACNVChart'
 import { BbtCNVChart } from 'components/Transparency/Charts/BbtCNVChart'
 import { RedemACNVCard } from 'components/UserDashboard/redeem/ACNVRedemptionDialog'
 import { RedemBBTCard } from 'components/UserDashboard/redeem/BBTCNVRedemptionDialog'
 import { RedeemPCNVCard } from 'components/UserDashboard/redeem/PCNVRedemptionDialog'
+import { RedeemCard } from 'components/UserDashboard/redeem/RedeemCard'
+import { useRedeemACNVCard } from 'components/UserDashboard/redeem/useRedeemACNVCard'
+import { useRedeemBBTCNVCard } from 'components/UserDashboard/redeem/useRedeemBBTCNVCard'
+import { useRedeemPCNVCard } from 'components/UserDashboard/redeem/useRedeemPCNVCard'
 
 export const RedeemSnapshot = () => {
   const View = useBreakpointValue({ base: RedeemSnapshotMobile, md: RedeemSnapshotDesktop })
@@ -11,20 +15,40 @@ export const RedeemSnapshot = () => {
 }
 
 export const RedeemSnapshotMobile = () => {
-  const gap = 2
-
+  const redeemPCNVProps = useRedeemPCNVCard()
+  const redeemACNVProps = useRedeemACNVCard()
+  const redeemBBTCNVProps = useRedeemBBTCNVCard()
+  if (
+    redeemPCNVProps.redeemStatus?.redeemable.equalTo(0) &&
+    redeemACNVProps.redeemStatus?.redeemable.equalTo(0) &&
+    redeemBBTCNVProps.redeemStatus?.redeemable.equalTo(0)
+  ) {
+    return (
+      <Flex
+        w={'full'}
+        direction={'column'}
+        justifyContent={'space-around'}
+        overflowX={'scroll'}
+        shadow={'Down Big'}
+        borderRadius={'3xl'}
+        gap={2}
+        p={2}
+      >
+        <Text>{"Your wallet don't have aCNV, bbtCNV or pCNV to redeem"} </Text>
+      </Flex>
+    )
+  }
   return (
-    <Flex w={'full'} direction={'column'} overflowX={'scroll'} gap={gap}>
-      <RedeemPCNVCard w={'full'} />
-      <RedemACNVCard w={'full'} />
-      <RedemBBTCard w={'full'} />
+    <Flex w={'full'} direction={'column'} overflowX={'scroll'} gap={2}>
+      <RedeemCard {...redeemPCNVProps} w={'full'} />
+      <RedeemCard {...redeemACNVProps} w={'full'} />
+      <RedeemCard {...redeemBBTCNVProps} w={'full'} />
     </Flex>
   )
 }
 
 export const RedeemSnapshotDesktop = () => {
   const gap = 2
-
   return (
     <Flex w={'full'} direction={'column'} flexWrap={'wrap'} gap={gap}>
       <Flex direction={{ base: 'column', sm: 'row' }} gap={gap} w={'full'} h={'fit-content'}>
--- apps/cave/components/UserDashboard/redeem/RedeemCard.tsx
+++ apps/cave/components/UserDashboard/redeem/RedeemCard.tsx
@@ -1,6 +1,16 @@
 import { Token } from '@concave/core'
 import { ExpandArrowIcon } from '@concave/icons'
-import { Box, Button, Card, Flex, HStack, Text, useBreakpointValue, VStack } from '@concave/ui'
+import {
+  Box,
+  BoxProps,
+  Button,
+  Card,
+  Flex,
+  HStack,
+  Text,
+  useBreakpointValue,
+  VStack,
+} from '@concave/ui'
 import { TransactionSubmittedDialog } from 'components/TransactionDialog/TransactionSubmittedDialog'
 
 import { CustomRecipient } from 'components/AMM'
@@ -57,7 +67,7 @@ export const RedeemCard = ({
   redeemTransaction,
   redeemMax,
   ...boxProps
-}: RedeemCard<Token, Token>) => {
+}: RedeemCard<Token, Token> & BoxProps) => {
   const {
     immutableAmount,
     amountIn,
