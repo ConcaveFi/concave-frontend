7ba4bebbd9c88d53d3adc2dee8780060cf54e276
Schrammel <schrammel@live.com>
2023-04-05 20:47:40 -0300

    new treasurys

--- apps/cave/components/TransparencyDiagram/types.ts
+++ apps/cave/components/TransparencyDiagram/types.ts
@@ -4,6 +4,9 @@ export enum ChainEnum {
   ETH = 'etherscan.io',
   AVAX = 'snowtrace.io',
   BSC = 'bscscan.com',
+  FTM = 'ftmscan.com',
+  POLYGON = 'polygonscan.com',
+  OP = 'optimistic.etherscan.io',
   ARBI = 'arbiscan.io',
 }
 
