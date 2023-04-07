73994094ddeb8bbb2ac6d6910ca84e6e69ce046b
21Chani <dmendoncacosta03@gmail.com>
2023-04-07 13:34:24 -0300

    v1 + v2 reedemed sum

--- apps/cave/components/UserDashboard/redeem/BBTCNVRedemptionDialog.tsx
+++ apps/cave/components/UserDashboard/redeem/BBTCNVRedemptionDialog.tsx
@@ -1,9 +1,10 @@
-import { BoxProps, Modal } from '@concave/ui'
+import { BoxProps } from '@concave/ui'
 
 import { RedeemCard } from '../../UserDashboard/redeem/RedeemCard'
 import { useRedeemBBTCNVCard } from '../../UserDashboard/redeem/useRedeemBBTCNVCard'
 
 export const RedemBBTCard = (props: BoxProps) => {
   const redeemCardProps = useRedeemBBTCNVCard()
+
   return <RedeemCard {...redeemCardProps} {...props} />
 }
--- apps/cave/components/UserDashboard/redeem/useRedeemBBTCNVCard.tsx
+++ apps/cave/components/UserDashboard/redeem/useRedeemBBTCNVCard.tsx
@@ -5,17 +5,30 @@ import { useAccount } from 'wagmi'
 import { RedeemCard } from './RedeemCard'
 import { useRedeemable } from './useRedeemable'
 import { useRedeemBBTCNV } from './useRedeemBBTCNV'
+import { useMemo } from 'react'
 
 export const useRedeemBBTCNVCard = () => {
   const chainId = useCurrentSupportedNetworkId()
   const { address } = useAccount()
 
   const tokenOut = BBTCNV[chainId]
-  const { data: redeemStatus } = useRedeemable({
-    address,
+  const {data: redeemStatusOld} = useRedeemable({
+    address: "0xdd11Ae83B49ee68B37fF3E6442f994Fc037bb4a1",
+    contract: "0x7fcc30E97D718864d46a84F13E3Ba111a56123D3",
+    token: BBTCNV[chainId],
+  })
+  
+  const { data: redeemStatusNew } = useRedeemable({
+    address: "0xdd11Ae83B49ee68B37fF3E6442f994Fc037bb4a1",
     contract: BBTCNV_REDEMPTION_V2[chainId],
     token: BBTCNV[chainId],
   })
+
+ const redeemStatus = useMemo(() => {
+  if(!redeemStatusOld || !redeemStatusNew) return
+  return {...redeemStatusNew, redeemed: redeemStatusNew.redeemed.add(redeemStatusOld.redeemed)}
+ }, [redeemStatusNew, redeemStatusOld])
+  
   const redeemFields = useRedeemFields({
     amountOut: CurrencyAmount.fromRawAmount(tokenOut, 0),
     tokenIn: CNV[chainId],
