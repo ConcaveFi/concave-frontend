f9aa79e44cbf26d9f505b57c730afbdeede2e77e
Diogo <62969153+21Chani@users.noreply.github.com>
2023-04-07 20:15:58 -0300

    v1 + v2 reedemed sum (#715)
    * v1 + v2 reedemed sum
    
    * ...
    
    ---------
    
    Co-authored-by: shanghigh <95733088+shanghigh@users.noreply.github.com>

--- apps/cave/components/UserDashboard/redeem/useRedeemBBTCNVCard.tsx
+++ apps/cave/components/UserDashboard/redeem/useRedeemBBTCNVCard.tsx
@@ -5,17 +5,30 @@ import { useAccount } from 'wagmi'
 import { RedeemCard } from './RedeemCard'
 import { useRedeemable } from './useRedeemable'
 import { useRedeemBBTCNV } from './useRedeemBBTCNV'
+import { useMemo } from 'react'
 
 export const useRedeemBBTCNVCard = () => {
   const chainId = useCurrentSupportedNetworkId()
   const { address } = useAccount()
 
   const tokenOut = BBTCNV[chainId]
-  const { data: redeemStatus } = useRedeemable({
+  const {data: redeemStatusOld} = useRedeemable({
+    address,
+    contract: "0x7fcc30E97D718864d46a84F13E3Ba111a56123D3", // Contract V1
+    token: BBTCNV[chainId],
+  })
+  
+  const { data: redeemStatusNew } = useRedeemable({
     address,
     contract: BBTCNV_REDEMPTION_V2[chainId],
     token: BBTCNV[chainId],
   })
+
+ const redeemStatus = useMemo(() => {
+  if(!redeemStatusOld || !redeemStatusNew) return
+  return {...redeemStatusNew, redeemed: redeemStatusNew.redeemed.add(redeemStatusOld.redeemed)}
+ }, [redeemStatusNew, redeemStatusOld])
+  
   const redeemFields = useRedeemFields({
     amountOut: CurrencyAmount.fromRawAmount(tokenOut, 0),
     tokenIn: CNV[chainId],
