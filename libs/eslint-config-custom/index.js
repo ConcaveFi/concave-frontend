a1d44b530caa5503ceccf22be36fcbc8c20769ad
Schrammel <schrammel@live.com>
2023-05-31 14:23:04 -0300

    wip

--- libs/eslint-config-custom/index.js
+++ libs/eslint-config-custom/index.js
@@ -1,13 +1,13 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ['next', 'turbo', 'prettier'],
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
   parserOptions: {
     babelOptions: {
-      presets: [require.resolve("next/babel")],
+      presets: [require.resolve('next/babel')],
     },
   },
 };
